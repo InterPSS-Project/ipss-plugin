7430c1d4245fb24d8bbdfa9bcbbe9423d2c2dc3b
ezzhou <mike@interpss.org>
2013-09-12 14:25:29 -0500

    refactor mapper code

--- ipss.plugin.core/src/org/interpss/mapper/bean/aclf/AclfNet2ResultBeanMapper.java
+++ ipss.plugin.core/src/org/interpss/mapper/bean/aclf/AclfNet2ResultBeanMapper.java
@@ -25,23 +25,17 @@
 package org.interpss.mapper.bean.aclf;
 
 import org.apache.commons.math3.complex.Complex;
-import org.interpss.datamodel.bean.BaseBranchBean;
 import org.interpss.datamodel.bean.aclf.AclfBranchResultBean;
 import org.interpss.datamodel.bean.aclf.AclfBusBean;
 import org.interpss.datamodel.bean.aclf.AclfNetResultBean;
-import org.interpss.datamodel.bean.datatype.BranchValueBean;
 import org.interpss.datamodel.bean.datatype.ComplexBean;
 import org.interpss.datamodel.bean.datatype.MismatchResultBean;
 import org.interpss.numeric.datatype.Unit.UnitType;
-import org.interpss.numeric.util.Number2String;
 
 import com.interpss.common.exp.InterpssException;
-import com.interpss.common.mapper.AbstractMapper;
 import com.interpss.core.aclf.AclfBranch;
-import com.interpss.core.aclf.AclfBranchCode;
 import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
-import com.interpss.core.aclf.adpter.AclfXformer;
 import com.interpss.core.algo.AclfMethod;
 import com.interpss.core.datatype.Mismatch;
 
@@ -50,7 +44,7 @@ import com.interpss.core.datatype.Mismatch;
  * 
  * @author mzhou
  */
-public class AclfNet2ResultBeanMapper extends AbstractMapper<AclfNetwork, AclfNetResultBean> {
+public class AclfNet2ResultBeanMapper extends BaseAclfNet2BeanMapper<AclfNetResultBean> {
 	/**
 	 * constructor
 	 */
@@ -80,7 +74,6 @@ public class AclfNet2ResultBeanMapper extends AbstractMapper<AclfNetwork, AclfNe
 	@Override public boolean map2Model(AclfNetwork aclfNet, AclfNetResultBean aclfResult) {
 		boolean noError = true;
 		
-		aclfResult.base_kva = aclfNet.getBaseKva();
 		aclfResult.lf_converge = aclfNet.isLfConverged();
 		
 		MismatchResultBean misBean = new MismatchResultBean();
@@ -97,8 +90,7 @@ public class AclfNet2ResultBeanMapper extends AbstractMapper<AclfNetwork, AclfNe
 		aclfResult.load = new ComplexBean(format(load));
 		aclfResult.loss = new ComplexBean(format(loss));
 		
-		/*BaseNetBeanMapper mapper = new BaseNetBeanMapper();
-		mapper.map2Model(aclfNet, aclfResult);	*/	
+		aclfResult.base_kva = aclfNet.getBaseKva();			
 		
 		for (AclfBus bus : aclfNet.getBusList()) {
 			AclfBusBean bean = new AclfBusBean();
@@ -111,88 +103,13 @@ public class AclfNet2ResultBeanMapper extends AbstractMapper<AclfNetwork, AclfNe
 			aclfResult.branch_list.add(bean);
 			mapBaseBranch(branch, bean);
 		}
-		
 
 		return noError;
 	}	
 	
-	private void mapBaseBus(AclfBus bus, AclfBusBean bean) {
-		bean.number = bus.getNumber();
-		bean.id = bus.getId();
-		bean.name = bus.getName();
-		bean.status = 1;
-		boolean status = bus.isActive();
-		if(!status)
-			bean.status = 0;
-		bean.base_v = bus.getBaseVoltage()/1000;
-		bean.v_mag = format(bus.getVoltageMag());
-		bean.v_ang = format(bus.getVoltageAng(UnitType.Deg));
-		bean.vmax = format(bus.getVLimit().getMax()) == 0? bean.vmax : format(bus.getVLimit().getMax());
-		bean.vmin = format(bus.getVLimit().getMin()) == 0? bean.vmin : format(bus.getVLimit().getMin());
-
-		bean.gen_code = bus.isGenPQ() ? AclfBusBean.GenCode.PQ :
-			(bus.isGenPV() ? AclfBusBean.GenCode.PV : 
-				(bus.isSwing() ? AclfBusBean.GenCode.Swing : AclfBusBean.GenCode.NonGen));
-				
-		Complex gen = bus.getGenResults();
-		bean.gen = new ComplexBean(format(gen));
-
-		bean.load_code = bus.isConstPLoad() ? AclfBusBean.LoadCode.ConstP :
-			(bus.isConstZLoad() ? AclfBusBean.LoadCode.ConstZ : 
-				(bus.isConstILoad() ? AclfBusBean.LoadCode.ConstI : AclfBusBean.LoadCode.NonLoad));
-
-		Complex load = bus.getLoadResults();
-		bean.load = new ComplexBean(format(load));
+	protected void mapBaseBranch(AclfBranch branch, AclfBranchResultBean bean) {
+		super.mapBaseBranch(branch, bean);
 		
-		Complex sh = bus.getShuntY();
-		bean.shunt = new ComplexBean(format(sh));
-		
-		bean.area = (long) 1;
-		bean.zone = (long) 1;
-		if(bus.getArea() !=null)
-		bean.area = bus.getArea().getNumber();
-		if(bus.getZone() != null)
-		bean.zone = bus.getZone().getNumber();
-		
-	}
-	
-	private void mapBaseBranch(AclfBranch branch, AclfBranchResultBean bean) {
-		bean.id = branch.getId();
-		bean.name = branch.getName();
-		bean.f_id = branch.getFromBus().getId();
-		bean.f_num = branch.getFromBus().getNumber();
-		bean.t_id = branch.getToBus().getId();
-		bean.t_num = branch.getToBus().getNumber();		
-		bean.cir_id = branch.getCircuitNumber();
-		bean.f_name = branch.getFromBus().getName();
-		bean.t_name = branch.getToBus().getName();
-		
-		bean.status = branch.isActive()? 1 : 0; 		
-		
-		bean.bra_code = branch.isLine() ? BaseBranchBean.BranchCode.Line :
-			(branch.isXfr() ? BaseBranchBean.BranchCode.Xfr : 
-			(branch.isPSXfr() ? BaseBranchBean.BranchCode.PsXfr:BaseBranchBean.BranchCode.ZBR ));
-		
-		Complex z = branch.getZ();
-		bean.z = new ComplexBean(z);
-		bean.shunt_y = new ComplexBean(format(new Complex(0, 0)));	
-		bean.ratio = new BranchValueBean(1.0,1.0);		
-		if (branch.getBranchCode() == AclfBranchCode.LINE) {
-			if (branch.getHShuntY() != null)				
-				bean.shunt_y = new ComplexBean(format(new Complex(0, branch.getHShuntY().getImaginary()*2)));				
-				
-		}
-		else if (branch.getBranchCode() == AclfBranchCode.XFORMER ||
-				branch.getBranchCode() == AclfBranchCode.PS_XFORMER){
-			AclfXformer xfr = branch.toXfr();			
-			bean.ratio.f = xfr.getFromTurnRatio();
-			bean.ratio.t = xfr.getToTurnRatio();			
-		}
-		
-		bean.mvaRatingA = branch.getRatingMva1();
-		bean.mvaRatingB = branch.getRatingMva2();
-		bean.mvaRatingC = branch.getRatingMva3();
-				
 		Complex flow = branch.powerFrom2To();
 		bean.flow_f2t = new ComplexBean(format(flow));
 
@@ -204,18 +121,4 @@ public class AclfNet2ResultBeanMapper extends AbstractMapper<AclfNetwork, AclfNe
 		
 		bean.cur = format2(branch.current(UnitType.Amp));
 	}	
-	
-	private Complex format(Complex x) {
-		return new Complex(new Double(Number2String.toStr(x.getReal())).doubleValue(), 
-				           new Double(Number2String.toStr(x.getImaginary())).doubleValue());
-	}
-
-	private double format(double x) {
-		return new Double(Number2String.toStr(x)).doubleValue();
-	}
-
-	private double format2(double x) {
-		return new Double(Number2String.toStr(x, "#0.0#")).doubleValue();
-	}
-	
 }
---
+++ ipss.plugin.core/src/org/interpss/mapper/bean/aclf/BaseAclfNet2BeanMapper.java
@@ -0,0 +1,143 @@
+/*
+ * @(#) BaseAclfNet2BeanMapper.java   
+ *
+ * Copyright (C) 2008-2013 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 01/15/2013
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
+package org.interpss.mapper.bean.aclf;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.datamodel.bean.BaseBranchBean;
+import org.interpss.datamodel.bean.aclf.AclfBranchBean;
+import org.interpss.datamodel.bean.aclf.AclfBusBean;
+import org.interpss.datamodel.bean.datatype.BranchValueBean;
+import org.interpss.datamodel.bean.datatype.ComplexBean;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.interpss.numeric.util.Number2String;
+
+import com.interpss.common.mapper.AbstractMapper;
+import com.interpss.core.aclf.AclfBranch;
+import com.interpss.core.aclf.AclfBranchCode;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.adpter.AclfXformer;
+
+/**
+ * base mapper implementation to map AclfNetwork object to BaseNetBean (AclfNetBean and AclfNetResultBean)
+ * 
+ * 
+ */
+public abstract class BaseAclfNet2BeanMapper<TBean> extends AbstractMapper<AclfNetwork, TBean> {
+	
+	protected void mapBaseBus(AclfBus bus, AclfBusBean bean) {
+		bean.number = bus.getNumber();
+		bean.id = bus.getId();
+		bean.name = bus.getName();
+		boolean status = bus.isActive();
+		bean.status = 1;
+		if(!status)
+			bean.status = 0;
+		bean.base_v = bus.getBaseVoltage()/1000;
+		bean.v_mag = format(bus.getVoltageMag());
+		bean.v_ang = format(bus.getVoltageAng(UnitType.Deg));
+		bean.vmax = format(bus.getVLimit().getMax()) == 0? bean.vmax : format(bus.getVLimit().getMax());
+		bean.vmin = format(bus.getVLimit().getMin()) == 0? bean.vmin : format(bus.getVLimit().getMin());
+
+		bean.gen_code = bus.isGenPQ() || !bus.isGen() ? AclfBusBean.GenCode.PQ :
+			(bus.isGenPV() ? AclfBusBean.GenCode.PV : 
+				(bus.isSwing()? AclfBusBean.GenCode.Swing : AclfBusBean.GenCode.NonGen));
+				
+		Complex gen = bus.getGenResults();
+		bean.gen = new ComplexBean(format(gen));
+
+		bean.load_code = bus.isConstPLoad() ? AclfBusBean.LoadCode.ConstP :
+			(bus.isConstZLoad() ? AclfBusBean.LoadCode.ConstZ : 
+				(bus.isConstILoad() ? AclfBusBean.LoadCode.ConstI : AclfBusBean.LoadCode.NonLoad));
+
+		Complex load = bus.getLoadResults();
+		bean.load = new ComplexBean(format(load));
+		
+		Complex sh = bus.getShuntY();
+		bean.shunt = new ComplexBean(format(sh));
+		
+		bean.area = (long) 1;
+		bean.zone = (long) 1;
+		if(bus.getArea() !=null)
+		bean.area = bus.getArea().getNumber();
+		if(bus.getZone() != null)
+		bean.zone = bus.getZone().getNumber();
+		
+	}
+	
+	protected void mapBaseBranch(AclfBranch branch, AclfBranchBean bean) {
+		bean.id = branch.getId();
+		bean.name = branch.getName();
+		bean.f_id = branch.getFromBus().getId();
+		bean.f_num = branch.getFromBus().getNumber();
+		bean.t_id = branch.getToBus().getId();
+		bean.t_num = branch.getToBus().getNumber();		
+		bean.cir_id = branch.getCircuitNumber();
+		bean.f_name = branch.getFromBus().getName();
+		bean.t_name = branch.getToBus().getName();
+		
+		bean.status = branch.isActive()? 1 : 0; 		
+		
+		
+		bean.bra_code = branch.isLine() ? BaseBranchBean.BranchCode.Line :
+			(branch.isXfr() ? BaseBranchBean.BranchCode.Xfr : 
+			(branch.isPSXfr() ? BaseBranchBean.BranchCode.PsXfr:BaseBranchBean.BranchCode.ZBR ));
+		
+		Complex z = branch.getZ();
+		bean.z = new ComplexBean(z);
+		bean.shunt_y = new ComplexBean(format(new Complex(0, 0)));	
+		bean.ratio = new BranchValueBean(1.0,1.0);		
+		if (branch.getBranchCode() == AclfBranchCode.LINE ||
+				branch.getBranchCode() == AclfBranchCode.ZBR) {
+			if (branch.getHShuntY() != null)				
+				bean.shunt_y = new ComplexBean(format(new Complex(0, branch.getHShuntY().getImaginary()*2)));				
+				
+		}
+		else if (branch.getBranchCode() == AclfBranchCode.XFORMER ||
+				branch.getBranchCode() == AclfBranchCode.PS_XFORMER){
+			AclfXformer xfr = branch.toXfr();			
+			bean.ratio.f = xfr.getFromTurnRatio();
+			bean.ratio.t = xfr.getToTurnRatio();			
+		}
+		
+		bean.mvaRatingA = branch.getRatingMva1();
+		bean.mvaRatingB = branch.getRatingMva2();
+		bean.mvaRatingC = branch.getRatingMva3();			
+		
+	}	
+	
+	protected Complex format(Complex x) {
+		return new Complex(new Double(Number2String.toStr(x.getReal())).doubleValue(), 
+				           new Double(Number2String.toStr(x.getImaginary())).doubleValue());
+	}
+
+	protected double format(double x) {
+		return new Double(Number2String.toStr(x)).doubleValue();
+	}
+
+	protected double format2(double x) {
+		return new Double(Number2String.toStr(x, "#0.0#")).doubleValue();
+	}	
+}
