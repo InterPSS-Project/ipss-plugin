b911859e8dbb251e0b122ab30a5f2b513312030d
ezzhou <mike@interpss.org>
2013-09-15 18:52:21 -0500

    updated the AclfBeanMapperTest

--- ipss.test.plugin.core/src/org/interpss/plugin/beanModel/AclfBeanMapperTestCases.java
+++ ipss.test.plugin.core/src/org/interpss/plugin/beanModel/AclfBeanMapperTestCases.java
@@ -28,13 +28,16 @@ import static org.interpss.CorePluginFunction.AclfResultBusStyle;
 import static org.junit.Assert.assertTrue;
 
 import org.interpss.CorePluginTestSetup;
+import org.interpss.datamodel.bean.BaseBranchBean.BranchCode;
+import org.interpss.datamodel.bean.aclf.AclfBranchResultBean;
+import org.interpss.datamodel.bean.aclf.AclfBusBean;
 import org.interpss.datamodel.bean.aclf.AclfNetBean;
 import org.interpss.datamodel.bean.aclf.AclfNetResultBean;
-import org.interpss.display.AclfOutFunc;
 import org.interpss.mapper.bean.aclf.AclfBean2NetMapper;
 import org.interpss.mapper.bean.aclf.AclfNet2BeanMapper;
 import org.interpss.mapper.bean.aclf.AclfNet2ResultBeanMapper;
 import org.interpss.numeric.datatype.Unit.UnitType;
+import org.interpss.numeric.util.NumericUtil;
 import org.junit.Test;
 
 import com.interpss.CoreObjectFactory;
@@ -44,6 +47,17 @@ import com.interpss.core.aclf.adpter.AclfSwingBus;
 import com.interpss.simu.util.sample.SampleCases;
 
 public class AclfBeanMapperTestCases extends CorePluginTestSetup {
+	//@Test
+	public void testCase() throws Exception {
+  		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
+		SampleCases.load_LF_5BusSystem(net);
+		
+	  	net.accept(CoreObjectFactory.createLfAlgoVisitor());
+  		//System.out.println(net.net2String());
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet));
+  		System.out.println(AclfResultBusStyle.f(net));
+	}
+	
 	@Test
 	public void testCase1() throws Exception {
   		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
@@ -60,7 +74,7 @@ public class AclfBeanMapperTestCases extends CorePluginTestSetup {
 	  	aclfNet.accept(CoreObjectFactory.createLfAlgoVisitor());
   		//System.out.println(net.net2String());
 		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet));
-  		System.out.println(AclfResultBusStyle.f(aclfNet));
+  		//System.out.println(AclfResultBusStyle.f(aclfNet));
 		
   		assertTrue(aclfNet.isLfConverged());	
   		
@@ -72,6 +86,8 @@ public class AclfBeanMapperTestCases extends CorePluginTestSetup {
 		
 		AclfNetResultBean aclfResult = new AclfNet2ResultBeanMapper()
 				.map2Model(aclfNet);
+		//System.out.println(new Gson().toJson(aclfResult));
+		
 /*		
 ------------------------------------------------------------------------------------------------------------------------------------------
  Bus ID             Bus Voltage         Generation           Load             To             Branch P+jQ          Xfr Ratio   PS-Xfr Ang
@@ -89,7 +105,81 @@ public class AclfBeanMapperTestCases extends CorePluginTestSetup {
 5              4.000  1.0500   0.0   257.94   229.94     0.00     0.00  3              257.94   229.94  45.239          1.05
 ------------------------------------------------------------------------------------------------------------------------------------------	
 */		
-		//System.out.println(new Gson().toJson(aclfResult));
+		assertTrue(NumericUtil.equals(aclfResult.gen.re, (500.0+257.94)*0.01, 0.0001));
+		//assertTrue(NumericUtil.equals(aclfResult.gen.im, (181.31+229.94)*0.01, 0.0001));
+		
+		assertTrue(NumericUtil.equals(aclfResult.load.re, (160.0+200.0+370.0)*0.01, 0.0001));
+		assertTrue(NumericUtil.equals(aclfResult.load.im, (80.0+100.0+130.0)*0.01, 0.0001));
+
+		assertTrue(NumericUtil.equals(aclfResult.loss.re, ((500.0+257.94)-(160.0+200.0+370.0))*0.01, 0.0001));
+		//assertTrue(NumericUtil.equals(aclfResult.loss.im, (80.0+100.0+130.0)*0.01, 0.0001));
+
+		for (AclfBusBean bus : aclfResult.bus_list) {
+			if (bus.id.equals("5")) {
+				assertTrue(NumericUtil.equals(bus.v_mag, 1.0500, 0.0001));
+				assertTrue(NumericUtil.equals(bus.v_ang, 0.0, 0.0001));
+				assertTrue(bus.gen_code == AclfBusBean.GenCode.Swing);
+				//bus.gen;
+				assertTrue(NumericUtil.equals(bus.gen.re, 2.5794, 0.0001));
+				assertTrue(NumericUtil.equals(bus.gen.im, 2.2994, 0.0001));
+				assertTrue(bus.load_code == AclfBusBean.LoadCode.NonLoad);
+				//bus.load;
+				assertTrue(NumericUtil.equals(bus.load.re, 0.0, 0.0001));
+				assertTrue(NumericUtil.equals(bus.load.im, 0.0, 0.0001));
+			}
+			else if (bus.id.equals("4")) {
+				assertTrue(NumericUtil.equals(bus.v_mag, 1.0500, 0.0001));
+				assertTrue(NumericUtil.equals(bus.v_ang, 21.8, 0.1));
+				assertTrue(bus.gen_code == AclfBusBean.GenCode.PV);
+				//bus.gen;
+				assertTrue(NumericUtil.equals(bus.gen.re, 5.0, 0.0001));
+				assertTrue(NumericUtil.equals(bus.gen.im, 1.8131, 0.0001));
+				assertTrue(bus.load_code == AclfBusBean.LoadCode.NonLoad);
+				//bus.load;
+				assertTrue(NumericUtil.equals(bus.load.re, 0.0, 0.0001));
+				assertTrue(NumericUtil.equals(bus.load.im, 0.0, 0.0001));
+			}
+			else if (bus.id.equals("1")) {
+				assertTrue(NumericUtil.equals(bus.v_mag, 0.8622, 0.0001));
+				assertTrue(NumericUtil.equals(bus.v_ang, -4.8, 0.1));
+				assertTrue(bus.gen_code == AclfBusBean.GenCode.NonGen);
+				//bus.gen;
+				assertTrue(NumericUtil.equals(bus.gen.re, 0.0, 0.0001));
+				assertTrue(NumericUtil.equals(bus.gen.im, 0.0, 0.0001));
+				assertTrue(bus.load_code == AclfBusBean.LoadCode.ConstP);
+				//bus.load;
+				assertTrue(NumericUtil.equals(bus.load.re, 1.6, 0.0001));
+				assertTrue(NumericUtil.equals(bus.load.im, 0.8, 0.0001));			}
+		}
+		
+		for (AclfBranchResultBean bra : aclfResult.branch_list) {
+			if (bra.id.equals("1->2(1)")) {
+				assertTrue(bra.bra_code == BranchCode.Line);
+				/*
+				bra.flow_f2t;  -146.62   -40.91   7.387
+				bra.flow_t2f;   158.45    67.26   7.387
+				bra.cur;
+				*/
+				assertTrue(NumericUtil.equals(bra.flow_f2t.re, -1.4662, 0.0001));
+				assertTrue(NumericUtil.equals(bra.flow_f2t.im, -0.4091, 0.0001));
+				assertTrue(NumericUtil.equals(bra.flow_t2f.re,  1.5845, 0.0001));
+				assertTrue(NumericUtil.equals(bra.flow_t2f.im,  0.6726, 0.0001));
+				assertTrue(NumericUtil.equals(bra.cur,  7387, 1.0));
+			}
+			else if (bra.id.equals("4->2(1)")) {
+				assertTrue(bra.bra_code == BranchCode.Xfr);
+				/*
+				bra.flow_f2t;  500.00   181.31   20.183
+				bra.flow_t2f;  -500.00  -142.82  20.183
+				bra.cur;
+				*/
+				assertTrue(NumericUtil.equals(bra.flow_f2t.re,  5.0, 0.0001));
+				assertTrue(NumericUtil.equals(bra.flow_f2t.im,  1.8131, 0.0001));
+				assertTrue(NumericUtil.equals(bra.flow_t2f.re,  -5.0, 0.0001));
+				assertTrue(NumericUtil.equals(bra.flow_t2f.im,  -1.4282, 0.0001));
+				assertTrue(NumericUtil.equals(bra.cur,  20183, 1.0));
+			}
+		}
 	}
 	
 	@Test
