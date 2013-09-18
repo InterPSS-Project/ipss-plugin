d6b41292dd47ac6f9b8e28ffb20030a57a46beb8
ezzhou <mike@interpss.org>
2013-09-17 23:02:37 -0500

    added a native simulator V32 test case native simulator_IEEE9Bus_Test.java

---
+++ ipss.test.plugin.core/src/org/interpss/core/adapter/psse/PSSE_IEEE9Bus_Test.java
@@ -0,0 +1,143 @@
+ /*
+  * @(#)IEEE9Bus_Test.java   
+  *
+  * Copyright (C) 2008 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Stephen Hou
+  * @Version 1.0
+  * @Date 02/01/2008
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.adapter.psse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.math3.complex.Complex;
+import org.ieee.odm.adapter.IODMAdapter;
+import org.ieee.odm.adapter.psse.PSSEAdapter;
+import org.ieee.odm.model.aclf.AclfModelParser;
+import org.interpss.CorePluginTestSetup;
+import org.interpss.datamodel.bean.aclf.AclfNetBean;
+import org.interpss.mapper.bean.aclf.AclfNet2BeanMapper;
+import org.interpss.mapper.odm.ODMAclfParserMapper;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.junit.Test;
+
+import com.interpss.CoreObjectFactory;
+import com.interpss.SimuObjectFactory;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.adpter.AclfSwingBus;
+import com.interpss.core.algo.AclfMethod;
+import com.interpss.core.algo.LoadflowAlgorithm;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+
+public class PSSE_IEEE9Bus_Test extends CorePluginTestSetup { 
+	@Test
+	public void compare() throws Exception {
+		IODMAdapter adapter = new PSSEAdapter(PSSEAdapter.PsseVersion.PSSE_30);
+		assertTrue(adapter.parseInputFile("testdata/psse/v30/IEEE9Bus/ieee9.raw"));
+		
+		AclfModelParser parser = (AclfModelParser)adapter.getModel();
+		//parser.stdout();
+		
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_NETWORK);
+		if (!new ODMAclfParserMapper()
+					.map2Model(parser, simuCtx)) {
+  	  		System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+  	  		return;
+		}		
+		AclfNetwork net = simuCtx.getAclfNet();
+		AclfNetBean netBean = new AclfNet2BeanMapper().map2Model(net);
+		
+		IODMAdapter adapter1 = new PSSEAdapter(PSSEAdapter.PsseVersion.PSSE_32);
+		assertTrue(adapter1.parseInputFile("testdata/psse/v32/ieee9_v32.raw"));
+		
+		AclfModelParser parser1 = (AclfModelParser)adapter1.getModel();
+		//parser.stdout();
+		
+		SimuContext simuCtx1 = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_NETWORK);
+		if (!new ODMAclfParserMapper()
+					.map2Model(parser1, simuCtx1)) {
+  	  		System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+  	  		return;
+		}		
+		AclfNetwork net1 = simuCtx1.getAclfNet();
+		AclfNetBean netBean1 = new AclfNet2BeanMapper().map2Model(net1);
+		
+		netBean.compareTo(netBean1);
+	}
+	
+	@Test
+	public void testV30() throws Exception {
+		IODMAdapter adapter = new PSSEAdapter(PSSEAdapter.PsseVersion.PSSE_30);
+		assertTrue(adapter.parseInputFile("testdata/psse/v30/IEEE9Bus/ieee9.raw"));
+		
+		AclfModelParser parser = (AclfModelParser)adapter.getModel();
+		//parser.stdout();
+		
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_NETWORK);
+		if (!new ODMAclfParserMapper()
+					.map2Model(parser, simuCtx)) {
+  	  		System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+  	  		return;
+		}		
+		AclfNetwork net = simuCtx.getAclfNet();
+
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.setLfMethod(AclfMethod.PQ);
+	  	algo.loadflow();
+  		//System.out.println(net.net2String());
+
+	  	AclfBus swingBus = net.getBus("Bus1");
+	  	AclfSwingBus swing = swingBus.toSwingBus();
+  		Complex p = swing.getGenResults(UnitType.PU);
+  		assertTrue(Math.abs(p.getReal()-0.71646)<0.00001);
+  		assertTrue(Math.abs(p.getImaginary()-0.27107)<0.00001);
+	}
+
+	@Test
+	public void testV32() throws Exception {
+		IODMAdapter adapter = new PSSEAdapter(PSSEAdapter.PsseVersion.PSSE_32);
+		assertTrue(adapter.parseInputFile("testdata/psse/v32/ieee9_v32.raw"));
+		
+		AclfModelParser parser = (AclfModelParser)adapter.getModel();
+		//parser.stdout();
+		
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_NETWORK);
+		if (!new ODMAclfParserMapper()
+					.map2Model(parser, simuCtx)) {
+  	  		System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+  	  		return;
+		}		
+		AclfNetwork net = simuCtx.getAclfNet();
+
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.setLfMethod(AclfMethod.PQ);
+	  	algo.loadflow();
+  		//System.out.println(net.net2String());
+
+	  	AclfBus swingBus = net.getBus("Bus1");
+	  	AclfSwingBus swing = swingBus.toSwingBus();
+  		Complex p = swing.getGenResults(UnitType.PU);
+  		assertTrue(Math.abs(p.getReal()-0.71646)<0.00001);
+  		assertTrue(Math.abs(p.getImaginary()-0.27107)<0.00001);
+	}
+}
+
+
