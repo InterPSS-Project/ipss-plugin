c63e354a21f22c16b3f73d6cf9cc4436093f3d7c
ezzhou <mike@interpss.org>
2013-12-18 18:03:48 -0500

    completed RemoteQBus cache implementation

--- ipss.test.plugin.core/src/org/interpss/core/cache/TestSeeder.java
+++ ipss.test.plugin.core/src/org/interpss/core/cache/TestSeeder.java
@@ -19,6 +19,7 @@ import com.interpss.common.exp.IpssCacheException;
 import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.aclf.adpter.AclfSwingBus;
+import com.interpss.simu.util.sample.SampleCases;
 
 public class TestSeeder {
 	public static void main(String[] args) throws IpssCacheException, InterpssException {
@@ -36,10 +37,7 @@ public class TestSeeder {
 	private static void seedNetwork(final HazelcastInstance client) throws IpssCacheException, InterpssException {
 		IpssCorePlugin.init();
 		
-		AclfNetwork net = CorePluginObjFactory
-				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
-				.load("testdata/adpter/ieee_format/Ieee14Bus.ieee")
-				.getAclfNet();
+  		AclfNetwork net = SampleCases.create5BusAclfReQBranchQ();
 		System.out.println(net.net2String());
 
 		AclfNetCacheWrapper cache = new AclfNetCacheWrapper(client);
@@ -48,38 +46,5 @@ public class TestSeeder {
 		 
 		AclfNetwork net1 = cache.get(key);
 		System.out.println(net1.net2String());
-		
-	  	net1.accept(CoreObjectFactory.createLfAlgoVisitor());
-  		//System.out.println(net.net2String());
-	  	
-  		assertTrue(net1.isLfConverged());		
-  		AclfSwingBus swing = net1.getBus("Bus1").toSwingBus();
-  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.32393)<0.0001);
-  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.16549)<0.0001);		
-		
-/*		
-		String id = "AclfNet12345";
-		cache.put(net, id);
-
-		AclfNetwork net1 = cache.get(id);
-		System.out.println(net1.net2String());
-*/		
-/*		
-		AclfBus bus = net.getBus("1");
-		
-		AclfBusCacheHelper busHelper = new AclfBusCacheHelper(client);
-		NetworkElementKey buskey = busHelper.put(bus, key);
-		
-		AclfBus bus1 = busHelper.get(buskey);
-		System.out.println(bus1.toString());
-
-		AclfBranch branch = net.getBranch("1->2(1)");
-		
-		AclfBranchCacheHelper braHelper = new AclfBranchCacheHelper(client);
-		NetworkElementKey brakey = braHelper.put(branch, key);
-		
-		AclfBranch bra1 = braHelper.get(brakey);
-		System.out.println(bra1.toString());
-*/			
 	}
 }
