32b2c8332eeec8079285454c657fdaf6bf00a8fe
ezzhou <mike@interpss.org>
2013-12-18 13:51:07 -0500

    Completed IEEE-14Bus sample cache implementation verification.

--- ipss.test.plugin.core/src/org/interpss/core/cache/TestSeeder.java
+++ ipss.test.plugin.core/src/org/interpss/core/cache/TestSeeder.java
@@ -3,7 +3,9 @@ package org.interpss.core.cache;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.interpss.CorePluginObjFactory;
 import org.interpss.IpssCorePlugin;
+import org.interpss.fadapter.IpssFileAdapter;
 import org.interpss.numeric.datatype.Unit.UnitType;
 
 import com.hazelcast.client.HazelcastClient;
@@ -12,14 +14,14 @@ import com.hazelcast.core.HazelcastInstance;
 import com.interpss.CoreObjectFactory;
 import com.interpss.cache.UgidGenerator;
 import com.interpss.cache.aclf.AclfNetCacheWrapper;
+import com.interpss.common.exp.InterpssException;
 import com.interpss.common.exp.IpssCacheException;
 import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.aclf.adpter.AclfSwingBus;
-import com.interpss.simu.util.sample.SampleCases;
 
 public class TestSeeder {
-	public static void main(String[] args) throws IpssCacheException {
+	public static void main(String[] args) throws IpssCacheException, InterpssException {
 		ClientConfig clientConfig = new ClientConfig();
 		
 		clientConfig.addAddress("127.0.0.1");
@@ -31,33 +33,29 @@ public class TestSeeder {
 		client.shutdown();
 	}
 	
-	private static void seedNetwork(final HazelcastInstance client) throws IpssCacheException {
+	private static void seedNetwork(final HazelcastInstance client) throws IpssCacheException, InterpssException {
 		IpssCorePlugin.init();
 		
-  		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
-		SampleCases.load_LF_5BusSystem(net);
-		//System.out.println(net.net2String());
+		AclfNetwork net = CorePluginObjFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
+				.load("testdata/adpter/ieee_format/Ieee14Bus.ieee")
+				.getAclfNet();
+		System.out.println(net.net2String());
 
 		AclfNetCacheWrapper cache = new AclfNetCacheWrapper(client);
 
 		long key = cache.put(net);
 		 
 		AclfNetwork net1 = cache.get(key);
-		//System.out.println(net1.net2String());
+		System.out.println(net1.net2String());
 		
-	  	net.accept(CoreObjectFactory.createLfAlgoVisitor());
+	  	net1.accept(CoreObjectFactory.createLfAlgoVisitor());
   		//System.out.println(net.net2String());
 	  	
-  		assertTrue(net.isLfConverged());
-  		
-  		//System.out.println("Area1 output power: " + net.areaOutputPower(1, UnitType.PU));
-  		assertEquals(true, Math.abs(net.areaOutputPower(1, UnitType.PU)-1.28164)<0.0001);
-
-  		AclfBus swingBus = (AclfBus)net.getBus("5");
-		AclfSwingBus swing = swingBus.toSwingBus();
-  		//System.out.println(swing.getGenResults(UnitType.PU, net.getBaseKva()));
-		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.57943)<0.0001);
-		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()-2.2994)<0.0001);		
+  		assertTrue(net1.isLfConverged());		
+  		AclfSwingBus swing = net1.getBus("Bus1").toSwingBus();
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.32393)<0.0001);
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.16549)<0.0001);		
 		
 /*		
 		String id = "AclfNet12345";
