10496d89e161f46e4c8faf63b6e5caf59000d2ca
Tony Huang <eetonyhuang@gmail.com>
2014-01-09 11:56:28 -0700

    update Dstab Test case

--- ipss.test.plugin.core/src/org/interpss/core/dstab/DStab_IEEE9Bus_Test.java
+++ ipss.test.plugin.core/src/org/interpss/core/dstab/DStab_IEEE9Bus_Test.java
@@ -37,7 +37,7 @@ import com.interpss.simu.SimuCtxType;
 
 public class DStab_IEEE9Bus_Test extends DStabTestSetupBase{
 	
-	@Test
+	//@Test
 	public void test_IEEE9Bus_Dstab(){
 		IpssCorePlugin.init();
 		PSSEAdapter adapter = new PSSEAdapter(PsseVersion.PSSE_30);
@@ -62,9 +62,6 @@ public class DStab_IEEE9Bus_Test extends DStabTestSetupBase{
 		
 	    DStabilityNetwork dsNet =simuCtx.getDStabilityNet();
 	    //System.out.println(dsNet.net2String());
-//	    if(!dsNet.isSaturatedMachineParameter()){
-//	    	dsNet.setSaturatedMachineParameter(true);
-//	    }
 	    
 		DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
 		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
@@ -89,8 +86,8 @@ public class DStab_IEEE9Bus_Test extends DStabTestSetupBase{
 				0.001,                                      // time steps for recording 
 				10);                                      // total points to record
 		ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
-				StateVarRecType.BusState,    // record type
-				DStabOutSymbol.OUT_SYMBOL_BUS_VMAG,       // state variable name
+				StateVarRecType.MachineState,    // record type
+				DStabOutSymbol.OUT_SYMBOL_MACH_Efd,       // state variable name
 				0.001,                                      // time steps for recording 
 				10);
 		// set the output handler
@@ -100,19 +97,9 @@ public class DStab_IEEE9Bus_Test extends DStabTestSetupBase{
 		if (dstabAlgo.initialization()) {
 			//System.out.println(dsNet.net2String());
 			System.out.println("Running DStab simulation ...");
-			//dstabAlgo.performSimulation();
+			dstabAlgo.performSimulation();
 			//dstabAlgo.performOneStepSimulation();
-			//System.out.println("Time:"+ dstabAlgo.getInstantSimuTime());
-			
-			System.out.println("Volt@Bus1 : "+dsNet.getDStabBus("Bus1").getVoltageMag()+" , "
-			                    +dsNet.getDStabBus("Bus1").getVoltageAng(UnitType.Deg));
-			for(int i = 0;i<10; i++)
-			//dstabAlgo.performOneStepSimulation();
-			//System.out.println("Time:"+ dstabAlgo.getInstantSimuTime());
-			System.out.println("Volt@Bus2 : "+dsNet.getDStabBus("Bus2").getVoltageMag()+" , "
-                    +dsNet.getDStabBus("Bus2").getVoltageAng(UnitType.Deg));
-			System.out.println("Volt@Bus1 : "+dsNet.getDStabBus("Bus1").getVoltageMag()+" , "
-                    +dsNet.getDStabBus("Bus1").getVoltageAng(UnitType.Deg));
+
 		}
 		
 		// output recorded simulation results
@@ -131,12 +118,120 @@ public class DStab_IEEE9Bus_Test extends DStabTestSetupBase{
 			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
 		}
 		list = ssRecorder.getMachineRecords(
-				"Bus2-mach1", StateVarRecType.BusState, DStabOutSymbol.OUT_SYMBOL_BUS_VMAG);
-		System.out.println("\n\n Bus2 voltage mag");
+				"Bus2-mach1", StateVarRecType.MachineState, DStabOutSymbol.OUT_SYMBOL_MACH_Efd);
+		System.out.println("\n\n Bus2 Machine Efd");
 		for (StateRecord rec : list) {
 			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
 		}
 		
+		
+		/*
+		 *  Bus2 Machine Anagle
+				0.0000, 57.56288
+				0.0010, 57.56288
+				0.0020, 57.56288
+				0.0030, 57.56288
+				0.0040, 57.56288
+				0.0050, 57.56288
+				0.0060, 57.56288
+				0.0070, 57.56288
+				0.0080, 57.56288
+				0.0090, 57.56288
+		 */
+		
+		/*
+		 *  Bus2 Machine PM
+			0.0000, 1.6300
+			0.0010, 1.6300
+			0.0020, 1.6300
+			0.0030, 1.6300
+			0.0040, 1.6300
+			0.0050, 1.6300
+			0.0060, 1.6300
+			0.0070, 1.6300
+			0.0080, 1.6300
+			0.0090, 1.6300
+		 */
+		
+		/*
+		 *  Bus2 Machine Efd
+			0.0000, 1.78898
+			0.0010, 1.78898
+			0.0020, 1.78898
+			0.0030, 1.78898
+			0.0040, 1.78898
+			0.0050, 1.78898
+			0.0060, 1.78898
+			0.0070, 1.78898
+			0.0080, 1.78898
+			0.0090, 1.78898
+		 */
+	}
+	
+	@Test
+	public void IEEE9_Dstab_benchMark(){
+		IpssCorePlugin.init();
+		PSSEAdapter adapter = new PSSEAdapter(PsseVersion.PSSE_30);
+		assertTrue(adapter.parseInputFile(NetType.DStabNet, new String[]{
+				"testData/adpter/psse/v30/IEEE9Bus/ieee9.raw",
+				"testData/adpter/psse/v30/IEEE9Bus/ieee9.seq",
+				"testData/adpter/psse/v30/IEEE9Bus/ieee9_dyn_onlyGen.dyr"
+		}));
+		DStabModelParser parser =(DStabModelParser) adapter.getModel();
+		
+		System.out.println(parser.toXmlDoc());
+
+		
+		
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET);
+		if (!new ODMDStabParserMapper(msg)
+					.map2Model(parser, simuCtx)) {
+			System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+			return;
+		}
+		
+		
+	    DStabilityNetwork dsNet =simuCtx.getDStabilityNet();
+	    //System.out.println(dsNet.net2String());
+   
+		DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
+		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
+		assertTrue(aclfAlgo.loadflow());
+		
+		
+		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
+		dstabAlgo.setSimuStepSec(0.001);
+		dstabAlgo.setTotalSimuTimeSec(0.01);
+		dstabAlgo.setRefMachine(dsNet.getMachine("Bus1-mach1"));
+		
+		double[] timePoints   = {0.0,    0.004,    0.007,    0.009},
+	      			 machPmPoints = {1.6300, 1.6300,   1.6300,   1.6300},
+	      			 machAngPoints  = {57.56288, 57.56288,  57.56288,   57.56288},
+	      			 machEfdPoints  = {1.78898, 1.78898,   1.78898,   1.78898};
+		
+		StateVariableRecorder stateTestRecorder = new StateVariableRecorder(0.0001);
+		stateTestRecorder.addTestRecords("Bus2-mach1", MachineState, 
+					DStabOutSymbol.OUT_SYMBOL_MACH_PM, timePoints, machPmPoints);
+		stateTestRecorder.addTestRecords("Bus2-mach1", MachineState, 
+					DStabOutSymbol.OUT_SYMBOL_MACH_ANG, timePoints, machAngPoints);
+		stateTestRecorder.addTestRecords("Bus2-mach1", MachineState, 
+				DStabOutSymbol.OUT_SYMBOL_MACH_Efd, timePoints, machEfdPoints);
+		dstabAlgo.setSimuOutputHandler(stateTestRecorder);
+			
+
+		if (dstabAlgo.initialization()) {
+			//System.out.println(simuCtx.getDStabilityNet().net2String());
+
+			System.out.println("Running DStab simulation ...");
+			assertTrue(dstabAlgo.performSimulation());
+		}
+			
+		assertTrue(stateTestRecorder.diffTotal("Bus2-mach1", MachineState, 
+					DStabOutSymbol.OUT_SYMBOL_MACH_PM) < 0.0001);
+		assertTrue(stateTestRecorder.diffTotal("Bus2-mach1", MachineState, 
+					DStabOutSymbol.OUT_SYMBOL_MACH_ANG) < 0.0001);
+		assertTrue(stateTestRecorder.diffTotal("Bus2-mach1", MachineState, 
+				DStabOutSymbol.OUT_SYMBOL_MACH_Efd) < 0.0001);
 	}
 
 }
