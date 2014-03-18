745be2f6880456664ecbf8882d0f487b9a359480
Tony Huang <eetonyhuang@gmail.com>
2014-03-17 17:20:30 -0700

    IEEE 1981 and 2005 Exciter implementation

---
+++ ipss.plugin.dstab/src/org/interpss/dstab/control/cml/func/LoadCompFunction.java
@@ -0,0 +1,7 @@
+package org.interpss.dstab.control.cml.func;
+
+import com.interpss.dstab.controller.block.adapt.CMLFunctionAdapter;
+
+public class LoadCompFunction extends CMLFunctionAdapter {
+
+}
