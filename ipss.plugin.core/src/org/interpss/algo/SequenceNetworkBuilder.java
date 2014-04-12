741a02c775ce886e9bd22ea2de124c45cca113db
Tony Huang <eetonyhuang@gmail.com>
2014-04-11 20:53:52 -0700

    update after bug in the core engine is fixed

--- ipss.plugin.core/src/org/interpss/algo/SequenceNetworkBuilder.java
+++ ipss.plugin.core/src/org/interpss/algo/SequenceNetworkBuilder.java
@@ -37,7 +37,8 @@ public class SequenceNetworkBuilder {
 			return ok = false;
 		if(!setSwitchedShuntSeqData(seq))
 			return ok = false;
-		
+		//update the sequence data attribute
+		this._net.setPositiveSeqDataOnly(false);
 		return ok;
 			
 	}
