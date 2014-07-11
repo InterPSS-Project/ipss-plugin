e7d7f1131bc0998b48e88500c4b830876e38c444
ezzhou <mike@interpss.org>
2014-07-11 14:47:09 -0500

    added sparse eqn solver customization sample code

---
+++ ipss.sample/src/org/interpss/sample/sparseeqn/CustomComplexSparseEqnSolver.java
@@ -0,0 +1,32 @@
+package org.interpss.sample.sparseeqn;
+
+import org.interpss.numeric.exp.IpssNumericException;
+import org.interpss.numeric.sparse.ISparseEqnComplex;
+
+import com.interpss.core.sparse.ISparseEqnSolver;
+
+public class CustomComplexSparseEqnSolver implements ISparseEqnSolver {
+	protected boolean LUed = false;
+	
+	private ISparseEqnComplex sparseEqn;
+	
+	public CustomComplexSparseEqnSolver(ISparseEqnComplex sparseEqn) {
+		this.sparseEqn = sparseEqn;
+	}	
+	
+	@Override public void setMatrixDirty() {
+		this.LUed = false;
+	}
+
+	@Override public boolean luMatrix(double arg0) throws IpssNumericException {
+		System.out.println("Call CustomComplexSparseEqnSolver.luMatrix() ...");
+		this.LUed = true;
+		return true;
+	}
+
+	@Override public void solveEqn() throws IpssNumericException {
+		if (!this.LUed)
+			this.luMatrix(1.0e-10);
+		System.out.println("Call CustomComplexSparseEqnSolver.solveEqn() ...");
+	}
+}
---
+++ ipss.sample/src/org/interpss/sample/sparseeqn/CustomDoubleSparseEqnSolver.java
@@ -0,0 +1,34 @@
+package org.interpss.sample.sparseeqn;
+
+import org.interpss.numeric.exp.IpssNumericException;
+import org.interpss.numeric.sparse.ISparseEqnDouble;
+
+import com.interpss.core.sparse.ISparseEqnSolver;
+
+public class CustomDoubleSparseEqnSolver implements ISparseEqnSolver {
+	private boolean LUed = false;
+	
+	// sparse eqn to be solved by this solver
+	private ISparseEqnDouble sparseEqn;
+	
+	public CustomDoubleSparseEqnSolver(ISparseEqnDouble sparseEqn) {
+		this.sparseEqn = sparseEqn;
+	}
+	
+	@Override public void setMatrixDirty() {
+		this.LUed = false;
+	}
+
+	@Override public boolean luMatrix(double arg0) throws IpssNumericException {
+		System.out.println("Call CustomDoubleSparseEqnSolver.luMatrix() ...");
+		this.LUed = true;
+		return true;
+	}
+
+	@Override public void solveEqn() throws IpssNumericException {
+		if (!this.LUed)
+			this.luMatrix(1.0e-10);
+		System.out.println("Call CustomDoubleSparseEqnSolver.solveEqn() ...");
+	}
+}
+
