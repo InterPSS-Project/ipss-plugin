7c8fb29879309f85ebecb916d6419ff8f6c896e0
Tony Huang <eetonyhuang@gmail.com>
2015-04-14 17:55:23 -0700

    first commit of multiNet

---
+++ ipss.plugin.multiNet/src/org/ipss/multiNet/algo/MultiNetDStabSolverImpl.java
@@ -0,0 +1,299 @@
+package org.ipss.multiNet.algo;
+
+import static com.interpss.common.util.IpssLogger.ipssLogger;
+import static com.interpss.dstab.funcImpl.DStabFunction.BuiltBusState;
+import static com.interpss.dstab.funcImpl.DStabFunction.BuiltMachineState;
+import static com.interpss.dstab.funcImpl.DStabFunction.BuiltScriptDynamicBusDeviceState;
+
+import java.util.Hashtable;
+import java.util.List;
+
+import org.interpss.numeric.util.NumericUtil;
+
+import com.interpss.common.exp.InterpssException;
+import com.interpss.common.msg.IPSSMsgHub;
+import com.interpss.core.aclf.AclfGen;
+import com.interpss.core.net.Branch;
+import com.interpss.core.net.Bus;
+import com.interpss.dstab.DStabBranch;
+import com.interpss.dstab.DStabBus;
+import com.interpss.dstab.DStabGen;
+import com.interpss.dstab.DStabilityNetwork;
+import com.interpss.dstab.algo.DynamicSimuAlgorithm;
+import com.interpss.dstab.algo.DynamicSimuMethod;
+import com.interpss.dstab.algo.defaultImpl.DStabSolverImpl;
+import com.interpss.dstab.common.DStabSimuException;
+import com.interpss.dstab.datatype.DStabSimuEvent;
+import com.interpss.dstab.device.DynamicBranchDevice;
+import com.interpss.dstab.device.DynamicBusDevice;
+import com.interpss.dstab.mach.Machine;
+
+public class MultiNetDStabSolverImpl extends DStabSolverImpl {
+	
+	private MultiNetDStabSimuHelper multiNetSimuHelper = null;
+	private List<DStabilityNetwork> subNetList = null;
+
+	public MultiNetDStabSolverImpl(DynamicSimuAlgorithm algo, IPSSMsgHub msg,MultiNetDStabSimuHelper mNetSimuHelper) {
+		super(algo, msg);
+		this.msg = msg;
+		this.multiNetSimuHelper = mNetSimuHelper;
+		this.subNetList = this.multiNetSimuHelper.getSubNetworkProcessor().getSubNetworkList();
+	}
+	
+	@Override 
+	public boolean initialization() {
+		this.simuPercent = 0;
+		this.simuTime = 0; //required for running multiple DStab simulations in a loop
+		consoleMsg("Start multi-SubNetwork initialization ...");
+
+		if (!dstabAlgo.getNetwork().isLfConverged()) {
+			ipssLogger.severe("Error: Loadflow not converged yet!");
+			return false;
+		}
+		
+		// network initialization, initial bus sc data, transfer machine sc info to bus.
+		for(DStabilityNetwork dsNet: this.subNetList){
+			
+				if (!dsNet.initDStabNet()){
+					  ipssLogger.severe("Error: SubNetwork initialization error:"+dsNet.getId());
+					  return false;
+				}
+			
+		}
+		// output initial states
+		if (!procInitOutputEvent())
+			return false;
+
+	  	//System.out.println(net.net2String());
+		consoleMsg("Initialization finished");
+		return true;		
+	}
+	
+	/**
+	 * perform a full step of simulation by solving the differential 
+	 * eqn 
+	 * 
+	 * @param updateTime indicator to update the simuTime field
+	 */
+	@Override
+	public boolean solveDEqnStep(boolean updateTime){
+		try{
+			
+		boolean hasEvent = hasDynEvent(simuTime);
+		if(hasDynEvent(simuTime)){
+			for(DStabilityNetwork dsNet: subNetList){
+				
+				// Solve DEqn for all dynamic bus devices
+				for (DStabBus bus : dsNet.getBusList()) {
+				  //only the measurements of active buses will be output. 
+				   if(bus.isActive())
+				     output(bus, simuTime, true);
+			     }
+			}
+		}
+		
+		
+		// performing actions before solving DEqn, for example, applying dynamic event
+		beforeStep(simuTime);
+
+		
+		// solving DEqn
+		nextStep(simuTime, dstabAlgo.getSimuStepSec(), dstabAlgo.getSimuMethod());
+
+		
+		if (updateTime) {
+			    
+			// output simulation results
+			++outCnt;
+            for(DStabilityNetwork dsNet: subNetList){
+				
+				// Solve DEqn for all dynamic bus devices
+			   for (DStabBus bus : dsNet.getBusList()) {
+				   //only the measurements of active buses will be output. 
+				    if(bus.isActive())
+				         output(bus, simuTime, outCnt >= outPerSteps);
+			   }
+			}
+
+				// increase simulation time
+				simuTime += dstabAlgo.getSimuStepSec();
+
+
+
+				if (outCnt >= outPerSteps)
+					outCnt = 0;
+		}
+		// performing actions after solving QEqn
+		afterStep(simuTime);
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+
+	
+	@Override 
+	public void nextStep(double time, double dt, DynamicSimuMethod method)  throws DStabSimuException {
+		 
+		 boolean netSolConverged = true;
+		 maxIterationTimes =1;
+		 int maxOuterIterationTimes =30;
+		for(int outerIdx=0;outerIdx<maxOuterIterationTimes;outerIdx++){
+			 
+			 
+			// network initialization, initial bus sc data, transfer machine sc info to bus.
+			for(DStabilityNetwork dsNet: subNetList){
+				
+				for(int i=0;i<maxIterationTimes;i++){
+				
+					// Solve DEqn for all dynamic bus devices
+					for (Bus b : dsNet.getBusList()) {
+						if(b.isActive()){
+							DStabBus bus = (DStabBus)b;
+							for (DynamicBusDevice device : bus.getDynamicBusDeviceList()) {
+								// solve DEqn for the step. This includes all controller's nextStep() call
+								if(device.isActive()){
+									if (!device.nextStep(dt, method)) {
+										throw new DStabSimuException("Error occured, Simulation will be stopped");
+									}
+								}
+							}
+							
+							// Solve DEqn for generator 
+							if(bus.getContributeGenList().size()>0){
+								for(AclfGen gen:bus.getContributeGenList()){
+									if(gen.isActive()){
+										Machine mach = ((DStabGen)gen).getMach();
+										if(mach!=null && mach.isActive()){
+										   if (!mach.nextStep(dt, method)) {
+											  throw new DStabSimuException("Error occured when solving nextStep for mach #"+ mach.getId()+ "@ bus - "
+										                   +bus.getId()+", Simulation will be stopped!");
+										   }
+										}
+									}
+								}
+							}
+							
+							//TODO Solve DEqn for dynamic load, e.g. induction motor
+						}
+					}// bus-loop
+
+					// Solve DEqn for all dynamic branch devices
+					for (Branch b : dsNet.getBranchList()) {
+						DStabBranch branch = (DStabBranch)b;
+						for (DynamicBranchDevice device : branch.getDynamicBranchDeviceList()) {
+							// solve DEqn for the step. This includes all controller's nextStep() call
+							if (!device.nextStep(dt, method)) {
+								throw new DStabSimuException("Error occured, Simulation will be stopped");
+							}
+						}
+					}
+					
+					// solve net equation
+					if (!dsNet.solveNetEqn(false))
+						throw new DStabSimuException("Exception in dstabNet.solveNetEqn()");
+					
+					// calculate dynamic measurement signals - they are secondary signals 
+					for ( Bus busi : dsNet.getBusList() ) {
+						DStabBus bus = (DStabBus)busi;
+						if(bus.isActive()){
+							if (!bus.nextStep(dt, method)) {
+								throw new DStabSimuException("Error occured, Simulation will be stopped");
+							}
+							if(i>=1){
+								if(!NumericUtil.equals(bus.getVoltage(),voltageRecTable.get(bus.getId()),this.converge_tol))
+									netSolConverged =false;
+							}
+							voltageRecTable.put(bus.getId(), bus.getVoltage());
+						}
+					}
+					
+				
+				 
+				// update the states
+				 for (Bus b :  dsNet.getBusList()) {
+						if(b.isActive()){
+							DStabBus bus = (DStabBus)b;
+							// Solve DEqn for generator 
+							if(bus.getContributeGenList().size()>0){
+								for(AclfGen gen:bus.getContributeGenList()){
+									if(gen.isActive()){
+										Machine mach = ((DStabGen)gen).getMach();
+										if(mach!=null && mach.isActive()){
+										  mach.backUpStates();
+										}
+									}
+								}
+							}
+							
+							//TODO Solve DEqn for dynamic load, e.g. induction motor
+						}
+				}
+			    	
+			 } // for subNetwork loop
+			
+				
+
+			
+				/*
+			// check whether the network solution is converged?
+			if(i>=1 && netSolConverged) {
+				IpssLogger.getLogger().fine("SimuTime: "+dstabAlgo.getSimuTime()+"\n Network solution converges with "+(i+1)+" iterations");
+				break;
+			}
+			*/
+	
+		  } // for maxIterationTimes loop
+			//using the lastest available information to update the equivalent current injection
+			this.multiNetSimuHelper.updateBoundaryBusEquivCurrentInjection();
+			
+			
+		}// for outer loop
+	
+	}
+	
+	@Override public boolean procInitOutputEvent() {
+		try {
+			for(DStabilityNetwork dsNet: subNetList){
+				
+				// Solve DEqn for all dynamic bus devices
+				for (DStabBus bus : dsNet.getBusList()) {
+
+					if(bus.isActive()){ // only the active bus will be consider, otherwise errors will occur
+						if (bus.isMachineBus()) {
+							for(AclfGen gen: bus.getContributeGenList()){
+							//consider active or in-service machine only
+							 if(gen.isActive() && ((DStabGen)gen).getMach()!=null){
+								  Machine mach = ((DStabGen)gen).getMach();
+								  Hashtable<String, Object> states = BuiltMachineState.f(mach, 0.0, this.dstabAlgo);
+								  procOutputEvent(DStabSimuEvent.PlotStepMachineStates, states);
+								  procOutputEvent(DStabSimuEvent.TimeStepMachineStates, states);
+							 }
+						  }
+						}
+						if (bus.getScriptDynamicBusDevice() != null) {
+				            Hashtable<String, Object> states = BuiltScriptDynamicBusDeviceState.f(bus, 0.0, this.dstabAlgo);
+				            procOutputEvent(DStabSimuEvent.TimeStepScriptDynamicBusDeviceStates, states);
+							procOutputEvent(DStabSimuEvent.PlotStepScriptDynamicBusDeviceStates, states);
+						}
+						//else {
+				            Hashtable<String, Object> states = BuiltBusState.f(bus, 0.0, this.dstabAlgo);
+				            procOutputEvent(DStabSimuEvent.TimeStepBusStates, states);
+							procOutputEvent(DStabSimuEvent.PlotStepBusStates, states);
+						//}
+					 }
+			}
+		  }
+			procOutputEvent(DStabSimuEvent.EndOfSimuStep, null);
+			return true;
+		} catch (InterpssException e) {
+			ipssLogger.severe(e.toString());
+			return false;
+		}
+	}
+	
+
+
+}
---
+++ ipss.plugin.multiNet/src/org/ipss/multiNet/algo/MultiNetDynamicEventProcessor.java
@@ -0,0 +1,200 @@
+package org.ipss.multiNet.algo;
+
+import static com.interpss.common.util.IpssLogger.ipssLogger;
+
+import java.util.List;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.numeric.NumericConstant;
+import org.interpss.numeric.exp.IpssNumericException;
+
+
+
+
+
+
+
+import org.interpss.numeric.sparse.ISparseEqnComplex;
+
+import com.interpss.common.exp.InterpssRuntimeException;
+import com.interpss.common.msg.IpssMessage;
+import com.interpss.core.acsc.AcscBus;
+import com.interpss.core.acsc.SequenceCode;
+import com.interpss.core.acsc.fault.AcscBusFault;
+import com.interpss.core.acsc.fault.SimpleFaultCode;
+import com.interpss.dstab.DStabilityNetwork;
+import com.interpss.dstab.algo.defaultImpl.DynamicEventProcessor;
+import com.interpss.dstab.datatype.DStabSimuTimeEvent;
+import com.interpss.dstab.devent.DynamicEvent;
+import com.interpss.dstab.devent.DynamicEventType;
+
+public class MultiNetDynamicEventProcessor extends DynamicEventProcessor {
+	
+	
+	private List<DStabilityNetwork> subnetList = null;
+	DStabilityNetwork net = null;
+	/**
+	 * process network dynamic event
+	 * 
+	 * @param eventMsg network dynamic event
+	 * @return false if there is any issue during event handling process 
+	 */
+	@Override public boolean onMsgEventStatus(IpssMessage eventMsg) {
+		if (eventMsg instanceof DStabSimuTimeEvent) {
+			DStabSimuTimeEvent dEventMsg = (DStabSimuTimeEvent) eventMsg;
+			if (dEventMsg.getType() == DStabSimuTimeEvent.ProessDynamicEvent) {
+				
+				this.net = dEventMsg.getDStabNetData();
+				
+				double t = dEventMsg.getTime();
+				if (hasAnyEvent(t)) {
+					
+					//System.out.println("dynamic event at: "+t);
+					/*
+					 * We always start from a full Y-matrix without any fault. At
+					 * any point, if there is an event, we apply all current
+					 * active events to re-construct the Y-matrix.
+					 */
+
+					// apply those events which result in changing Y-matrix,
+					// such as turn-off a branch
+					for (DynamicEvent dEvent : net.getDynamicEventList()) {
+						if (dEvent.hasEvent()) {
+							applyDynamicEventBefore(dEvent, t);
+						}
+					}
+
+					// Rebuild the Ymatrix to no-fault condition, which means
+					// all events are cleared.
+					//net.setYMatrix(net.formYMatrix(SequenceCode.POSITIVE, false));
+					
+					for (DynamicEvent dEvent : net.getDynamicEventList()) {
+						//if (dEvent.hasEvent()) {
+						   
+					        if (dEvent.getType() == DynamicEventType.BUS_FAULT) {
+						         AcscBusFault fault = dEvent.getBusFault();
+						         AcscBus bus = fault.getBus();
+						         DStabilityNetwork faultSubNet = (DStabilityNetwork) bus.getNetwork();
+						         ISparseEqnComplex ymatrix = faultSubNet.formYMatrix(SequenceCode.POSITIVE, false);
+						         //TODO don't forget to set the ymatrix
+						         faultSubNet.setYMatrix(ymatrix);
+						         faultSubNet.setYMatrixDirty(true);
+						         System.out.println("Time :"+t+", ymatrix of network is rebuilted:"+faultSubNet.getId());
+						         System.out.println("ymatrix = \n"+ymatrix);
+		
+
+					         }
+					}
+					        
+
+
+					
+					
+					ipssLogger.fine("Reset Ymatrix for event applying");
+
+					// apply those events which result in adding z to Y-matrix,
+					// such as applying fault Z
+					for (DynamicEvent dEvent : net.getDynamicEventList()) {
+						if (dEvent.hasEvent()) {
+							try{
+								applyDynamicEventAfter(dEvent, t);
+							} catch (IpssNumericException e) {
+								ipssLogger.severe(e.toString());
+								return false;
+							}
+						}
+					}
+					
+
+					for (DynamicEvent dEvent : net.getDynamicEventList()) {
+						//if (dEvent.hasEvent()) {
+						   
+					        if (dEvent.getType() == DynamicEventType.BUS_FAULT) {
+						         AcscBusFault fault = dEvent.getBusFault();
+						         AcscBus bus = fault.getBus();
+						         DStabilityNetwork faultSubNet = (DStabilityNetwork) bus.getNetwork();
+						         ISparseEqnComplex ymatrix = faultSubNet.getYMatrix();
+						         
+						         System.out.println("Time :"+t+", ymatrix of network after applying bus fault:"+faultSubNet.getId());
+						         System.out.println("ymatrix = \n"+ymatrix);
+
+
+					         }
+					}
+					
+					
+					
+					
+				}
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	protected boolean hasAnyEvent(double t) {
+		boolean has = false;
+		for (DynamicEvent dEvent : net.getDynamicEventList()) {
+			if (dEvent.hasEventAt(t)) {
+				has = true;
+			}
+		}
+		return has;
+	}
+
+	@Override public void onMsgEvent(IpssMessage eventMsg) {
+		throw new InterpssRuntimeException("Method not applicable");
+	}
+	
+	// apply event before building the Y-matrix
+	@Override
+	protected void applyDynamicEventBefore(DynamicEvent e, double t) {
+		
+	}
+	
+	
+	// apply event after after building the Y-matrix
+	@Override
+	protected void applyDynamicEventAfter(DynamicEvent e, double t) throws IpssNumericException {
+		// active indicates applying the event. We only modify the Y matrix when
+				// applying an event
+				if (e.isActive()) {
+					if (e.getType() == DynamicEventType.BUS_FAULT) {
+						AcscBusFault fault = e.getBusFault();
+						AcscBus bus = fault.getBus();
+						
+						int i = bus.getSortNumber();
+						
+						Complex ylarge =  NumericConstant.LargeBusZ;
+						
+						if(fault.getFaultCode()==SimpleFaultCode.GROUND_LG){
+							
+						}
+						
+						else if(fault.getFaultCode()==SimpleFaultCode.GROUND_LL){
+					
+							
+						}
+	                    else if(fault.getFaultCode()==SimpleFaultCode.GROUND_LLG){
+	                    
+	                    	
+						}
+                        else if(fault.getFaultCode()==SimpleFaultCode.GROUND_3P){
+      
+                        	//((DStabilityNetwork) bus.getNetwork()).getYMatrix().addToA(ylarge, i, i);
+                        	//TODO for testing only
+                        	((DStabilityNetwork) bus.getNetwork()).getYMatrix().addToA(new Complex(0.5,0), i, i);
+						}
+						
+					}
+					/*
+					 * Apply the branch fault to the Y-matrix
+					 */
+					else if (e.getType() == DynamicEventType.BRANCH_FAULT) {
+						 throw new UnsupportedOperationException();
+					}
+					 
+					}
+	}
+
+}
