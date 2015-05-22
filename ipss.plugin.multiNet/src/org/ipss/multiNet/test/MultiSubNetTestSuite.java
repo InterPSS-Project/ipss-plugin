b5917e520882385bce19688b7531d5fa3edbb40f
Tony Huang <eetonyhuang@gmail.com>
2015-05-22 11:18:57 -0700

    add multiNet test suite

---
+++ ipss.plugin.multiNet/src/org/ipss/multiNet/test/MultiSubNetTestSuite.java
@@ -0,0 +1,16 @@
+package org.ipss.multiNet.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TestSubNetEquiv.class,
+	TestAddDummyBus.class,
+	TestMultiNetDStab.class,
+	
+})
+public class MultiSubNetTestSuite {
+
+}
