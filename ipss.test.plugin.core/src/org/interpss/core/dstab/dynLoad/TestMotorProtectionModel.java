e92eedfdbb2609a1252ae6498cf7b95b848ffacc
thuang <eetonyhuang@gmail.com>
2017-05-30 18:44:17 -0700

    fixed the motor Inorton calculation bug and add protection test case

--- ipss.test.plugin.core/src/org/interpss/core/dstab/dynLoad/TestMotorProtectionModel.java
+++ ipss.test.plugin.core/src/org/interpss/core/dstab/dynLoad/TestMotorProtectionModel.java
@@ -7,6 +7,7 @@ import java.util.logging.Level;
 import org.apache.commons.math3.complex.Complex;
 import org.interpss.core.dstab.mach.TestSetupBase;
 import org.interpss.display.AclfOutFunc;
+import org.interpss.numeric.datatype.Point;
 import org.interpss.numeric.datatype.Unit.UnitType;
 import org.junit.Test;
 
@@ -31,100 +32,13 @@ import com.interpss.dstab.cache.StateMonitor;
 import com.interpss.dstab.cache.StateMonitor.DynDeviceType;
 import com.interpss.dstab.cache.StateMonitor.MonitorRecord;
 import com.interpss.dstab.dynLoad.InductionMotor;
-import com.interpss.dstab.dynLoad.impl.InductionMotorImpl;
+import com.interpss.dstab.dynLoad.impl.MotorElectronicRelayProtection;
 import com.interpss.dstab.mach.EConstMachine;
 import com.interpss.dstab.mach.MachineType;
 
-public class TestInductionMotorModel extends TestSetupBase {
+public class TestMotorProtectionModel  extends TestSetupBase {
 	
 	//@Test
-	public void test_induction_Motor_dynModel_single_cage()  throws InterpssException {
-		DStabilityNetwork net = create2BusSystem();
-		assertTrue(net.isLfConverged());
-		
-		DStabBus bus1 = net.getDStabBus("Bus1");
-		
-		InductionMotor indMotor= DStabObjectFactory.createInductionMotor(bus1,"1");
-		
-
-		indMotor.setXm(3.0);
-		indMotor.setXl(0.07);
-		indMotor.setRa(0.032);
-		indMotor.setXr1(0.2);
-		indMotor.setRr1(0.01);
-		indMotor.setXr2(0.0);
-		indMotor.setRr2(0.0);
-		
-		indMotor.setLoadPercent(50);
-		indMotor.setMVABase(50);
-		indMotor.setH(1.0);
-		
-		DynamicSimuAlgorithm dstabAlgo = DStabObjectFactory.createDynamicSimuAlgorithm(net, msg);
-		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
-		assertTrue(aclfAlgo.loadflow());
-		System.out.println(AclfOutFunc.loadFlowSummary(net));
-		
-		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
-		dstabAlgo.setSimuStepSec(0.005d);
-		dstabAlgo.setTotalSimuTimeSec(0.1);
-
-		dstabAlgo.setRefMachine(net.getMachine("Swing-mach1"));
-		//net.addDynamicEvent(DStabObjectFactory.createBusFaultEvent("Bus1",net,SimpleFaultCode.GROUND_3P,0.0d,0.05),"3phaseFault@Bus5");
-        
-        
-		
-		StateMonitor sm = new StateMonitor();
-		sm.addGeneratorStdMonitor(new String[]{"Swing-mach1"});
-		sm.addBusStdMonitor(new String[]{"Bus1"});
-		sm.addDynDeviceMonitor(DynDeviceType.InductionMotor, "IndMotor_1@Bus1");
-		
-		// set the output handler
-		dstabAlgo.setSimuOutputHandler(sm);
-		dstabAlgo.setOutPutPerSteps(1);
-		
-		IpssLogger.getLogger().setLevel(Level.FINE);
-		
-		
-		if (dstabAlgo.initialization()) {
-			System.out.println(net.getMachineInitCondition());
-			
-			System.out.println("Running DStab simulation ...");
-		    while(dstabAlgo.getSimuTime()<=dstabAlgo.getTotalSimuTimeSec())
-			     dstabAlgo.solveDEqnStep(true);
-			
-		}
-		else{
-			System.out.println("Initialization error!");
-		}
-		/*
-		 * slip =0.009072990571330868
-           motor power =(0.7999999997203182, 0.49512907648490423)
-		 */
-		assertTrue(Math.abs(indMotor.getSlip()-0.009072990571330868)<1.0E-5);
-		
-		assertTrue(Math.abs(indMotor.getLoadPQ().getReal()-0.8)<1.0E-5);
-		
-		assertTrue( Math.abs(bus1.getVoltageMag()-0.99677)<1.0E-5);
-		
-		System.out.println("slip ="+indMotor.getSlip());
-		System.out.println("motor power ="+indMotor.getLoadPQ());
-		//}
-		//System.out.println(sm.toCSVString(sm.getMachAngleTable()));
-		System.out.println("\n bus volt:\n"+sm.toCSVString(sm.getBusVoltTable()));
-		System.out.println("\n gen Pe:\n"+sm.toCSVString(sm.getMachPeTable()));
-		System.out.println("\n motor slip:\n"+sm.toCSVString(sm.getMotorSlipTable()));
-		System.out.println("\n motor Te:\n"+sm.toCSVString(sm.getMotorTeTable()));
-		System.out.println("\n motor Tm:\n"+sm.toCSVString(sm.getMotorTmTable()));
-		System.out.println("\n motor Power:\n"+sm.toCSVString(sm.getMotorPTable()));
-		
-		MonitorRecord pRec0 = sm.getMotorPTable().get("IndMotor_1@Bus1").get(0);
-		
-		MonitorRecord pRec10 = sm.getMotorPTable().get("IndMotor_1@Bus1").get(10);
-		
-		assertTrue(Math.abs(pRec0.value-pRec10.value)<1.0E-5);
-	}
-	
-	@Test
 	public void test_induction_Motor_dynModel_single_cage_protection()  throws InterpssException {
 		DStabilityNetwork net = create2BusSystem();
 		assertTrue(net.isLfConverged());
@@ -212,8 +126,9 @@ public class TestInductionMotorModel extends TestSetupBase {
 		assertTrue(Math.abs(pRec0.value-pRec10.value)<1.0E-5);
 	}
 	
-	//@Test
-	public void test_induction_Motor_dynModel_double_cage()  throws InterpssException {
+	
+	@Test
+	public void test_single_induction_Motor_electronic_relay_protection()  throws InterpssException {
 		DStabilityNetwork net = create2BusSystem();
 		assertTrue(net.isLfConverged());
 		
@@ -225,14 +140,21 @@ public class TestInductionMotorModel extends TestSetupBase {
 		indMotor.setXm(3.0);
 		indMotor.setXl(0.07);
 		indMotor.setRa(0.032);
-		indMotor.setXr1(0.3);
-		indMotor.setRr1(0.05);
-		indMotor.setXr2(0.3);
-		indMotor.setRr2(0.02);
+		indMotor.setXr1(0.2);
+		indMotor.setRr1(0.01);
+		indMotor.setXr2(0.0);
+		indMotor.setRr2(0.0);
 		
 		indMotor.setLoadPercent(50);
 		indMotor.setMVABase(50);
-		indMotor.setH(1.0);
+		indMotor.setH(0.3);
+		
+		//create electronic relay and add it to the list
+		MotorElectronicRelayProtection eleRelay = new MotorElectronicRelayProtection(indMotor);
+		eleRelay.getTripVoltTimeCurve().getPoints().add(new Point(0.05,0.7)); // x-axis is time, y-axis is voltage
+		
+		indMotor.getProtectionControlList().add(eleRelay);
+
 		
 		DynamicSimuAlgorithm dstabAlgo = DStabObjectFactory.createDynamicSimuAlgorithm(net, msg);
 		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
@@ -241,16 +163,16 @@ public class TestInductionMotorModel extends TestSetupBase {
 		
 		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
 		dstabAlgo.setSimuStepSec(0.005d);
-		dstabAlgo.setTotalSimuTimeSec(0.1);
+		dstabAlgo.setTotalSimuTimeSec(1.0);
 
 		dstabAlgo.setRefMachine(net.getMachine("Swing-mach1"));
-		//net.addDynamicEvent(DStabObjectFactory.createBusFaultEvent("Bus1",net,SimpleFaultCode.GROUND_3P,0.0d,0.05),"3phaseFault@Bus5");
+		net.addDynamicEvent(DStabObjectFactory.createBusFaultEvent("Bus1",net,SimpleFaultCode.GROUND_3P,0.1d,0.08),"3phaseFault@Bus1");
         
         
 		
 		StateMonitor sm = new StateMonitor();
 		sm.addGeneratorStdMonitor(new String[]{"Swing-mach1"});
-		sm.addBusStdMonitor(new String[]{"Bus1"});
+		sm.addBusStdMonitor(new String[]{"Bus1","Swing"});
 		sm.addDynDeviceMonitor(DynDeviceType.InductionMotor, "IndMotor_1@Bus1");
 		
 		// set the output handler
@@ -264,36 +186,37 @@ public class TestInductionMotorModel extends TestSetupBase {
 			System.out.println(net.getMachineInitCondition());
 			
 			System.out.println("Running DStab simulation ...");
-		    while(dstabAlgo.getSimuTime()<=dstabAlgo.getTotalSimuTimeSec())
+		    while(dstabAlgo.getSimuTime()<=dstabAlgo.getTotalSimuTimeSec()){
+		    	System.out.println("\n time = "+dstabAlgo.getSimuTime());
 			     dstabAlgo.solveDEqnStep(true);
+		    }
 			
 		}
 		else{
 			System.out.println("Initialization error!");
 		}
 		/*
-		 * slip =0.009598914568821991
-           motor power =(0.7999999997171088, 0.5790774480423598)
+		 * slip =0.009072990571330868
+           motor power =(0.7999999997203182, 0.49512907648490423)
 		 */
-//		assertTrue(Math.abs(indMotor.getSlip()-0.00959891)<1.0E-5);
-//		
-//		assertTrue(Math.abs(indMotor.getLoadPQ().getReal()-0.8)<1.0E-5);
-//		
-//		assertTrue( Math.abs(bus1.getVoltageMag()-0.99677)<1.0E-5);
 		
 		System.out.println("slip ="+indMotor.getSlip());
 		System.out.println("motor power ="+indMotor.getLoadPQ());
-		//}
-		//System.out.println(sm.toCSVString(sm.getMachAngleTable()));
-		System.out.println(sm.toCSVString(sm.getBusVoltTable()));
-		System.out.println(sm.toCSVString(sm.getMachPeTable()));
-		System.out.println(sm.toCSVString(sm.getMotorSlipTable()));
-		System.out.println(sm.toCSVString(sm.getMotorTeTable()));
-		System.out.println(sm.toCSVString(sm.getMotorTmTable()));
-		System.out.println(sm.toCSVString(sm.getMotorPTable()));
 		
+		System.out.println("\n bus volt:\n"+sm.toCSVString(sm.getBusVoltTable()));
+		System.out.println("\n gen Pe:\n"+sm.toCSVString(sm.getMachPeTable()));
+		System.out.println("\n motor slip:\n"+sm.toCSVString(sm.getMotorSlipTable()));
+		System.out.println("\n motor Te:\n"+sm.toCSVString(sm.getMotorTeTable()));
+		System.out.println("\n motor Tm:\n"+sm.toCSVString(sm.getMotorTmTable()));
+		System.out.println("\n motor Power:\n"+sm.toCSVString(sm.getMotorPTable()));
 		
+		MonitorRecord pRec0 = sm.getMotorPTable().get("IndMotor_1@Bus1").get(0);
+		
+		MonitorRecord pRec10 = sm.getMotorPTable().get("IndMotor_1@Bus1").get(10);
+		
+		assertTrue(Math.abs(pRec0.value-pRec10.value)<1.0E-5);
 	}
+	
 	
 private DStabilityNetwork create2BusSystem() throws InterpssException{
 		
