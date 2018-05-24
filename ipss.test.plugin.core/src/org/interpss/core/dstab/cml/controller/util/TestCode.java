063b6e780c814d674a71a7c88755007c70594495
jinjingfarm <mzhou@interpss.org>
2018-05-24 21:51:22 +0800

    working CML testing cases

---
+++ ipss.test.plugin.core/src/org/interpss/core/dstab/cml/controller/util/TestCode.java
@@ -0,0 +1,10 @@
+package org.interpss.core.dstab.cml.controller.util;
+
+public class TestCode {
+	public static void main(String[] args) throws Exception {
+		TestAnnotateExciter exe = new TestAnnotateExciter();
+		
+		exe.getField("delayBlock");
+	}
+
+}
