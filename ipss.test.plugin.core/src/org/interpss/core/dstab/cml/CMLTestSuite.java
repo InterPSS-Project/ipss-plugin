d377ae4b63c9c41635efa0c691d9fc42813916f8
jinjingfarm <mzhou@interpss.org>
2018-05-28 09:05:24 +0800

    working CML unit testing cases

--- ipss.test.plugin.core/src/org/interpss/core/dstab/cml/CMLTestSuite.java
+++ ipss.test.plugin.core/src/org/interpss/core/dstab/cml/CMLTestSuite.java
@@ -2,11 +2,9 @@ package org.interpss.core.dstab.cml;
 
 import org.interpss.core.dstab.cml.block.DelayControlBlockTests;
 import org.interpss.core.dstab.cml.block.FilterControlBlockTests;
-import org.interpss.core.dstab.cml.block.GainBlockExtensionTests;
 import org.interpss.core.dstab.cml.block.IntegrationControlBlockTests;
 import org.interpss.core.dstab.cml.block.PIControlBlockTests;
 import org.interpss.core.dstab.cml.block.WashoutControlBlockTests;
-import org.interpss.core.dstab.cml.controller.AnnotateParserTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
