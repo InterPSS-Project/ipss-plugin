26d1f155b8500e0ce946ce57ea053eca63564d2f
jinjingfarm <mzhou@interpss.org>
2018-07-09 10:49:13 +0800

    updated the .classpath, unit test cases to remove unnecessary

--- ipss.test.plugin.core/src/org/interpss/core/adapter/bpa/CampareModel_OfBpaO7CTest.java
+++ ipss.test.plugin.core/src/org/interpss/core/adapter/bpa/CampareModel_OfBpaO7CTest.java
@@ -21,10 +21,10 @@ import com.interpss.core.aclf.BaseAclfNetwork;
 import com.interpss.dstab.BaseDStabNetwork;
 import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
-import com.interpss.tools.compare.IAclfBranchComparator;
-import com.interpss.tools.compare.IAclfBusComparator;
-import com.interpss.tools.compare.IAclfNetComparator;
-import com.interpss.tools.compare.NetModelComparator;
+import org.interpss.QA.compare.IAclfBranchComparator;
+import org.interpss.QA.compare.IAclfBusComparator;
+import org.interpss.QA.compare.IAclfNetComparator;
+import org.interpss.QA.compare.NetModelComparator;
 
 public class CampareModel_OfBpaO7CTest extends DStabTestSetupBase {
 	@Test
