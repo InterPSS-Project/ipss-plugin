981adf3a083f4bdff9666e118a3110db8c6dce9e
mike <mike.zhou@interpss.org>
2020-06-05 10:59:40 +0800

    added the ipss.plugin.3phase project

---
+++ ipss.plugin.3phase/src/org/interpss/threePhase/odm/ODM3PhaseDStabParserMapper.java
@@ -0,0 +1,17 @@
+package org.interpss.threePhase.odm;
+
+import org.ieee.odm.model.dstab.DStabModelParser;
+
+import com.interpss.common.msg.IPSSMsgHub;
+
+public class ODM3PhaseDStabParserMapper extends AbstractODM3PhaseDStabParserMapper<DStabModelParser> {
+      
+	/**
+	 * constructor
+	 * 
+	 * @param msg
+	 */
+	public ODM3PhaseDStabParserMapper(IPSSMsgHub msg) {
+		this.msg = msg;
+	}
+}
