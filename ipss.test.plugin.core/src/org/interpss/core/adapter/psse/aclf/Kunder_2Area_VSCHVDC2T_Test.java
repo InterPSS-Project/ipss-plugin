8938b32c3a6ab4e5791325bbc545bf120702e611
thuang <eetonyhuang@gmail.com>
2020-07-05 22:06:14 -0700

    update vschvdc power flow unit test case with results

--- ipss.test.plugin.core/src/org/interpss/core/adapter/psse/aclf/Kunder_2Area_VSCHVDC2T_Test.java
+++ ipss.test.plugin.core/src/org/interpss/core/adapter/psse/aclf/Kunder_2Area_VSCHVDC2T_Test.java
@@ -2,12 +2,14 @@ package org.interpss.core.adapter.psse.aclf;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.math3.complex.Complex;
 import org.ieee.odm.adapter.IODMAdapter;
 import org.ieee.odm.adapter.psse.PSSEAdapter;
 import org.ieee.odm.model.aclf.AclfModelParser;
 import org.interpss.CorePluginTestSetup;
 import org.interpss.display.AclfOutFunc;
 import org.interpss.mapper.odm.ODMAclfParserMapper;
+import org.interpss.numeric.util.NumericUtil;
 import org.junit.Test;
 
 import com.interpss.CoreObjectFactory;
@@ -64,6 +66,41 @@ public class Kunder_2Area_VSCHVDC2T_Test extends CorePluginTestSetup {
   		assertTrue(net.isLfConverged());
   		
   		System.out.println(AclfOutFunc.loadFlowSummary(net));
+  		
+  		/*
+  		 * PSS/E power flow results
+  		 * 
+  		 * Bus  Number	Bus  Name	Base kV	 Area Num	 Area Name	Zone Num	Zone Name	Owner Num	Owner Name	Code	Voltage (pu)	Angle (deg)	Normal Vmax (pu)	Normal Vmin (pu)	Emergency Vmax (pu)	Emergency Vmin (pu)
+			1	BUS1   AR1	20.0	1	AREA1	1		1		2	1.0300	13.15	1.1000	0.9000	1.1000	0.9000
+			2	BUS2   AR1	20.0	1	AREA1	1		1		2	1.0100	3.38	1.1000	0.9000	1.1000	0.9000
+			3	BUS3   AR2	20.0	2	AREA2	2		1		3	1.0300	0.00	1.1000	0.9000	1.1000	0.9000
+			4	BUS4   AR2	20.0	2	AREA2	2		1		2	1.0100	-9.79	1.1000	0.9000	1.1000	0.9000
+			5	BUS5   AR1	230.0	1	AREA1	1		1		1	1.0061	6.68	1.1000	0.9000	1.1000	0.9000
+			6	BUS6   AR1	230.0	1	AREA1	1		1		1	0.9773	-3.41	1.1000	0.9000	1.1000	0.9000
+			7	BUS7   L	230.0	1	AREA1	1		1		1	0.9595	-11.84	1.1000	0.9000	1.1000	0.9000
+			8	BUS8	230.0	2	AREA2	2		1		1	0.9739	-18.41	1.1000	0.9000	1.1000	0.9000
+			9	BUS9   L	230.0	2	AREA2	2		1		1	0.9800	-24.73	1.1000	0.9000	1.1000	0.9000
+			10	BUS10   AR2	230.0	2	AREA2	2		1		1	0.9886	-16.50	1.1000	0.9000	1.1000	0.9000
+			11	BUS11   AR2	230.0	2	AREA2	2		1		1	1.0107	-6.47	1.1000	0.9000	1.1000	0.9000
+															
+
+  		 */
+  		
+  		/*
+  		 * BUS      7 BUS7   L    230.00 CKT     MW     MVAR     MVA   % 0.9595PU  -11.84  X--- LOSSES ---X X---- AREA -----X X---- ZONE -----X      7
+                                                               220.68KV               MW     MVAR    1 AREA1           1
+		  TO LOAD-PQ                         967.0   100.0   972.2
+		  TO SHUNT                             0.0  -184.1   184.1
+		  TO      9 BUS9   L    230.00 VSC   209.0    68.7   220.0  55                       0.71  131.49    2 AREA2           2              "VDCLINE1"
+		  TO      6 BUS6   AR1  230.00  1  -1367.3    66.3  1368.9                          20.36  203.56    1 AREA1           1
+		  TO      8 BUS8        230.00  1     95.6   -25.4    99.0                           1.13   11.26    2 AREA2           2
+		  TO      8 BUS8        230.00  2     95.6   -25.4    99.0                           1.13   11.26    2 AREA2           2
+
+  		 */
+  		assertTrue(NumericUtil.equals(net.getBus("Bus7").getGenPQ(), new Complex(-2.09,-0.687),0.001));
+  		assertTrue(NumericUtil.equals(net.getBus("Bus7").getVoltageMag(),0.95946,0.00001));
+  		assertTrue(NumericUtil.equals(net.getBus("Bus7").getVoltageAng(),-11.84/(180/Math.PI),0.01));
+  		assertTrue(NumericUtil.equals(net.getBus("Bus9").getVoltageMag(),0.98,0.00001));
 		
 	}
 }
