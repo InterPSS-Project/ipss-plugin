3a68202d4b605c21a6ea7a049158640923da6701
mike <mike.zhou@interpss.org>
2020-08-05 08:32:50 +0800

    restructured the piecewise package structure.

---
+++ ipss.extendedPiecewiseAlgo/src/org/interpss/piecewise/subAreaNet/seq012/SubAcscNetwork.java
@@ -0,0 +1,68 @@
+ /* @(#)SubAcscNetwork.java   
+  *
+  * Copyright (C) 2006-2016 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 11/15/2016
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.piecewise.subAreaNet.seq012;
+
+import com.interpss.CoreObjectFactory;
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.acsc.AcscBranch;
+import com.interpss.core.acsc.AcscBus;
+import com.interpss.core.acsc.AcscGen;
+import com.interpss.core.acsc.AcscLoad;
+import com.interpss.core.acsc.AcscNetwork;
+
+/**
+ * Class for modeling the SubNetwork concept for representing a sub-network of type AcscNetwork. 
+ */
+public class SubAcscNetwork extends SubNetwork012<AcscBus, AcscBranch, AcscNetwork>{
+	
+	/**
+	 * default constructor
+	 * 
+	 * @param flag
+	 */
+	public SubAcscNetwork(int flag) {
+		super(flag);
+	}
+	
+	/**
+	 * constructor
+	 * 
+	 * @param flag
+	 * @param ids
+	 */
+	public SubAcscNetwork(int flag, String[] ids) {
+		super(flag, ids);
+	}
+	
+	@Override public AcscNetwork createSubNetwork() {
+		return CoreObjectFactory.createAcscNetwork();
+	};
+	
+	@Override public void buildSubNet(AcscNetwork parentNet) throws InterpssException {
+		super.buildSubNet(parentNet);
+		
+		// TODO only bus and branch objects are copied to the SubNetwork in the super class. We
+		//      may need to add missing objects
+	}
+}	
---
+++ ipss.extendedPiecewiseAlgo/src/org/interpss/piecewise/subAreaNet/seqPos/CuttingBranchPos.java
@@ -0,0 +1,77 @@
+ /*
+  * @(#)CuttingBranchPos.java   
+  *
+  * Copyright (C) 2006-2016 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 04/15/2016
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.piecewise.subAreaNet.seqPos;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.numeric.datatype.ComplexFunc;
+import org.interpss.piecewise.subAreaNet.base.BaseCuttingBranch;
+
+import com.interpss.core.net.BranchBusSide;
+
+
+/**
+ * Class for modeling the cutting branch concept with current of Complex type
+ * for single phase or positive sequence network analysis
+ * 
+ * @author Mike
+ *
+ */
+public class CuttingBranchPos extends BaseCuttingBranch<Complex> {
+	/**
+	 * constructor
+	 * 
+	 * @param id branch id
+	 * @param fromFlag branch from bus area flag
+	 * @param toFlag branch to bus area flag
+	 */
+	public CuttingBranchPos(String id, int fromFlag, int toFlag) {
+		super(id, fromFlag, toFlag);
+	}
+	
+	/**
+	 * constructor
+	 * 
+	 * @param id branch id
+	 */
+	public CuttingBranchPos(String id) {
+		super(id, BaseCuttingBranch.DefaultFlag, BaseCuttingBranch.DefaultFlag);
+	}
+
+	/**
+	 * constructor
+	 * 
+	 * @param id branch id
+	 */
+	public CuttingBranchPos(String id, BranchBusSide cuttingSide) {
+		super(id, BaseCuttingBranch.DefaultFlag, BaseCuttingBranch.DefaultFlag, cuttingSide);
+	}
+	
+	public String toString() {
+		String str =  super.toString();
+		if (this.cur != null)
+			str += "Branch current (from->to): " + ComplexFunc.toStr(this.cur) + "\n";
+		return str;
+	}
+}
