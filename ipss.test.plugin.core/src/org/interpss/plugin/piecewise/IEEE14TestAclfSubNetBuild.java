57ec34a9ad11d2b5d1d56f4e0bbff690ce6045fb
mike <mike.zhou@interpss.org>
2020-08-07 22:24:16 +0800

    updated the test cases.

--- ipss.test.plugin.core/src/org/interpss/plugin/piecewise/IEEE14TestAclfSubNetBuild.java
+++ ipss.test.plugin.core/src/org/interpss/plugin/piecewise/IEEE14TestAclfSubNetBuild.java
@@ -26,6 +26,8 @@ package org.interpss.plugin.piecewise;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.logging.Level;
+
 import org.apache.commons.math3.complex.Complex;
 import org.interpss.piecewise.subAreaNet.SubAreaNetProcessor;
 import org.interpss.piecewise.subAreaNet.seqPos.CuttingBranchPos;
@@ -33,16 +35,19 @@ import org.interpss.piecewise.subAreaNet.seqPos.SubNetworkPos;
 import org.interpss.piecewise.subAreaNet.seqPos.impl.SubNetworkPosProcessorImpl;
 import org.junit.Test;
 
+import com.interpss.CoreObjectFactory;
+import com.interpss.common.util.IpssLogger;
 import com.interpss.core.aclf.AclfBranch;
 import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.algo.LoadflowAlgorithm;
 
 /*
  * This test case is for testing sub-area search algorithm implementation.
  */
 public class IEEE14TestAclfSubNetBuild extends PiecewiseAlgoTestSetup {
 	@Test
-	public void testCase1() throws Exception {
+	public void splitNetworkTest1() throws Exception {
 		AclfNetwork net = IEEE14TestSubAreaSearch.getTestNet();
 		
 		
@@ -68,9 +73,9 @@ public class IEEE14TestAclfSubNetBuild extends PiecewiseAlgoTestSetup {
 	}
 
 	@Test
-	public void testCase2() throws Exception {
+	public void splitNetworkTest2() throws Exception {
 		AclfNetwork net = IEEE14TestSubAreaSearch.getTestNet();
-		
+	
 		SubAreaNetProcessor<AclfBus, AclfBranch, SubNetworkPos, Complex> 
 				proc = new SubNetworkPosProcessorImpl<>(net, new CuttingBranchPos[] { 
 							new CuttingBranchPos("4->71(1)"),
@@ -84,16 +89,91 @@ public class IEEE14TestAclfSubNetBuild extends PiecewiseAlgoTestSetup {
   		// we should have three sub-network objects
   		assertTrue("we should have three sub-network objects", proc.getSubAreaNetList().size() == 3);
 
+  		assertTrue("SubNetwork 1 should have 5 buses", proc.getSubAreaNet(1).getSubNet().getBusList().size() == 5);
+  		assertTrue("SubNetwork 2 should have 11 buses", proc.getSubAreaNet(2).getSubNet().getBusList().size() == 11);
+  		assertTrue("SubNetwork 3 should have 1 buses", proc.getSubAreaNet(3).getSubNet().getBusList().size() == 1);
+
   		//System.out.println("Bus-1 subarea flag: " + net.getBus("1").getIntFlag());
   		//System.out.println("Bus-14 subarea flag: " + net.getBus("14").getIntFlag());
   		assertTrue(net.getBus("1").getSubAreaFlag() == 1);
   		assertTrue(net.getBus("9").getSubAreaFlag() == 2);
   		assertTrue(net.getBus("14").getSubAreaFlag() == 3);
+	}
+	
+	@Test
+	public void splitMergeNetworkTest1() throws Exception {
+		AclfNetwork net = IEEE14TestSubAreaSearch.getTestNet();
+		net.setId("Parenet Net");
+		
+		assertTrue("", net.getBusList().size() == 17);
+		
+		SubAreaNetProcessor<AclfBus, AclfBranch, SubNetworkPos, Complex> 
+				proc = new SubNetworkPosProcessorImpl<>(net, new CuttingBranchPos[] { 
+							new CuttingBranchPos("4->71(1)"),
+							new CuttingBranchPos("4->91(1)"),
+							new CuttingBranchPos("5->61(1)"),
+							new CuttingBranchPos("9->14(1)"),
+							new CuttingBranchPos("14->13(1)")});	
+  		
+  		proc.processSubAreaNet();
+  		
+  		/*
+  		 * Please note: The following behavior has been changed. The Parent Net still has
+  		 * references to all Bus/Branch objects after the splitting. 
+  		 */
+  		assertTrue("Parent Net should have 17 still buses", net.getBusList().size() == 17);
 
-  		assertTrue("SubNetwork 1 should have 5 buses", proc.getSubAreaNet(1).getSubNet().getBusList().size() == 5);
+  		assertTrue("Bus 1 in the SubNet-1",  net.getBus("1").getNetwork().getId().equals("SubNet-1"));
+  		assertTrue("Bus 9 in the SubNet-2", net.getBus("9").getNetwork().getId().equals("SubNet-2"));
+  		assertTrue("Bus 14 in the SubNet-14", net.getBus("14").getNetwork().getId().equals("SubNet-3"));
   		
-  		assertTrue("SubNetwork 2 should have 11 buses", proc.getSubAreaNet(2).getSubNet().getBusList().size() == 11);
+  		//System.out.println(net.getBranch("1->2(1)").getNetwork().getId());
+  		//System.out.println(net.getBranch("5->61(1)").getNetwork().getId());
+  		assertTrue("Branch 1->2(1) should be in SubNet-1", net.getBranch("1->2(1)").getNetwork().getId().equals("SubNet-1"));
+  		assertTrue("Branch 5->61(1) should be in Parent Net", net.getBranch("5->61(1)").getNetwork().getId().equals("Parenet Net"));
   		
-  		assertTrue("SubNetwork 3 should have 1 buses", proc.getSubAreaNet(3).getSubNet().getBusList().size() == 1);
+  		proc.mergeSubNet();
+  	
+  		assertTrue("we should have no sub-network objects", proc.getSubAreaNetList().size() == 0);
+
+  		assertTrue("",  net.getBus("1").getNetwork().getId().equals("Parenet Net"));
+  		assertTrue("", net.getBus("9").getNetwork().getId().equals("Parenet Net"));
+  		assertTrue("", net.getBus("14").getNetwork().getId().equals("Parenet Net"));
+	
+  		assertTrue("", net.getBranch("1->2(1)").getNetwork().getId().equals("Parenet Net"));
+  		assertTrue("", net.getBranch("5->61(1)").getNetwork().getId().equals("Parenet Net"));
+	}
+	
+	@Test
+	public void splitMergeNetworkTest2() throws Exception {
+		AclfNetwork net = IEEE14TestSubAreaSearch.getTestNet();
+		net.setId("Parenet Net");
+		
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.loadflow();
+  		assertTrue(net.isLfConverged());
+  		//System.out.println(net.net2String());
+		
+		SubAreaNetProcessor<AclfBus, AclfBranch, SubNetworkPos, Complex> 
+				proc = new SubNetworkPosProcessorImpl<>(net, new CuttingBranchPos[] { 
+							new CuttingBranchPos("4->71(1)"),
+							new CuttingBranchPos("4->91(1)"),
+							new CuttingBranchPos("5->61(1)"),
+							new CuttingBranchPos("9->14(1)"),
+							new CuttingBranchPos("14->13(1)")});	
+  		
+  		proc.processSubAreaNet();
+  		
+  		assertTrue("Bus 1 in the SubNet-1",  net.getBus("1").getNetwork().getId().equals("SubNet-1"));
+  		assertTrue("Bus 9 in the SubNet-2", net.getBus("9").getNetwork().getId().equals("SubNet-2"));
+  		assertTrue("Bus 14 in the SubNet-14", net.getBus("14").getNetwork().getId().equals("SubNet-3"));  		
+  		
+  		proc.mergeSubNet();
+  		//System.out.println(net.net2String());
+
+  		//IpssLogger.getLogger().setLevel(Level.INFO);;
+	  	algo.loadflow();
+  		assertTrue(net.isLfConverged());
+  		//System.out.println(net.net2String());
 	}
 }
