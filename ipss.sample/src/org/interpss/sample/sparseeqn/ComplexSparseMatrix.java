83e2e3b73d11ed24f6e3635c68ff8c22f5617669
mike <mike.zhou@interpss.org>
2020-11-18 15:02:26 +0800

    added a new ComplexSparseMatrix sample class.

---
+++ ipss.sample/src/org/interpss/sample/sparseeqn/ComplexSparseMatrix.java
@@ -0,0 +1,80 @@
+package org.interpss.sample.sparseeqn;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.numeric.datatype.ComplexFunc;
+import org.interpss.numeric.exp.IpssNumericException;
+import org.interpss.numeric.sparse.ISparseEqnComplex;
+
+import com.interpss.core.sparse.impl.SparseEqnComplexImpl;
+
+public class ComplexSparseMatrix {
+	public static void main(String[] args) throws Exception {
+		sample1();
+		
+		sample2();
+		
+		sample3();
+	}	
+	
+	public static void sample1() {
+		ISparseEqnComplex m = new SparseEqnComplexImpl(2,3) ;
+		
+		m.setA(new Complex(1.0,1.0), 0, 0);
+		m.setA(new Complex(2.0,2.0), 1, 1);
+
+		m.setA(new Complex(0.0,11.0), 0, 2);
+		m.setA(new Complex(0.0,12.0), 1, 2);
+		
+		System.out.println("Sample 1 output: ");
+		System.out.println(m);		
+	}
+	
+	public static void sample2() {
+		ISparseEqnComplex m = new SparseEqnComplexImpl(2, 2);
+		
+		Complex  a11 = new Complex( 1.0, 1.1 ),
+			     a12 = new Complex( 3.1, 4.1 ),
+			     a21 = new Complex( 2.1, 1.9 ),
+			     a22 = new Complex( 3.0, 4.0 );
+		m.setA( a11, 0, 0 );
+		m.setA( a12, 0, 1 );
+		m.setA( a21, 1, 0 );
+		m.setA( a22, 1, 1 );
+
+		Complex b1 = new Complex( 2.03624, 0.3256 ),
+			    b2 = new Complex(-0.16731, 0.03888 );
+
+		Complex[] xAry = new Complex[2];
+		xAry[0] = b1;
+		xAry[1] = b2;
+		
+		Complex[] y = m.multiply(xAry);
+		System.out.println("\n\nSample 2 output: ");
+		System.out.println(ComplexFunc.toStr(y[0]));
+		System.out.println(ComplexFunc.toStr(y[1]));		
+	}
+	
+	public static void sample3() throws IpssNumericException {
+		ISparseEqnComplex eqn = new SparseEqnComplexImpl(2);
+		
+		Complex  a11 = new Complex( 1.0, 1.1 ),
+			     a12 = new Complex( 3.1, 4.1 ),
+			     a21 = new Complex( 2.1, 1.9 ),
+			     a22 = new Complex( 3.0, 4.0 );
+		eqn.setA( a11, 0, 0 );
+		eqn.setA( a12, 0, 1 );
+		eqn.setA( a21, 1, 0 );
+		eqn.setA( a22, 1, 1 );
+		
+		Complex b1 = new Complex( 1.0, 2.0 ),
+			    b2 = new Complex( 3.0, 4.0 );
+
+		eqn.setBi( b1, 0 );
+		eqn.setBi( b2, 1 );
+		
+		eqn.solveEqn(1.0e-20);
+		
+		System.out.println("\n\nSample 3 output: ");		
+		System.out.println(eqn);
+	}
+}
