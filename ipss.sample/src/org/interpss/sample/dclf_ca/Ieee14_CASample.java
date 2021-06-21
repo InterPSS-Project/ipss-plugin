d1fd593e68998b265c13565183c8dc5677cdd13c
mike <mike.zhou@interpss.org>
2021-06-21 11:02:39 +0800

    chagne SampleCases to SampleTestingCases

--- ipss.sample/src/org/interpss/sample/dclf_ca/Ieee14_CASample.java
+++ ipss.sample/src/org/interpss/sample/dclf_ca/Ieee14_CASample.java
@@ -24,14 +24,24 @@
 
 package org.interpss.sample.dclf_ca;
 
+import static com.interpss.core.DclfAlgoObjectFactory.createContingency;
+import static com.interpss.core.DclfAlgoObjectFactory.createContingencyAnalysisAlgorithm;
+import static com.interpss.core.DclfAlgoObjectFactory.createMonitoringBranch;
+import static com.interpss.core.DclfAlgoObjectFactory.createOutageBranch;
+
 import org.interpss.IpssCorePlugin;
 import org.interpss.numeric.exp.IpssNumericException;
 import org.interpss.pssl.common.PSSLException;
+import org.interpss.pssl.plugin.IpssAdapter;
 
 import com.interpss.common.exp.InterpssException;
 import com.interpss.core.aclf.AclfBranch;
 import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.contingency.Contingency;
+import com.interpss.core.algo.dclf.BranchOutageType;
+import com.interpss.core.algo.dclf.ContingencyAnalysisAlgorithm;
 import com.interpss.core.algo.dclf.OutageBranch;
+import com.interpss.core.algo.dclf.adapter.DclfAlgoBranch;
 import com.interpss.core.common.OutageConnectivityException;
 import com.interpss.core.common.ReferenceBusException;
 
@@ -45,84 +55,76 @@ public class Ieee14_CASample {
 	}
 	
 	public static void singleOutageSample() throws InterpssException, ReferenceBusException, IpssNumericException, PSSLException  {
-		AclfNetwork net = Ieee14_CA_Utils.getSampleNet();
-        double baseMva = net.getBaseMva();
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/ieee14.ieee")
+				.setFormat(IpssAdapter.FileFormat.IEEECommonFormat)
+				.load()
+				.getImportedObj();	
         
 		// run Dclf
-		DclfAlgorithmDSL algoDsl = IpssDclf.createDclfAlgorithm(net)
-				.runDclfAnalysis();
+		ContingencyAnalysisAlgorithm algo = createContingencyAnalysisAlgorithm(net);
+		algo.calculateDclf();
 
-		// set single outage branch
-		AclfBranch outBranch = net.getBranch("Bus5->Bus6(1)");
-		algoDsl.outageBranch(outBranch);
+		DclfAlgoBranch dclfBranch1 = algo.getDclfAlgoBranch("Bus5->Bus6(1)");
+		DclfAlgoBranch dclfBranch2 = algo.getDclfAlgoBranch("Bus4->Bus7(1)");
+		DclfAlgoBranch dclfBranch3 = algo.getDclfAlgoBranch("Bus4->Bus9(1)");
+
+		double preFlowSum = dclfBranch1.getDclfFlow() + dclfBranch2.getDclfFlow() + dclfBranch3.getDclfFlow();
+
+		// define a contingency object
+		Contingency cont = createContingency("contId");
 		
-        double outBanchPreFlow = outBranch.getDclfFlow()*baseMva;
-        
+		// define an outage branch
+		cont.setOutageBranch(createOutageBranch(algo.getDclfAlgoBranch("Bus5->Bus6(1)"), BranchOutageType.OPEN));
+
+		// define monitoring branches
+		cont.addMonitoringBranch(createMonitoringBranch(algo.getDclfAlgoBranch("Bus4->Bus7(1)")));
+		cont.addMonitoringBranch(createMonitoringBranch(algo.getDclfAlgoBranch("Bus4->Bus9(1)")));
+
+		// perform CA analysis
+		algo.ca(cont);
+		
         /*
          * This example checks if the pre-outage-flow on branch 5->6 has been 
          * moved to branch 4->7 and 4->9 
          */
-        double sum = 0.0;
-        for (AclfBranch branch : net.getBranchList()) {
-        	double 	preFlow = branch.getDclfFlow()*baseMva,
-        			LODF = algoDsl.monitorBranch(branch).lineOutageDFactor(),
-           			postFlow = preFlow + LODF * outBanchPreFlow;
-
-       		if (branch.getId().equals("Bus4->Bus7(1)") ||
-       				branch.getId().equals("Bus4->Bus9(1)"))
-       			sum += postFlow - preFlow;
-       		else if (branch.getId().equals("Bus5->Bus6(1)"))
-       			sum -= preFlow;
-		}
-        System.out.println("The sum should be equal to zero: " + sum);
-	}
+        double postFlowSum = cont.getMonitoringBranch("Bus4->Bus7(1)").calPostFlow() + 
+        					 cont.getMonitoringBranch("Bus4->Bus9(1)").calPostFlow();
+        System.out.println("The sum should be the same: " + preFlowSum + ", " + postFlowSum);
+	} 
 
 	public static void multipleOutageSample() throws InterpssException, ReferenceBusException, IpssNumericException, OutageConnectivityException  {
-		AclfNetwork net = Ieee14_CA_Utils.getSampleNet();
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/ieee14.ieee")
+				.setFormat(IpssAdapter.FileFormat.IEEECommonFormat)
+				.load()
+				.getImportedObj();
 		
-		DclfAlgorithmDSL algoDsl = IpssDclf.createDclfAlgorithm(net)
-				.runDclfAnalysis();
+		// run Dclf
+		ContingencyAnalysisAlgorithm algo = createContingencyAnalysisAlgorithm(net);
+		algo.calculateDclf();
 
-		algoDsl.setLODFAnalysisType(LODFSenAnalysisType.MULTI_BRANCH)
-				.addOutageBranch("Bus1", "Bus5", "1")
-				.addOutageBranch("Bus3", "Bus4", "1")
-				.addOutageBranch("Bus6", "Bus11", "1");
-		algoDsl.setRefBus("Bus14");
-
-
-        //System.out.println("Contingency, Monitoring Branch, Pre Contingency Flow (MW), Post Contingency Flow (MW), Limit (MW), Loading (%)");	
-        //String contId = "Cont 1";
-        double baseMva = net.getBaseMva();
-		algoDsl.calLineOutageDFactors("ContId");
-
-		for (AclfBranch branch : net.getBranchList()) {
-        	double preFlow = branch.getDclfFlow()*baseMva,
-        		   postFlow = 0.0;
-
-        	double[] factors = algoDsl.monitorBranch(branch)
-  				  					  .getLineOutageDFactors();
-       		if (factors != null) {  // factors = null if branch is an outage branch
-           		double sum = 0.0;
-           		int cnt = 0;
-       			for (OutageBranch outBranch : algoDsl.outageBranchList()) {
-       				double flow = outBranch.getBranch().getDclfFlow();
-       				sum += flow * factors[cnt++];
-       			}
-       			postFlow = sum*baseMva + preFlow;
-       		}
-      
-       		/*
-       		 *  This example checks post outage flow per IEEE paper "Direct Calculation of Line Outage Distribution Factors" 
-       		 */
+		algo.setRefBus("Bus14");
+		
+		algo.multiOpenOutgageAnalysis(new OutageBranch[] {
+				createOutageBranch(algo.getDclfAlgoBranch("Bus1->Bus5(1)"), BranchOutageType.OPEN),
+				createOutageBranch(algo.getDclfAlgoBranch("Bus3->Bus4(1)"), BranchOutageType.OPEN),
+				createOutageBranch(algo.getDclfAlgoBranch("Bus6->Bus11(1)"), BranchOutageType.OPEN)
+			});
+	      
+   		/*
+   		 *  This example checks post outage flow per IEEE paper "Direct Calculation of Line Outage Distribution Factors" 
+   		 */
+		algo.getDclfAlgoBranchList().forEach(dclfBranch -> {
+			AclfBranch branch = dclfBranch.getBranch();
+			double postFlow = dclfBranch.calPostFlow() * net.getBaseMva();
        		if (branch.getId().equals("Bus1->Bus5(1)") ||
-       				branch.getId().equals("Bus3->Bus4(1)")||
-       				branch.getId().equals("Bus6->Bus11(1)"))
+       			branch.getId().equals("Bus3->Bus4(1)")||
+       			branch.getId().equals("Bus6->Bus11(1)"))
        			System.out.println("Branch " + branch.getId() + " should be 0.0: " + postFlow);
        		else if (branch.getId().equals("Bus2->Bus5(1)"))
        			System.out.println("Branch " + branch.getId() + " should be 69.08805: " + postFlow);
        		else if (branch.getId().equals("Bus6->Bus13(1)"))
        			System.out.println("Branch " + branch.getId() + " should be 17.88058: " + postFlow);
-		}
+		});
 	}
 }
 
