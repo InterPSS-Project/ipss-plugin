2f49f720edecc9982f35812233379f47909e1606
mike <mike.zhou@interpss.org>
2021-07-13 14:50:22 +0800

    Updated the DclfSample sample.

--- ipss.sample/src/org/interpss/sample/dclf_ca/DclfSample.java
+++ ipss.sample/src/org/interpss/sample/dclf_ca/DclfSample.java
@@ -38,53 +38,60 @@ public class DclfSample {
 	public static void main(String args[]) throws Exception {
 		IpssCorePlugin.init();
 		
-		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
-		SampleTestingCases.load_LF_5BusSystem(net);
+		AclfNetwork aclfNet = CoreObjectFactory.createAclfNetwork();
+		SampleTestingCases.load_LF_5BusSystem(aclfNet);
 		//System.out.println(net.net2String());
 
-		DclfAlgorithm algo = DclfAlgoObjectFactory.createDclfAlgorithm(net);
+		DclfAlgorithm dclfAlgo = DclfAlgoObjectFactory.createDclfAlgorithm(aclfNet);
 		
 		// perform DCLF calculation
-		algo.calculateDclf();
+		dclfAlgo.calculateDclf();
 		
-		System.out.println(DclfOutFunc.dclfResults(algo, false /*branchViolation*/));
+		System.out.println(DclfOutFunc.dclfResults(dclfAlgo, false /*branchViolation*/));
 		
 		System.out.println("\nBase Case\n==========");
 		
-		System.out.println("Bus-1 P: " + algo.getBusPower("1"));
-		System.out.println("Bus-1 Ang: " + algo.getBusAngle("1"));
-		System.out.println("Bus-1 P in to the network: " + algo.getBusPowerInfoNet("1"));
+		System.out.println("Bus-1 P: " + dclfAlgo.getBusPower("1"));
+		System.out.println("Bus-1 Ang: " + dclfAlgo.getBusAngle("1"));
+		System.out.println("Bus-1 P in to the network: " + dclfAlgo.getBusPowerInfoNet("1"));
 
-		System.out.println("Bus-4 P: " + algo.getBusPower("4"));
-		System.out.println("Bus-4 Ang: " + algo.getBusAngle("4"));
-		System.out.println("Bus-4 P in to the network: " + algo.getBusPowerInfoNet("4"));
+		System.out.println("Bus-4 P: " + dclfAlgo.getBusPower("4"));
+		System.out.println("Bus-4 Ang: " + dclfAlgo.getBusAngle("4"));
+		System.out.println("Bus-4 P in to the network: " + dclfAlgo.getBusPowerInfoNet("4"));
 		
-		System.out.println("Bus-5 P: " + algo.getBusPower("5"));
-		System.out.println("Bus-5 Ang: " + algo.getBusAngle("5"));
-		System.out.println("Bus-5 P in to the network: " + algo.getBusPowerInfoNet("5"));
+		System.out.println("Bus-5 P: " + dclfAlgo.getBusPower("5"));
+		System.out.println("Bus-5 Ang: " + dclfAlgo.getBusAngle("5"));
+		System.out.println("Bus-5 P in to the network: " + dclfAlgo.getBusPowerInfoNet("5"));
 		
 		System.out.println("\nGen/Load Change Case\n======================");
 		
-		DclfAlgoBus dclfBus1 = algo.getDclfAlgoBus("1");
+		DclfAlgoBus dclfBus1 = dclfAlgo.getDclfAlgoBus("1");
 		dclfBus1.setLoadAdjust(0.2);
 		
-		DclfAlgoBus dclfBus4 = algo.getDclfAlgoBus("4");
+		DclfAlgoBus dclfBus4 = dclfAlgo.getDclfAlgoBus("4");
 		dclfBus4.setGenAdjust(0.1);
 		
 		// perform DCLF calculation
-		algo.calculateDclf();
+		dclfAlgo.calculateDclf();
 		
-		System.out.println("Bus-1 P: " + algo.getBusPower("1"));
-		System.out.println("Bus-1 Ang: " + algo.getBusAngle("1"));
-		System.out.println("Bus-1 P in to the network: " + algo.getBusPowerInfoNet("1"));
+		System.out.println("Bus-1 P: " + dclfAlgo.getBusPower("1"));
+		System.out.println("Bus-1 Ang: " + dclfAlgo.getBusAngle("1"));
+		System.out.println("Bus-1 P in to the network: " + dclfAlgo.getBusPowerInfoNet("1"));
 
-		System.out.println("Bus-4 P: " + algo.getBusPower("4"));
-		System.out.println("Bus-4 Ang: " + algo.getBusAngle("4"));
-		System.out.println("Bus-4 P in to the network: " + algo.getBusPowerInfoNet("4"));
+		System.out.println("Bus-4 P: " + dclfAlgo.getBusPower("4"));
+		System.out.println("Bus-4 Ang: " + dclfAlgo.getBusAngle("4"));
+		System.out.println("Bus-4 P in to the network: " + dclfAlgo.getBusPowerInfoNet("4"));
 		
-		System.out.println("Bus-5 P: " + algo.getBusPower("5"));
-		System.out.println("Bus-5 Ang: " + algo.getBusAngle("5"));
-		System.out.println("Bus-5 P in to the network: " + algo.getBusPowerInfoNet("5"));
+		System.out.println("Bus-5 P: " + dclfAlgo.getBusPower("5"));
+		System.out.println("Bus-5 Ang: " + dclfAlgo.getBusAngle("5"));
+		System.out.println("Bus-5 P in to the network: " + dclfAlgo.getBusPowerInfoNet("5"));
+		
+		/*
+		 * At this point, the aclfNet object is untouched 
+		 */
+		
+		// transfer Dclf results to the AclfNetwork object
+		dclfAlgo.transfer2AclfNet(true /* clearGenLoadAdjInfo */); 
 	}	
 }
 
