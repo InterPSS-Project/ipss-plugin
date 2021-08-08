8e00c5bbb3bb09239bb59f59f183e24422acdde1
mike <mike.zhou@interpss.org>
2021-08-08 15:33:39 +0800

    updated the EDclf test cases.

--- ipss.test.plugin.core/src/org/interpss/core/dclf/IEEE39_EDclf_Test.java
+++ ipss.test.plugin.core/src/org/interpss/core/dclf/IEEE39_EDclf_Test.java
@@ -27,18 +27,22 @@ package org.interpss.core.dclf;
 import static com.interpss.core.algo.dclf.solver.IConnectBusProcessor.predicateConnectBus;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.math3.complex.Complex;
 import org.interpss.CorePluginFactory;
 import org.interpss.CorePluginTestSetup;
-import org.interpss.display.AclfOutFunc;
 import org.interpss.fadapter.IpssFileAdapter;
+import org.interpss.numeric.datatype.Counter;
 import org.interpss.numeric.sparse.ISparseEqnComplex;
+import org.interpss.numeric.util.NumericUtil;
 import org.junit.Test;
 
 import com.interpss.core.DclfAlgoObjectFactory;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.algo.AclfMethodType;
 import com.interpss.core.algo.dclf.EDclfAlgorithm;
+import com.interpss.core.algo.dclf.solver.IConnectBusProcessor;
 import com.interpss.core.algo.dclf.solver.IDclfSolver.CacheType;
+import com.interpss.core.datatype.Mismatch;
 import com.interpss.core.funcImpl.AclfNetHelper;
 
 public class IEEE39_EDclf_Test extends CorePluginTestSetup {
@@ -52,8 +56,27 @@ public class IEEE39_EDclf_Test extends CorePluginTestSetup {
 		EDclfAlgorithm edclfAlgo = DclfAlgoObjectFactory.createEDclfAlgorithm(aclfNet, CacheType.SenNotCached);
 		edclfAlgo.calculateEDclf();
 		
-		System.out.println("Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
+		System.out.println("EDclf Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
 		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		
+		IConnectBusProcessor processor = DclfAlgoObjectFactory.createConnectBusProcessor(aclfNet);
+		Complex[] newConVolt = processor.calConnectBusVoltage();
+		
+		Counter mCnt = new Counter();
+		aclfNet.getBusList().stream()
+			.filter(bus -> bus.isActive())
+			.forEach(bus -> {
+				if (predicateConnectBus.test(bus)) {
+					int i = mCnt.getCountThenIncrement();
+					bus.setVoltage(newConVolt[i]);
+				}
+			});
+		
+		Mismatch mis = aclfNet.maxMismatch(AclfMethodType.NR, predicateConnectBus);
+		System.out.println("ConnectBus VAdjustment Mismatch: " + mis);
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		
+		assertTrue("", mis.maxMis.abs() < 0.0001);
 	}
 	
 	@Test 
