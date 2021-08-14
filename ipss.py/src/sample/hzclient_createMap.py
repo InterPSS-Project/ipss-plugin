b2c2172e4982f6d1ebe5b26134bea0e646a29d75
mike <mike.zhou@interpss.org>
2021-08-14 14:36:50 +0800

    updated the python samples.

--- ipss.py/src/sample/hzclient_createMap.py
+++ ipss.py/src/sample/hzclient_createMap.py
@@ -2,14 +2,7 @@
 import hazelcast
 
 # Create a client using the configuration below
-client = hazelcast.HazelcastClient(
-    # Add member's host:port to the configuration.
-    # For each member on your Hazelcast cluster, you should add its host:port pair to the configuration.
-    # If the port is not specified, by default 5701, 5702 and 5703 will be tried.
-    cluster_members=[
-        "192.168.3.29:5701",
-    ]
-)
+client = hazelcast.HazelcastClient()
 
 # Get a map that is stored on the server side. We can access it from the client
 greetings_map = client.get_map("greetings-map").blocking()
