d872bd18555b6f234a3c7564e521b0c0b130cdf1
mike <mike.zhou@interpss.org>
2021-09-09 20:12:12 +0800

    updated the EDclf samples

--- ipss.test.plugin.core/src/org/interpss/core/dclf/UCTE2000Summer_EDclf_Test.java
+++ ipss.test.plugin.core/src/org/interpss/core/dclf/UCTE2000Summer_EDclf_Test.java
@@ -24,6 +24,7 @@
 
 package org.interpss.core.dclf;
 
+import static com.interpss.core.algo.dclf.solver.IConnectBusProcessor.predicateConnectBus;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.math3.complex.Complex;
@@ -31,15 +32,15 @@ import org.interpss.CorePluginFactory;
 import org.interpss.CorePluginTestSetup;
 import org.interpss.display.AclfOutFunc;
 import org.interpss.fadapter.IpssFileAdapter;
-import org.interpss.numeric.util.NumericUtil;
 import org.junit.Test;
 
 import com.interpss.core.DclfAlgoObjectFactory;
+import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.algo.AclfMethodType;
 import com.interpss.core.algo.dclf.EDclfAlgorithm;
-import com.interpss.core.algo.dclf.solver.IEDclfSolver;
 import com.interpss.core.algo.dclf.solver.IDclfSolver.CacheType;
+import com.interpss.core.datatype.Mismatch;
 
 public class UCTE2000Summer_EDclf_Test extends CorePluginTestSetup {
 	@Test 
@@ -52,9 +53,61 @@ public class UCTE2000Summer_EDclf_Test extends CorePluginTestSetup {
 		EDclfAlgorithm edclfAlgo = DclfAlgoObjectFactory.createEDclfAlgorithm(aclfNet, CacheType.SenNotCached);
 		edclfAlgo.calculateEDclf();
 		
-		System.out.println("Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
-		System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		System.out.println("EDclf Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+	}
+
+	@Test 
+	public void edclfVCorrectionTest() throws Exception {
+		AclfNetwork aclfNet = CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
+				.load("testdata/adpter/ieee_format/UCTE_2000_Summer.ieee")
+				.getAclfNet();	
+		
+		EDclfAlgorithm edclfAlgo = DclfAlgoObjectFactory.createEDclfAlgorithm(aclfNet, CacheType.SenNotCached);
+		edclfAlgo.calculateEDclf();
+		
+		System.out.println("EDclf Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		Mismatch mis = aclfNet.maxMismatch(AclfMethodType.NR, predicateConnectBus);
+		System.out.println("ConnectBus VAdjustment Mismatch: " + mis);
+		
+		DclfAlgoObjectFactory.createConnectBusProcessor(aclfNet)
+        					 .updateConnectBusVoltage();
+
+		System.out.println("EDclf/VCorrection Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
+		mis = aclfNet.maxMismatch(AclfMethodType.NR, predicateConnectBus);
+		System.out.println("ConnectBus VAdjustment Mismatch: " + mis);
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		assertTrue("", mis.maxMis.abs() < 0.0001);
 	}
 	
+	@Test 
+	public void edclfVCorAlterDataTest() throws Exception {
+		AclfNetwork aclfNet = CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
+				.load("testdata/adpter/ieee_format/UCTE_2000_Summer.ieee")
+				.getAclfNet();	
+		
+		AclfBus bus = aclfNet.getBus("Bus706");
+		bus.getContributeLoad("Bus706-L1").setLoadCP(new Complex(1.5349, -1.0));
+
+		EDclfAlgorithm edclfAlgo = DclfAlgoObjectFactory.createEDclfAlgorithm(aclfNet, CacheType.SenNotCached);
+		edclfAlgo.calculateEDclf();
+		
+		System.out.println("EDclf Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		Mismatch mis = aclfNet.maxMismatch(AclfMethodType.NR, predicateConnectBus);
+		System.out.println("ConnectBus VAdjustment Mismatch: " + mis);
+		
+		DclfAlgoObjectFactory.createConnectBusProcessor(aclfNet)
+        					 .updateConnectBusVoltage();
+
+		System.out.println("EDclf/VCorrection Mismatch: " + aclfNet.maxMismatch(AclfMethodType.NR));
+		mis = aclfNet.maxMismatch(AclfMethodType.NR, predicateConnectBus);
+		System.out.println("ConnectBus VAdjustment Mismatch: " + mis);
+		//System.out.println(AclfOutFunc.loadFlowSummary(aclfNet, true));
+		assertTrue("", mis.maxMis.abs() < 0.0001);
+	}	
 }
 
