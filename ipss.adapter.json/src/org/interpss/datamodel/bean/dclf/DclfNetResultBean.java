e4cbf1ec79a29577f44c583c567a6fe292e9af0a
mike <mike.zhou@interpss.org>
2021-09-14 22:39:32 +0800

    created a new project ipss.adapter.json and first-time check-in

---
+++ ipss.adapter.json/src/org/interpss/datamodel/bean/dclf/DclfNetResultBean.java
@@ -0,0 +1,10 @@
+package org.interpss.datamodel.bean.dclf;
+
+import org.interpss.datamodel.bean.aclf.BaseAclfNetBean;
+
+public class DclfNetResultBean extends BaseAclfNetBean<DclfBusResultBean, DclfBranchResultBean> {	
+	
+	public DclfNetResultBean() {
+		super();
+	}
+}
