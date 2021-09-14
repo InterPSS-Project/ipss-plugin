2dfe7e6cbd81f47eec8d5a250e1ab9847da355c7
mike <mike.zhou@interpss.org>
2021-09-14 22:39:32 +0800

    created a new project ipss.adapter.json and first-time check-in

---
+++ ipss.adapter.json/src/org/interpss/datamodel/bean/aclf/AclfBranchBean.java
@@ -0,0 +1,84 @@
+/*
+ * @(#)BaseJSONBean.java   
+ *
+ * Copyright (C) 2008-2013 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 01/10/2013
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+package org.interpss.datamodel.bean.aclf;
+
+import java.util.List;
+
+import org.interpss.datamodel.bean.BaseBranchBean;
+import org.interpss.datamodel.bean.BaseJSONBean;
+import org.interpss.datamodel.bean.aclf.adj.PsXfrTapControlBean;
+import org.interpss.datamodel.bean.aclf.adj.XfrTapControlBean;
+import org.interpss.datamodel.bean.datatype.BranchValueBean;
+
+/**
+ * Bean class for storing AclfBranch object info
+ * 
+ * @author mzhou
+ *
+ */
+public class AclfBranchBean extends BaseBranchBean {
+	
+	public BranchValueBean 
+			ratio = new BranchValueBean(1.0,1.0),			// xfr branch turn ratio, it is assumed on the from bus side per PSSE
+			ang = new BranchValueBean(0.0,0.0);				// PsXfr shifting angle, in rad, it is assumed on the from bus side per PSSE
+	
+	public XfrTapControlBean xfrTapControl;					// control bean for xfr
+	
+	public PsXfrTapControlBean psXfrTapControl;				// control bean for phase shiter control
+	
+	public AclfBranchBean() { }
+	
+	@Override public int compareTo(BaseJSONBean b) {
+		int eql = super.compareTo(b);
+		
+		AclfBranchBean bean = (AclfBranchBean)b;
+
+		String str = "ID: " + this.id + " AclfBranchBean.";
+		
+		if (this.ratio.compareTo(bean.ratio) != 0) {
+			logCompareMsg(str + "ratio is not equal");	eql = 1; }
+
+		if (this.ang.compareTo(bean.ang) != 0) {
+			logCompareMsg(str + "ang is not equal");	eql = 1; }
+		
+		if (this.xfrTapControl != null && bean.xfrTapControl == null)
+			eql = 1;
+		if (this.xfrTapControl == null && bean.xfrTapControl != null)
+			eql = 1;
+		if (this.xfrTapControl != null && bean.xfrTapControl != null)
+			if (this.xfrTapControl.compareTo(bean.xfrTapControl) != 0) eql = 1;
+		
+		if (this.psXfrTapControl != null && bean.psXfrTapControl == null)
+			eql = 1;
+		if (this.psXfrTapControl == null && bean.psXfrTapControl != null)
+			eql = 1;
+		if (this.psXfrTapControl != null && bean.psXfrTapControl != null)
+			if (this.psXfrTapControl.compareTo(bean.psXfrTapControl) != 0) eql = 1;
+
+		return eql;
+	}	
+	
+	@Override
+	public boolean validate(List<String> msgList) { return true; }
+}
---
+++ ipss.adapter.json/src/org/interpss/datamodel/bean/aclf/AclfBranchResultBean.java
@@ -0,0 +1,68 @@
+/*
+ * @(#)AclfBusResultBean.java   
+ *
+ * Copyright (C) 2008-2013 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 01/10/2013
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
+package org.interpss.datamodel.bean.aclf;
+
+import org.interpss.datamodel.bean.BaseJSONBean;
+import org.interpss.datamodel.bean.datatype.ComplexBean;
+import org.interpss.numeric.util.NumericUtil;
+
+/**
+ * Bean class for storing AclfBranch result info
+ * 
+ * @author mzhou
+ *
+ */
+public class AclfBranchResultBean extends AclfBranchBean {
+
+	public ComplexBean 
+		flow_f2t,		// branch power flow from->to
+	    flow_t2f,		// branch power flow to->from
+	    loss;			// branch power loss
+	
+	public double 
+		cur;			// branch current in amps, for Xfr, it is at the high voltage side
+	
+	public AclfBranchResultBean() { }
+	
+	@Override public int compareTo(BaseJSONBean b) {
+		int eql = super.compareTo(b);
+		
+		AclfBranchResultBean bean = (AclfBranchResultBean)b;
+		
+		String str = "ID: " + this.id + " AclfBranchResultBean.";
+
+		if (this.flow_f2t.compareTo(bean.flow_f2t) != 0) {
+			logCompareMsg(str + "flow_f2t is not equal"); eql = 1; }
+		if (this.flow_t2f.compareTo(bean.flow_t2f) != 0) {
+			logCompareMsg(str + "flow_t2f is not equal"); eql = 1; }
+		if (this.loss.compareTo(bean.loss) != 0) {
+			logCompareMsg(str + "loss is not equal"); eql = 1; }
+		
+		if (!NumericUtil.equals(this.cur, bean.cur, BaseJSONBean.CUR_ERR)) {  // in amps
+			logCompareMsg(str + "cur is not equal, " + this.cur + ", " + bean.cur); eql = 1; }
+
+		return eql;
+	}	
+}
