c8bc9835d589c444b2178e688f3e8daeaf164644
mike <mike.zhou@interpss.org>
2021-09-16 15:00:41 +0800

    updated the json adapter sample.

--- ipss.adapter.json/src/sample/Ieee14JSonAdapterSample.java
+++ ipss.adapter.json/src/sample/Ieee14JSonAdapterSample.java
@@ -4,7 +4,6 @@ import org.interpss.CorePluginFactory;
 import org.interpss.IpssCorePlugin;
 import org.interpss.datamodel.bean.aclf.AclfNetBean;
 import org.interpss.datamodel.bean.base.BaseJSONUtilBean;
-import org.interpss.datamodel.bean.base.DefaultExtBean;
 import org.interpss.datamodel.mapper.aclf.AclfBean2AclfNetMapper;
 import org.interpss.datamodel.mapper.aclf.AclfNet2AclfBeanMapper;
 import org.interpss.fadapter.IpssFileAdapter;
@@ -15,19 +14,7 @@ import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.algo.AclfMethodType;
 import com.interpss.core.algo.LoadflowAlgorithm;
 
-public class Ieee14BusSample {
-	public static class BusExtBean extends BaseJSONUtilBean {
-		public BusExtBean() {}
-	}
-	
-	public static class BranchExtBean extends BaseJSONUtilBean {
-		public BranchExtBean() {}
-	}
-
-	public static class NetworkExtBean extends BaseJSONUtilBean {
-		public NetworkExtBean() {}
-	}
-	
+public class Ieee14JSonAdapterSample {
 	public static void main(String[] args) throws InterpssException {
 		IpssCorePlugin.init();
 		
@@ -37,15 +24,14 @@ public class Ieee14BusSample {
 				.getAclfNet();	
 		
 		// map AclfNet to AclfNetBean
-		AclfNetBean<BusExtBean, BranchExtBean, NetworkExtBean> netBean = 
-				new AclfNet2AclfBeanMapper<BusExtBean, BranchExtBean, NetworkExtBean>()
-				       .map2Model(aclfNet);	
+		AclfNetBean netBean = new AclfNet2AclfBeanMapper()
+				       				.map2Model(aclfNet);	
 		System.out.println(netBean.toString());
 		
 		// map AclfNetBean back to an AclfNet object
-		aclfNet = new AclfBean2AclfNetMapper<BusExtBean, BranchExtBean, NetworkExtBean>()
-			.map2Model(netBean)
-			.getAclfNet();
+		aclfNet = new AclfBean2AclfNetMapper()
+						.map2Model(netBean)
+						.getAclfNet();
 		
 	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(aclfNet);
 	  	algo.loadflow();
