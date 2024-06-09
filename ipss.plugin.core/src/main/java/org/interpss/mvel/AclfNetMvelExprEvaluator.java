204faeeed92800f4c615f32d60c9411dd49129cf
jinjingfarm <ipss.cntester@gmail.com>
2024-06-09 16:46:32 +0800

    updated the AclfNetMvelExprEvaluator implementation.

--- ipss.plugin.core/src/main/java/org/interpss/mvel/AclfNetMvelExprEvaluator.java
+++ ipss.plugin.core/src/main/java/org/interpss/mvel/AclfNetMvelExprEvaluator.java
@@ -19,4 +19,49 @@ public class AclfNetMvelExprEvaluator extends BaseMvelExprEvaluator {
 		this.aclfNet = aclfNet;
 		this.context.put("aclfnet", aclfNet);
 	}
+	
+	/**
+	 * Apply the factor to all generation in the AclfNetwork
+	 * 
+	 * @param factor adjustment factor in PU
+	 */
+	public void adjustGen(double factor) {
+		this.aclfNet.getBusList().stream()
+			.filter(bus -> bus.isActive() && bus.isGen())
+			.forEach(bus -> {
+				if (this.aclfNet.isContributeGenLoadModel()) {
+					bus.getContributeGenList().forEach(gen -> {
+						gen.setGen(gen.getGen().multiply(factor));
+					});
+				}
+				else {
+					bus.setGenP(factor * bus.getGenP());
+					if (bus.isGenPQ())
+						bus.setGenQ(factor * bus.getGenQ());
+				}
+			});
+	}
+	
+	/**
+	 * Apply the factor to all loads in the AclfNetwork
+	 * 
+	 * @param factor adjustment factor in PU
+	 */
+	public void adjustLoad(double factor) {
+		this.aclfNet.getBusList().stream()
+			.filter(bus -> bus.isActive() && bus.isLoad())
+			.forEach(bus -> {
+				if (this.aclfNet.isContributeGenLoadModel()) {
+					bus.getContributeLoadList().forEach(load -> {
+						load.setLoadCP(load.getLoadCP().multiply(factor));
+						load.setLoadCI(load.getLoadCI().multiply(factor));
+						load.setLoadCZ(load.getLoadCZ().multiply(factor));
+					});
+				}
+				else {
+					bus.setLoadP(factor * bus.getLoadP());
+					bus.setLoadQ(factor * bus.getLoadQ());
+				}
+			});
+	}
 }
