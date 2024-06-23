41c2b5f4374b6d7ff0d7e77a989b49f5444ff96f
power4all <thuang@users.noreply.github.com>
2024-06-23 16:00:24 -0600

    Merge branch 'dyn_sim_mod_euler'

--- ipss.plugin.3phase/src/test/java/org/interpss/_3phase/system/IEEE9Bus_3phase_LF_init_test.java
+++ ipss.plugin.3phase/src/test/java/org/interpss/_3phase/system/IEEE9Bus_3phase_LF_init_test.java
@@ -6,6 +6,10 @@ import static org.junit.Assert.assertTrue;
 import java.util.logging.Level;
 
 import org.apache.commons.math3.complex.Complex;
+import org.ieee.odm.adapter.IODMAdapter.NetType;
+import org.ieee.odm.adapter.psse.PSSEAdapter;
+import org.ieee.odm.adapter.psse.PSSEAdapter.PsseVersion;
+import org.ieee.odm.model.dstab.DStabModelParser;
 import org.interpss.IpssCorePlugin;
 import org.interpss.display.AclfOutFunc;
 import org.interpss.numeric.datatype.Complex3x3;
@@ -18,10 +22,13 @@ import org.interpss.threePhase.basic.dstab.DStab3PLoad;
 import org.interpss.threePhase.dynamic.DStabNetwork3Phase;
 import org.interpss.threePhase.dynamic.algo.DynamicEventProcessor3Phase;
 import org.interpss.threePhase.dynamic.impl.DStabNetwork3phaseImpl;
+import org.interpss.threePhase.odm.ODM3PhaseDStabParserMapper;
 import org.interpss.threePhase.util.ThreePhaseAclfOutFunc;
 import org.interpss.threePhase.util.ThreePhaseObjectFactory;
 import org.junit.Test;
 
+import com.interpss.dstab.DStabObjectFactory;
+import com.interpss.simu.SimuObjectFactory;
 import com.interpss.common.exp.InterpssException;
 import com.interpss.common.util.IpssLogger;
 import com.interpss.core.CoreObjectFactory;
@@ -35,10 +42,13 @@ import com.interpss.core.acsc.adpter.AcscXformerAdapter;
 import com.interpss.core.acsc.fault.SimpleFaultCode;
 import com.interpss.core.algo.LoadflowAlgorithm;
 import com.interpss.core.net.NetworkType;
-import com.interpss.dstab.DStabObjectFactory;
+import com.interpss.dstab.BaseDStabBus;
 import com.interpss.dstab.algo.DynamicSimuAlgorithm;
 import com.interpss.dstab.algo.DynamicSimuMethod;
 import com.interpss.dstab.cache.StateMonitor;
+import com.interpss.dstab.impl.DStabGenImpl;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
 
 public class IEEE9Bus_3phase_LF_init_test {
 	
@@ -290,6 +300,108 @@ ca = (0.0, 0.0),cb = (1.734723475976807E-18, -3.469446951953614E-18),cc = (-1.73
 			}
 		}
 	
+	    @Test
+		public void test_IEEE9_gen_load_update() throws InterpssException {
+			IpssCorePlugin.init();
+			IpssCorePlugin.setLoggerLevel(Level.INFO);
+			PSSEAdapter adapter = new PSSEAdapter(PsseVersion.PSSE_30);
+			assertTrue(adapter.parseInputFile(NetType.DStabNet,
+					new String[] { "testData/IEEE9Bus/ieee9.raw", "testData/IEEE9Bus/ieee9.seq",
+							"testData/IEEE9Bus/ieee9_dyn_onlyGen.dyr" }));
+			DStabModelParser parser = (DStabModelParser) adapter.getModel();
+
+			// System.out.println(parser.toXmlDoc());
+
+			SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET);
+
+			// The only change to the normal data import is the use of
+			// ODM3PhaseDStabParserMapper
+			if (!new ODM3PhaseDStabParserMapper(IpssCorePlugin.getMsgHub()).map2Model(parser, simuCtx)) {
+				System.out
+						.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+				return;
+			}
+
+			DStabNetwork3Phase dsNet = (DStabNetwork3Phase) simuCtx.getDStabilityNet();
+
+			// modify the generators, if needed
+//			DStab3PGen gen1 = dsNet.getBus("Bus1").getContributeGenList().get(0);
+			DStabGenImpl gen1 = (DStabGenImpl) dsNet.getBus("Bus1").getContributeGenList().get(0);
+			DStabGenImpl gen2 = (DStabGenImpl) dsNet.getBus("Bus2").getContributeGenList().get(0);
+			DStabGenImpl gen3 = (DStabGenImpl) dsNet.getBus("Bus3").getContributeGenList().get(0);
+			//gen3.setGen(new Complex(0.2, 0.0665));
+			//gen3.getParentBus().initContributeGen();
+			
+			gen2.setStatus(false);
+		
+			gen2.getParentBus().initContributeGen(false);
+			
+			((BaseDStabBus)gen2.getParentBus()).resetSeqEquivLoad();
+			
+
+			
+
+			/*
+			 * create the 1-phase AC model
+			 */
+
+			DStab3PBus bus5 = (DStab3PBus) dsNet.getBus("Bus5");
+
+
+
+			DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
+			LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
+			assertTrue(aclfAlgo.loadflow());
+			System.out.println(AclfOutFunc.loadFlowSummary(dsNet));
+
+			dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
+			dstabAlgo.setSimuStepSec(0.005d);
+			dstabAlgo.setTotalSimuTimeSec(10);
+
+			// dstabAlgo.setRefMachine(dsNet.getMachine("Bus1-mach1"));
+
+			// applied the event
+//			dsNet.addDynamicEvent(DStabObjectFactory.createBusFaultEvent("Bus9", dsNet, SimpleFaultCode.GROUND_3P,
+//					new Complex(0.0), null, 0.1d, 0.05), "3phaseFault@Bus5");
+//			 Generator trip event
+			dsNet.addDynamicEvent(DStabObjectFactory.createGeneratorTripEvent("Bus3", "1", dsNet, 1),
+					"Bus1_Mach1_trip_1sec");
+
+			StateMonitor sm = new StateMonitor();
+			sm.addGeneratorStdMonitor(new String[] { "Bus1-mach1", "Bus2-mach1", "Bus3-mach1" });
+			sm.addBusStdMonitor(new String[] { "Bus5", "Bus4", "Bus1" });
+
+			// set the output handler
+			dstabAlgo.setSimuOutputHandler(sm);
+			dstabAlgo.setOutPutPerSteps(5);
+			// dstabAlgo.setRefMachine(dsNet.getMachine("Bus1-mach1"));
+
+			IpssLogger.getLogger().setLevel(Level.WARNING);
+
+			PerformanceTimer timer = new PerformanceTimer(IpssLogger.getLogger());
+
+			// Must use this dynamic event process to modify the YMatrixABC
+			dstabAlgo.setDynamicEventHandler(new DynamicEventProcessor3Phase());
+
+			if (dstabAlgo.initialization()) {
+				System.out.println(ThreePhaseAclfOutFunc.busLfSummary(dsNet));
+				System.out.println(AclfOutFunc.loadFlowSummary(dsNet));
+				System.out.println(dsNet.getMachineInitCondition());
+
+				System.out.println("Running 3Phase DStab simulation ...");
+				timer.start();
+				// dstabAlgo.performSimulation();
+
+				while (dstabAlgo.getSimuTime() <= dstabAlgo.getTotalSimuTimeSec()) {
+
+					dstabAlgo.solveDEqnStep(true);
+				}
+			}
+
+			System.out.println(sm.toCSVString(sm.getBusAngleTable()));
+			System.out.println(sm.toCSVString(sm.getBusVoltTable()));
+	    }
+
 	private static DStabNetwork3Phase createIEEE9Bus() throws InterpssException{
 		
 		
