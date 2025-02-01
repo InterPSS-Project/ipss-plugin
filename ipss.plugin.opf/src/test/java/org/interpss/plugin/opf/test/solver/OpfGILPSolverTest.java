02ddf0e91be6222aab481764336883cc6cc12437
power4all <thuang@users.noreply.github.com>
2025-02-01 00:34:02 -0700

    update and add opf mapper and solver test

---
+++ ipss.plugin.opf/src/test/java/org/interpss/plugin/opf/test/solver/OpfGILPSolverTest.java
@@ -0,0 +1,14 @@
+package org.interpss.plugin.opf.test.solver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OpfGILPSolverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
