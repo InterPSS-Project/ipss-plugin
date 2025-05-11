4986031b1985be51089f549fe2e3700082222dee
ezzhou <ipss.cntester@gmail.com>
2025-05-11 17:32:19 +0800

    added a json comparison test case.

---
+++ ipss.test.plugin.core/src/test/java/org/interpss/core/adapter/ieee/IEEE14BusJsonCompareTest.java
@@ -0,0 +1,62 @@
+ /*
+  * @(#)Test_IEEECommonFormat_Comma.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.adapter.ieee;
+
+import org.interpss.CorePluginFactory;
+import org.interpss.CorePluginTestSetup;
+import org.interpss.fadapter.IpssFileAdapter;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.interpss.util.AclfNetJsonComparator;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.adpter.AclfSwingBusAdapter;
+import com.interpss.core.algo.LoadflowAlgorithm;
+
+public class IEEE14BusJsonCompareTest extends CorePluginTestSetup {
+	@Test 
+	public void testCase1() throws Exception {
+		AclfNetwork aclfNet = CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
+				.load("testData/adpter/ieee_format/ieee14_comma.ieee")
+				.getAclfNet();	
+		
+		String jsonFile = "testdata/json/ieee14Bus.json";
+		
+		// output aclfNet state to json file
+		//FileUtil.writeText2File(jsonFile, new AclfNetworkState(aclfNet).toString());
+		
+		assertTrue("", new AclfNetJsonComparator("IEEE Common format ieee14Bus")
+							.compareJson(aclfNet, new File(jsonFile)));
+	}
+}
+
