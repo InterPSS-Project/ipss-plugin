b3435e78f6f13b286dc22f59d114ff65d814f99d
ezzhou <ipss.cntester@gmail.com>
2025-07-09 14:41:09 +0800

    updated the PSSELfGControlConfig class.

--- ipss.plugin.core/src/main/java/org/interpss/plugin/aclf/PSSELfGControlConfig.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/aclf/PSSELfGControlConfig.java
@@ -17,12 +17,18 @@ public class PSSELfGControlConfig implements Consumer<LfAdjustAlgorithm>{
 	public static final int SwitchedShunt_Continuous = 2;
 	public static final int SwitchedShunt_Discrete = 3;
 
-	public static final int TapControl_LockTaps = 1;
-	public static final int TapControl_Direct = 2;
-	public static final int TapControl_Stepping = 3;
+	public static final int TapControl_LockTaps = 4;
+	public static final int TapControl_Direct = 5;
+	public static final int TapControl_Stepping = 6;
+	
+	public static final int VarLimit_Ignore = 7;
+	public static final int VarLimit_Immediate = 8;
+	public static final int VarLimit_Automatic = 9;
+	public static final int VarLimit_CertainItr = 10;
 	
 	public int gCtrlSwitchedShunt = SwitchedShunt_Discrete;
 	public int gCtrlTapControl = TapControl_Stepping;
+	public int gCtrlVarLimit = VarLimit_Automatic;
 	
 	/**
 	 * Constructor
@@ -36,12 +42,17 @@ public class PSSELfGControlConfig implements Consumer<LfAdjustAlgorithm>{
 	@Override
 	public void accept(LfAdjustAlgorithm lfAdjAlgo) {
   		lfAdjAlgo.getLfAlgo().getAclfNet().getBusList().forEach(bus -> {
-  			if (bus.isSwitchedShunt()) {
+  			if (bus.isSwitchedShunt()) {   // Switched shunt control and Static var compensator control
   				SwitchedShunt swShunt = bus.getSwitchedShunt();
   				swShunt.setControlMode(
   						this.gCtrlSwitchedShunt == SwitchedShunt_LockAll? AclfAdjustControlMode.FIXED : 
   							this.gCtrlSwitchedShunt == SwitchedShunt_Continuous? AclfAdjustControlMode.CONTINUOUS : 
   								AclfAdjustControlMode.DISCRETE);
+  			}
+  			else if (bus.isPQBusLimit() || bus.isRemoteQBus()) {
+  				if (this.gCtrlVarLimit == VarLimit_Ignore) {
+  					bus.getBusControl().setControlStatus(false);
+  				} 
   			}
   		});	
   		
