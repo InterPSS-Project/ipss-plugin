2ccc7c1b98d0994a1358a43052a1d8fb548163c8
ezzhou <ipss.cntester@gmail.com>
2025-07-14 10:09:38 +0800

    updated the ezro Z branch test cases.

--- ipss.test.plugin.core/src/test/java/org/interpss/core/zeroz/IEEE14ZeroZBranchAclfTest.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/core/zeroz/IEEE14ZeroZBranchAclfTest.java
@@ -48,7 +48,7 @@ public class IEEE14ZeroZBranchAclfTest extends CorePluginTestSetup {
 	@Test 
 	public void checkData_test() throws  InterpssException {
 		// Create an AclfNetwork object
-		AclfNetwork net = IpssAdapter.importAclfNet("testData/odm/Ieee14Bus_breaker.xml")
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/odm/zeroz/Ieee14Bus_breaker_1.xml")
 				.setFormat(IpssAdapter.FileFormat.IEEE_ODM)
 				.load()
 				.getImportedObj();
@@ -69,7 +69,7 @@ public class IEEE14ZeroZBranchAclfTest extends CorePluginTestSetup {
 	@Test 
 	public void zeroZProcessing_test() throws  InterpssException {
 		// Create an AclfNetwork object
-		AclfNetwork net = IpssAdapter.importAclfNet("testData/odm/Ieee14Bus_breaker.xml")
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/odm/zeroz/Ieee14Bus_breaker_1.xml")
 				.setFormat(IpssAdapter.FileFormat.IEEE_ODM)
 				.load()
 				.getImportedObj();
@@ -113,8 +113,8 @@ public class IEEE14ZeroZBranchAclfTest extends CorePluginTestSetup {
   		AclfSwingBusAdapter swing = swingBus.toSwingBus();
 		//System.out.println(swing.getGenResults(UnitType.PU).getReal());
 		//System.out.println(swing.getGenResults(UnitType.PU).getImaginary());
- 		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.32393)<0.0001);
-  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.16549)<0.0001);
+ 		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.23958)<0.0001);
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.15687)<0.0001);
     }
 }
 
