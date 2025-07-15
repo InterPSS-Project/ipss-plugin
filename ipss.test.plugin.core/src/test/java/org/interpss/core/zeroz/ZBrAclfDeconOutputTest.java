57db43f78304044e26baa153b5ab20c48a32d795
ezzhou <ipss.cntester@gmail.com>
2025-07-15 21:44:33 +0800

    added a new test case.

---
+++ ipss.test.plugin.core/src/test/java/org/interpss/core/zeroz/ZBrAclfDeconOutputTest.java
@@ -0,0 +1,87 @@
+ /*
+  * @(#)SampleLoadflow.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.zeroz;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.CorePluginTestSetup;
+import org.interpss.display.AclfOutFunc;
+import org.interpss.numeric.datatype.ComplexFunc;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.interpss.plugin.pssl.plugin.IpssAdapter;
+import org.junit.Test;
+
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.adpter.AclfSwingBusAdapter;
+import com.interpss.core.algo.LoadflowAlgorithm;
+import com.interpss.core.funcImpl.zeroz.AclfNetZeroZBranchHelper;
+import com.interpss.core.funcImpl.zeroz.AclfNetZeroZDeconsolidator;
+import com.interpss.core.funcImpl.zeroz.ZbrLfResultUtil;
+
+
+// ZeroZBranch Mark : IEEE14Bus Zero Z Branch Test
+public class ZBrAclfDeconOutputTest extends CorePluginTestSetup {
+	@Test 
+	public void test() throws  InterpssException {
+		// Create an AclfNetwork object
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/odm/zeroz/Ieee14Bus_breaker.xml")
+				.setFormat(IpssAdapter.FileFormat.IEEE_ODM)
+				.load()
+				.getImportedObj();
+	  	//System.out.println(net.net2String());
+
+		/*
+		 * process zeroZ branches by merging the zeroZ branches and connected buses to the retained bus
+		 */
+		new AclfNetZeroZBranchHelper(net).consolidate();
+				
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.loadflow();
+  		//System.out.println(net.net2String());
+	  	
+	  	//Check if loadflow has converged
+  		assertTrue(net.isLfConverged());
+  		
+  		// See IEEE14Bus_odm_Test.java for the expected values
+  		AclfBus swingBus = (AclfBus)net.getBus("Bus1");
+  		AclfSwingBusAdapter swing = swingBus.toSwingBus();
+		//System.out.println(swing.getGenResults(UnitType.PU).getReal());
+		//System.out.println(swing.getGenResults(UnitType.PU).getImaginary());
+ 		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.3239)<0.0001);
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.1654)<0.0001);
+  		  		
+  		// Deconsolidate the network, i.e., restore the zeroZ branches and connected buses to the original state
+  		new AclfNetZeroZDeconsolidator(net).deconsolidate(true);
+  		
+  		System.out.println(AclfOutFunc.loadFlowSummary(net));
+    }
+}
