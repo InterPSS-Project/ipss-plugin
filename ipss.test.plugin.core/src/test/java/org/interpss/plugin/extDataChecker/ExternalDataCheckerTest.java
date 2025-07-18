a50d2e9ab0c14b8aa95cdb912d7fbf37f679321f
ezzhou <ipss.cntester@gmail.com>
2025-07-18 15:27:08 +0800

    PSSEExternalDataChecker implementation, first-cut.

--- ipss.test.plugin.core/src/test/java/org/interpss/plugin/extDataChecker/ExternalDataCheckerTest.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/plugin/extDataChecker/ExternalDataCheckerTest.java
@@ -1,27 +1,3 @@
- /*
-  * @(#)SVCTest.java   
-  *
-  * Copyright (C) 2006 www.interpss.org
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-  * as published by the Free Software Foundation; either version 2.1
-  * of the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * @Author Mike Zhou
-  * @Version 1.0
-  * @Date 07/15/2007
-  * 
-  *   Revision History
-  *   ================
-  *
-  */
-
 package org.interpss.plugin.extDataChecker;
 
 import static org.junit.Assert.assertTrue;
@@ -50,7 +26,7 @@ public class ExternalDataCheckerTest extends CorePluginTestSetup {
 		
 		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
 		algo.loadflow(new PSSEExternalDataChecker(aclfNet -> {
-			// add data checker initialization logic here if needed
+			// add data checker logic after the initialization here if needed
 		}));
 		
 		assertTrue(net.isLfConverged());
