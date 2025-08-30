a933dd715babdf5a1d759290bd66c1039b216e10
ezzhou <ipss.cntester@gmail.com>
2025-08-30 19:22:50 +0800

    feat: introduce CMathCustomSolverLoadflow and related classes for enhanced loadflow calculations

---
+++ ipss.sample/src/main/java/org/interpss/sample/aclf/customSolver/impl/CMathSparseEqnDoubleSolver.java
@@ -0,0 +1,128 @@
+package org.interpss.sample.aclf.customSolver.impl;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.interpss.numeric.exp.IpssNumericException;
+import org.interpss.numeric.sparse.ISparseEqnDouble;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.sparse.DoubleSEqnElem;
+import com.interpss.core.sparse.DoubleSEqnRow;
+import com.interpss.core.sparse.impl.AbstractSparseEqnDoubleImpl;
+import com.interpss.core.sparse.solver.ISparseEqnSolver;
+
+
+/**
+ * Sparse Equation of data type double for solving the [A]X=B problem. 
+   using the apache common math lib. It is for illustrating extension implementation.
+ */
+public class CMathSparseEqnDoubleSolver extends AbstractSparseEqnDoubleImpl implements ISparseEqnSolver {
+	private static final Logger log = LoggerFactory.getLogger(CoreObjectFactory.class);
+	
+	// the sparse equation object to be solved
+	private AbstractSparseEqnDoubleImpl eqn;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param n the dimension of the equation
+	 */
+	public CMathSparseEqnDoubleSolver(int n) {
+		super(n);
+		log.info("dimension = " + n);
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param eqn the sparse equation object to be solved
+	 */
+	public CMathSparseEqnDoubleSolver(ISparseEqnDouble eqn) {
+		super(eqn.getDimension());
+		this.eqn = (AbstractSparseEqnDoubleImpl)eqn;
+		//System.out.println("eqn: " + eqn);
+	}
+		
+	@Override
+	public void solveEqn() {
+		// build the [A] matrix
+		RealMatrix A = buildAMatrix();
+
+		// do LU decomposition
+		LUDecomposition lu = new LUDecomposition(A);
+		
+		// build the {B} vector
+		int rows = this.eqn.getDimension(IndexType.Row);
+		RealVector B = new ArrayRealVector(rows);
+		for ( int i = 0; i < rows; i++ ) {
+			B.setEntry(i, this.eqn.getElem(i).bi);
+		}
+		
+		// solve the equation
+		RealVector X = lu.getSolver().solve(B);
+		
+		// set the solution back to the eqn B
+		this.eqn.setBVector(X.toArray());
+	}
+	
+	/**
+	 * Solve the equation [A]{x}={b}, return the solution {x}
+	 * 
+	 * @param b the right-hand side vector {b}
+	 * @return the solution vector {x}
+	 * @throws IpssNumericException
+	 */
+	@Override
+	public double[] solveEqn(double[] b) throws IpssNumericException {
+		// build the [A] matrix
+		RealMatrix A = buildAMatrix();
+		
+		// do LU decomposition
+		LUDecomposition lu = new LUDecomposition(A);
+		
+		// build the {B} vector
+		ArrayRealVector B = new ArrayRealVector(b);
+		
+		// solve the equation
+		return lu.getSolver().solve(B).toArray();
+	}
+	
+	@Override
+	public boolean factorization( final double tolerance) throws IpssNumericException {
+		// no need to do factorization here
+		 return true;
+	}
+	
+	/**
+	 * Build the [A] matrix from the lfEqen.
+	 * 
+	 */
+	protected RealMatrix buildAMatrix() {
+		//System.out.println("\n\neqn: " + eqn);
+		
+		int rows = this.eqn.getDimension(IndexType.Row);
+		
+		RealMatrix A = new Array2DRowRealMatrix( rows, rows );
+		// copy data from the Sparse eqn to the [A] matrix
+		for ( int i = 0; i < rows; i++ ) {
+			final DoubleSEqnRow ai = this.eqn.getElem(i);
+			A.setEntry(i, i, ai.aii);
+			for ( DoubleSEqnElem aij : ai.aijList )  {
+				A.setEntry(i, aij.j, aij.aij);
+			}
+		}
+		
+		//System.out.println("A = " + this.A.toString());
+		return A;
+	}
+	
+	@Override
+	public void setMatrixDirty() {
+		// do nothing
+	}
+}
---
+++ ipss.sample/src/main/java/org/interpss/sample/aclf/customSolver/impl/CMathSparseEqnMatrix2x2Impl.java
@@ -0,0 +1,21 @@
+package org.interpss.sample.aclf.customSolver.impl;
+
+import com.interpss.core.sparse.impl.AbstractSparseEqnMatrix2x2Impl;
+import com.interpss.core.sparse.solver.SparseEqnSolverFactory;
+
+/**
+ * CommonMath Sparse Equation of data type 2x2 matrix for solving the [A]X=B problem. 
+ * To outside, the index number is from 0 to n-1
+ */
+
+public class CMathSparseEqnMatrix2x2Impl extends AbstractSparseEqnMatrix2x2Impl {
+	/** Constructor
+	 * 
+	 * @param n the dimension of the equation
+	 */
+	public CMathSparseEqnMatrix2x2Impl(int n) {
+		super(n);
+		this.solver = new SparseEqnSolverFactory().createSparseEqnDoubleSolver(this);
+		setDimension(n);
+	}
+}
