32885702973a03b3c2f683738375d87a0457eaf9
ezzhou <ipss.cntester@gmail.com>
2025-08-31 12:06:35 +0800

    feat: add CMath custom solver implementations for AC system fault analysis

--- ipss.sample/src/main/java/org/interpss/sample/customSolver/impl/CMathSparseEqnComplexMatrix3x3.java
+++ ipss.sample/src/main/java/org/interpss/sample/customSolver/impl/CMathSparseEqnComplexMatrix3x3.java
@@ -7,9 +7,9 @@ import com.interpss.core.sparse.solver.ISparseEqnSolver;
  * CSJ Sparse Equation of data type complex3x3 for solving the [A]X=B problem. 
  * To outside, the index number is from 0 to n-1
  */
-public class CMathparseEqnComplexMatrix3x3 extends AbstractSparseEqnComplexMatrix3x3Impl<ISparseEqnSolver> {
+public class CMathSparseEqnComplexMatrix3x3 extends AbstractSparseEqnComplexMatrix3x3Impl<ISparseEqnSolver> {
 	
-	public CMathparseEqnComplexMatrix3x3(int n) {
+	public CMathSparseEqnComplexMatrix3x3(int n) {
 		super(n);
 		cplxMatrix = new CMathSparseEqnComplex(3*n);
 	}
