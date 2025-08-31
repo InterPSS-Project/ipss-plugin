5524b58978e286f23b0b281d8427963e36f8058d
ezzhou <ipss.cntester@gmail.com>
2025-08-31 10:33:38 +0800

    feat: add Common Math complex sparse equation solver implementations for [A]X=B problem

---
+++ ipss.sample/src/main/java/org/interpss/sample/aclf/customSolver/impl/CMathSparseEqnComplex.java
@@ -0,0 +1,28 @@
+package org.interpss.sample.aclf.customSolver.impl;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.numeric.exp.IpssNumericException;
+
+import com.interpss.core.sparse.impl.AbstractSparseEqnComplexImpl;
+import com.interpss.core.sparse.solver.ISparseEqnSolver;
+import com.interpss.core.sparse.solver.SparseEqnSolverFactory;
+
+/**
+ * Common Math Sparse Equation of data type complex for solving the [A]X=B problem. 
+ * To outside, the index number is from 0 to n-1
+ */
+public class CMathSparseEqnComplex extends AbstractSparseEqnComplexImpl<ISparseEqnSolver> {
+	/** Constructor
+	 * 
+	 * @param n the dimension of the equation
+	 */
+	public CMathSparseEqnComplex(int n) {
+		super(n);
+		this.solver = new SparseEqnSolverFactory().createSparseEqnComplexSolver(this);
+	}
+
+	@Override
+	public Complex[] solveLUedEqn(Complex[] b) throws IpssNumericException {
+		throw new IpssNumericException("Function not implemented");
+	}
+}
