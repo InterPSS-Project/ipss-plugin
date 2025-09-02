09691c24c43de7288b38e3d8bda460abeca43720
ezzhou <ipss.cntester@gmail.com>
2025-09-02 13:17:56 +0800

    test: add tests for high and low voltage scenarios in PSSE_IEEE9Bus_Result_Test

--- ipss.test.plugin.core/src/test/java/org/interpss/core/result/PSSE_IEEE9Bus_Result_Test.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/core/result/PSSE_IEEE9Bus_Result_Test.java
@@ -42,6 +42,68 @@ public class PSSE_IEEE9Bus_Result_Test extends CorePluginTestSetup {
   		// write the results to a file
   		FileUtil.writeText2File("output/ieee9_v31_result.json", resultStr);
 	}
+	
+	@Test
+	public void testHighVolt() throws Exception {
+		// load the test data V33
+		AclfNetwork netV33 = IpssAdapter.importAclfNet("testData/adpter/psse/v31/ieee9_v31.raw")
+				.setFormat(PSSE)
+				.setPsseVersion(PsseVersion.PSSE_31) 
+				.load()
+				.getImportedObj();
+		
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(netV33);
+	  	algo.loadflow();
+	  	
+  		assertTrue(netV33.isLfConverged());
+  		
+  		AclfResultContainer results = new AclfResultAdapter()
+  				.busComparator(AclfResultAdapter.busVoltHigherComparator)
+  				.numOfBusResults(5)
+  				.accept(netV33);
+  		
+  		assertTrue("Loadflow converged", results.isLoadflowConverged());
+  		assertTrue("", results.getBusResults().size() == 5);
+  		assertTrue(""+results.getBranchResults().size(), results.getBranchResults().size() == 9);
+  		
+  		// turn the results into a string
+  		String resultStr = results.toString();
+  		System.out.println("Higher Voltage: \n" + resultStr);
+  		
+  		// write the results to a file
+  		//FileUtil.writeText2File("output/ieee9_v31_result.json", resultStr);
+	}
+	
+	@Test
+	public void testLowVolt() throws Exception {
+		// load the test data V33
+		AclfNetwork netV33 = IpssAdapter.importAclfNet("testData/adpter/psse/v31/ieee9_v31.raw")
+				.setFormat(PSSE)
+				.setPsseVersion(PsseVersion.PSSE_31) 
+				.load()
+				.getImportedObj();
+		
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(netV33);
+	  	algo.loadflow();
+	  	
+  		assertTrue(netV33.isLfConverged());
+  		
+  		AclfResultContainer results = new AclfResultAdapter()
+  				.busComparator(AclfResultAdapter.busVoltLowerComparator)
+  				.numOfBusResults(5)
+  				.accept(netV33);
+  		
+  		assertTrue("Loadflow converged", results.isLoadflowConverged());
+  		assertTrue("", results.getBusResults().size() == 5);
+  		assertTrue(""+results.getBranchResults().size(), results.getBranchResults().size() == 9);
+  		
+  		// turn the results into a string
+  		String resultStr = results.toString();
+  		System.out.println("Lower Voltage: \n" + resultStr);
+  		
+  		// write the results to a file
+  		//FileUtil.writeText2File("output/ieee9_v31_result.json", resultStr);
+	}
 }
 
 
