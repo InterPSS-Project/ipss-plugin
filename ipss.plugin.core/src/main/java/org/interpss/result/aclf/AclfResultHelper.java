768997b632c6eb28c3d8da779bcebb8d0f6fec1e
ezzhou <ipss.cntester@gmail.com>
2025-09-04 09:23:39 +0800

    feat: implement methods to retrieve generator and load results in AclfResultHelper

--- ipss.plugin.core/src/main/java/org/interpss/result/aclf/AclfResultHelper.java
+++ ipss.plugin.core/src/main/java/org/interpss/result/aclf/AclfResultHelper.java
@@ -8,10 +8,14 @@ import org.interpss.numeric.datatype.Counter;
 import org.interpss.numeric.datatype.Unit.UnitType;
 import org.interpss.result.aclf.bean.AclfBranchInfo;
 import org.interpss.result.aclf.bean.AclfBusInfo;
+import org.interpss.result.aclf.bean.AclfGenInfo;
+import org.interpss.result.aclf.bean.AclfLoadInfo;
 import org.interpss.result.aclf.bean.AclfNetInfo;
 
 import com.interpss.core.aclf.AclfBranch;
 import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfGen;
+import com.interpss.core.aclf.AclfLoad;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.algo.AclfMethodType;
 
@@ -126,7 +130,151 @@ public class AclfResultHelper {
 		
 		return busInfo;
 	}
+	
+	/** 
+	 * create and return Gen Results 
+	 * 
+	 * @return List of AclfGenInfo objects containing bus-level gen results
+	 */
+	public List<AclfGenInfo> getGenResults() {
+		List<AclfGenInfo> genInfoList = new LinkedList<>();
+		createGenResults(genInfoList, (b1, b2) -> 0, 0);
+		return genInfoList;
+	}
+	
+	/** 
+	 * Populate the provided list with bus-level gen results.
+	 * 
+	 * @param genInfoList the list to populate with AclfGenInfo objects
+	 * @param genComparator comparator to sort gens before processing
+	 * @param maxNumOfGenResults maximum number of gen results to include, 0 means all
+	 */
+	public void createGenResults(List<AclfGenInfo> genInfoList, 
+					Comparator<AclfGen> genComparator, int maxNumOfGenResults) {
+		List<AclfGen> genList = new LinkedList<>();
+		this.aclfNet.getBusList().forEach(bus -> {
+			if (bus.isActive() && bus.getContributeGenList().size() > 0)
+				bus.getContributeGenList().forEach(gen -> {
+					if (gen.isActive())
+						genList.add(gen);
+				});
+		});
+		
+		Counter cnt = new Counter();
+		genList.stream()
+				.sorted(genComparator)
+				.filter(gen -> gen.isActive() && 
+						(cnt.increment() <= maxNumOfGenResults || maxNumOfGenResults == 0))
+				.forEach(gen -> {
+					AclfGenInfo genInfo = getGenResult(gen);
+					genInfoList.add(genInfo);
+				});
+	}
+	
+	/** 
+	 * Get results for a specific gen.
+	 * 
+	 * @param gen the gen object	
+	 * @return AclfGenInfo object containing results for the specified gen
+	 */
+	public AclfGenInfo getGenResult(AclfGen gen) {
+		AclfBus bus = (AclfBus)gen.getParentBus();
+		double baseMva = this.aclfNet.getBaseMva();
+					
+		AclfGenInfo genInfo = new AclfGenInfo();
+					
+		genInfo.setBusId(bus.getId());
+		genInfo.setGenId(gen.getId());
+		genInfo.setGenName(gen.getName());
+		/*
+		busInfo.setBusName(bus.getName());
+					
+		busInfo.setBusType(    // PV, PQ, Swing, Load
+				bus.isSwing()? "Swing" :
+					bus.isGenPV()? "PV" :
+						bus.isGen()? "PQ" : "Load"
+				);  
+		busInfo.setBusVoltageMagnitude(bus.getVoltageMag());
+		busInfo.setBusVoltageAnlgle(Math.toDegrees(bus.getVoltageAng()));
+		
+		busInfo.setBusGeneration(bus.calNetGenResults().multiply(baseMva));
+		busInfo.setBusLoad(bus.calNetLoadResults().multiply(baseMva));
+		*/
+		return genInfo;
+	}
 
+	/** 
+	 * create and return Load Results 
+	 * 
+	 * @return List of AclfLoadInfo objects containing bus-level load results
+	 */
+	public List<AclfLoadInfo> getLoadResults() {
+		List<AclfLoadInfo> loadInfoList = new LinkedList<>();
+		createLoadResults(loadInfoList, (b1, b2) -> 0, 0);
+		return loadInfoList;
+	}
+
+	/** 
+	 * Populate the provided list with bus-level load results.
+	 * 
+	 * @param loadInfoList the list to populate with AclfLoadInfo objects
+	 * @param loadComparator comparator to sort loads before processing
+	 * @param maxNumOfLoadResults maximum number of load results to include, 0 means all
+	 */
+	public void createLoadResults(List<AclfLoadInfo> loadInfoList, 
+					Comparator<AclfLoad> loadComparator, int maxNumOfLoadResults) {
+		List<AclfLoad> loadList = new LinkedList<>();
+		this.aclfNet.getBusList().forEach(bus -> {
+			if (bus.isActive() && bus.getContributeLoadList().size() > 0)
+				bus.getContributeLoadList().forEach(load -> {
+					if (load.isActive())
+						loadList.add(load);
+				});
+		});
+		
+		Counter cnt = new Counter();
+		loadList.stream()
+				.sorted(loadComparator)
+				.filter(load -> load.isActive() && 
+						(cnt.increment() <= maxNumOfLoadResults || maxNumOfLoadResults == 0))
+				.forEach(load -> {
+					AclfLoadInfo loadInfo = getLoadResult(load);
+					loadInfoList.add(loadInfo);
+				});
+	}
+	
+	/** 
+	 * Get results for a specific load.
+	 * 
+	 * @param load the gen object	
+	 * @return AclfLoadInfo object containing results for the specified load
+	 */
+	public AclfLoadInfo getLoadResult(AclfLoad load) {
+		AclfBus bus = (AclfBus)load.getParentBus();
+		double baseMva = this.aclfNet.getBaseMva();
+					
+		AclfLoadInfo loadInfo = new AclfLoadInfo();
+					
+		loadInfo.setBusId(bus.getId());
+		loadInfo.setLoadId(load.getId());
+		loadInfo.setLoadName(load.getName());
+		/*
+		busInfo.setBusName(bus.getName());
+					
+		busInfo.setBusType(    // PV, PQ, Swing, Load
+				bus.isSwing()? "Swing" :
+					bus.isGenPV()? "PV" :
+						bus.isGen()? "PQ" : "Load"
+				);  
+		busInfo.setBusVoltageMagnitude(bus.getVoltageMag());
+		busInfo.setBusVoltageAnlgle(Math.toDegrees(bus.getVoltageAng()));
+		
+		busInfo.setBusGeneration(bus.calNetGenResults().multiply(baseMva));
+		busInfo.setBusLoad(bus.calNetLoadResults().multiply(baseMva));
+		*/
+		return loadInfo;
+	}
+	
 	/** 
 	 * Create and return Branch Results 
 	 * 
