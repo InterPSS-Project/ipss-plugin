31579a42db11832bddc560ef21a216d08e13abb6
IpssDev-4 <ipss.cntester@gmail.com>
2025-10-13 13:25:40 +0800

    update the test case.

--- ipss.test.plugin.core/src/test/java/org/interpss/plugin/lfGCtrl/SwitchedShuntGControlTest.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/plugin/lfGCtrl/SwitchedShuntGControlTest.java
@@ -237,8 +237,8 @@ public class SwitchedShuntGControlTest extends CorePluginTestSetup {
 
 		System.out.println("Switched Shunt: " + swShunt);
 		assertTrue("", NumericUtil.equals(swShunt.getBInit(), 0.23637, 0.0001));
-		assertEquals(swShunt.getBActual(), 0.23637, 0.0001);
-		assertEquals(swShunt.getQ(), 0.23611  , 0.0001);
+		assertEquals(swShunt.getBActual(), 0.47274, 0.0001);
+		assertEquals(swShunt.getQ(), 0.47222  , 0.0001);
 		assertTrue("", swShunt.getControlMode() == AclfAdjustControlMode.DISCRETE);
 		assertEquals(swShunt.getVSpecified(), 1.0, 0.0001);
 		assertEquals(bus4.getVoltageMag(), 0.99945, 0.0001);
