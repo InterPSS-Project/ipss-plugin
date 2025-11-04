3b240f050bb352f1f1f691d2d4b074f880f35dfb
IpssDev-4 <ipss.cntester@gmail.com>
2025-11-04 11:20:24 +0800

    feat: introduce ContingencyResultRec class and update ContingencyResults to use generic type for convergence results

---
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/result/ContingencyResultRec.java
@@ -0,0 +1,9 @@
+package org.interpss.plugin.contingency.result;
+
+public class ContingencyResultRec {
+	public boolean isConverged;
+	
+	public ContingencyResultRec(boolean isConverged) {
+		this.isConverged = isConverged;
+	}
+}
