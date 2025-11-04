f8b5728f2b0be239f0d96a3b1c8d70876256454c
IpssDev-4 <ipss.cntester@gmail.com>
2025-11-04 14:56:31 +0800

    feat: introduce ContingencyResultContainer class to replace ContingencyResults for improved structure and clarity

--- ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/result/ContingencyResultContainer.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/result/ContingencyResultContainer.java
@@ -18,21 +18,21 @@ import com.interpss.state.aclf.AclfNetworkState;
  * 
  * @author InterPSS Team
  */
-public class ContingencyResults <TR extends ContingencyResultRec> {
-        private final Map<String, TR> convergenceResults;
+public class ContingencyResultContainer <TR extends ContingencyResultRec> {
+        private final Map<String, TR> caResults;
         private final long totalSuccessCount;
         private final int totalCases;
         private final long executionTimeMs;
         
-        public ContingencyResults(Map<String, TR> convergenceResults, long totalSuccessCount, 
+        public ContingencyResultContainer(Map<String, TR> caResults, long totalSuccessCount, 
                                int totalCases, long executionTimeMs) {
-            this.convergenceResults = new HashMap<>(convergenceResults);
+            this.caResults = caResults;
             this.totalSuccessCount = totalSuccessCount;
             this.totalCases = totalCases;
             this.executionTimeMs = executionTimeMs;
         }
         
-        public Map<String, TR> getConvergenceResults() { return convergenceResults; }
+        public Map<String, TR> getConvergenceResults() { return caResults; }
         public long getTotalSuccessCount() { return totalSuccessCount; }
         public int getTotalCases() { return totalCases; }
         public long getExecutionTimeMs() { return executionTimeMs; }
