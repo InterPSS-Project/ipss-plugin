6bc0cd94030075ae84ad21fae2660f47ca528081
IpssDev-4 <ipss.cntester@gmail.com>
2025-11-06 13:34:59 +0800

    refactor: replace branch and bus number calculations with sortNumber for consistency in AclfNet optimizers

--- ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/algo/util/AclfNetSensHelper.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/algo/util/AclfNetSensHelper.java
@@ -11,6 +11,7 @@ import com.interpss.core.aclf.AclfBranch;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.algo.dclf.DclfMethod;
 import com.interpss.core.algo.dclf.SenAnalysisAlgorithm;
+import com.interpss.core.algo.dclf.SenAnalysisType;
 import com.interpss.core.common.ReferenceBusException;
 
 
@@ -40,16 +41,23 @@ public class AclfNetSensHelper {
 	 * @return
 	 */
 	public float[][] calSen(){
-		setNetBusBranchNumber(aclfNet);
-		float[][] senMatrix = new float[aclfNet.getNoActiveBus()][aclfNet.getNoActiveBranch()];
 		SenAnalysisAlgorithm dclfAlgo = DclfAlgoObjectFactory.createSenAnalysisAlgorithm(aclfNet);
-		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);
+		setNetBusBranchNumber();
+		try {
+			dclfAlgo.getDclfSolver().prepareBMatrix(SenAnalysisType.PANGLE);
+		} catch (InterpssException | IpssNumericException e) {
+			log.error(e.toString());
+		}
+		
+		float[][] senMatrix = new float[aclfNet.getNoActiveBus()][aclfNet.getNoActiveBranch()];
 		aclfNet.getBusList().parallelStream().filter(bus -> bus.isActive()).forEach(bus -> {
 			try {
 				double[] dblAry = dclfAlgo.getDclfSolver().getSenPAngle(bus.getId());
-				aclfNet.getBranchList().stream().filter(branch -> branch.isActive() && branch.getNumber() != 0)
+				aclfNet.getBranchList().stream().filter(branch -> branch.isActive())
 						.forEach(branch -> {
-							senMatrix[(int) (bus.getNumber() - 1)][(int) (branch.getNumber() - 1)] = calSen(dblAry, branch);
+							double fAng = branch.getFromAclfBus().isRefBus()? 0.0 : dblAry[branch.getFromAclfBus().getSortNumber()];
+							double tAng = branch.getToAclfBus().isRefBus()? 0.0 : dblAry[branch.getToAclfBus().getSortNumber()];
+							senMatrix[bus.getSortNumber()][branch.getSortNumber()] = (float)(-branch.b1ft() * (fAng - tAng));
 						});
 			} catch (InterpssException | IpssNumericException | ReferenceBusException e) {
 				log.error(e.toString());
@@ -57,26 +65,20 @@ public class AclfNetSensHelper {
 		});
 		return senMatrix;
 	}
-
-	private float calSen(double[] dblAry, AclfBranch branch) {
-		double fAng = branch.getFromAclfBus().isRefBus()? 0.0 : dblAry[branch.getFromAclfBus().getSortNumber()];
-		double tAng = branch.getToAclfBus().isRefBus()? 0.0 : dblAry[branch.getToAclfBus().getSortNumber()];
-		return  (float)(-branch.b1ft() * (fAng - tAng));
-	}
 	
 	/**
 	 * set AclfNet bus number and branch number
 	 * 
 	 * @param aclfNet
 	 */
-	private static void setNetBusBranchNumber(AclfNetwork aclfNet) {
+	private void setNetBusBranchNumber() {
 		Counter cnt = new Counter();
 
 		aclfNet.getBusList().stream()
 			.filter(bus -> bus.isActive())
 			.forEach(bus -> {
-				cnt.increment();
-				bus.setNumber(cnt.getCount());
+				//cnt.increment();
+				bus.setSortNumber(cnt.getCountThenIncrement());
 			});
  
 		cnt.reset();
@@ -84,8 +86,8 @@ public class AclfNetSensHelper {
 		aclfNet.getBranchList().stream().filter(branch -> branch.isActive() && branch.getFromBus() != null
 				&& branch.getFromBus().isActive() && branch.getToBus() != null && branch.getToBus().isActive())
 				.forEach(branch -> {
-					cnt.increment();
-					branch.setNumber(cnt.getCount());
+					//cnt.increment();
+					branch.setSortNumber(cnt.getCountThenIncrement());
 				});
 	}
 }
