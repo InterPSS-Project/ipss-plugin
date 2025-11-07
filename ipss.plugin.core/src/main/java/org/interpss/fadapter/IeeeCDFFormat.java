3e3867f44914b4551ae6ae5a7455f64b71ceb14d
IpssDev-4 <ipss.cntester@gmail.com>
2025-11-07 11:18:35 +0800

    feat: implement logging and cleanup of empty contribution generators and loads in IeeeCDFFormat

--- ipss.plugin.core/src/main/java/org/interpss/fadapter/IeeeCDFFormat.java
+++ ipss.plugin.core/src/main/java/org/interpss/fadapter/IeeeCDFFormat.java
@@ -26,8 +26,16 @@ package org.interpss.fadapter;
 
 import org.ieee.odm.ODMFileFormatEnum;
 import org.interpss.fadapter.impl.IpssFileAdapterBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.interpss.common.exp.InterpssException;
 import com.interpss.common.msg.IPSSMsgHub;
+import com.interpss.core.aclf.AclfGen;
+import com.interpss.core.aclf.AclfGenCode;
+import com.interpss.core.aclf.AclfLoad;
+import com.interpss.core.aclf.AclfLoadCode;
+import com.interpss.simu.SimuContext;
 
 /**
  *  Custom input file adapter for IEEE Common Format. It loads a data file in the format and create an
@@ -35,6 +43,7 @@ import com.interpss.common.msg.IPSSMsgHub;
  */
 
 public class IeeeCDFFormat extends IpssFileAdapterBase {
+    private static final Logger log = LoggerFactory.getLogger(IeeeCDFFormat.class);
 	
 	public IeeeCDFFormat(IPSSMsgHub msgHub) {
 		super(msgHub, ODMFileFormatEnum.IeeeCDF);
@@ -42,5 +51,35 @@ public class IeeeCDFFormat extends IpssFileAdapterBase {
 	
 	public IeeeCDFFormat(IPSSMsgHub msgHub, IpssFileAdapter.Version v) {
 		super(msgHub, v == IpssFileAdapter.Version.IEEECDF? ODMFileFormatEnum.IeeeCDF : ODMFileFormatEnum.IeeeCDFExt1);
+	}
+	
+	@Override
+	public void load(final SimuContext simuCtx, final String filepath, boolean debug, String outfile) throws InterpssException {
+		super.load(simuCtx, filepath, debug, outfile);	
+		log.debug("IEEECDF Format file " + filepath + " loaded successfully.");
+		
+		// since the IEEE CDF format is not a contributionGen/Load model, we need to 
+		// remove the empty contributionGen/Load objects 
+		simuCtx.getAclfNet().getBusList().forEach(bus -> {
+			if (bus.getContributeGenList().size() > 0) {
+				AclfGen gen = bus.getContributeGenList().get(0);
+				if (bus.getGenCode() == AclfGenCode.GEN_PQ) {
+					if (gen.getGen().abs() == 0.0) {
+						bus.getContributeGenList().remove(0);
+						log.debug("Removed empty contributionGen " + gen.getId());
+					}
+				}
+			}
+			
+			if (bus.getContributeLoadList().size() > 0) {
+				AclfLoad load = bus.getContributeLoadList().get(0);
+				if (load.getCode() == AclfLoadCode.CONST_P) {
+					if (load.getLoadCP().abs() == 0.0) {
+						bus.getContributeLoadList().remove(0);
+						log.debug("Removed empty contributionLoad " + load.getId());
+					}
+				}				
+			}
+		});
 	}
 }
