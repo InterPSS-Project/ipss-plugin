1b5ba3a9b0eb9254f88303cd644413784bbd6d54
IpssDev-4 <ipss.cntester@gmail.com>
2025-11-07 16:34:33 +0800

    feat: add Sen2DMatrix class for 2D matrix operations with row and column indexing

---
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/algo/util/Sen2DMatrix.java
@@ -0,0 +1,64 @@
+package org.interpss.plugin.optadj.algo.util;
+
+public class Sen2DMatrix {
+	private double[][] data;
+	
+	private int[] rowIndex, colIndex;
+	
+	public Sen2DMatrix(int row, int col) {
+		this(row, col, true);
+	}
+	
+	public Sen2DMatrix(int row, int col, boolean initIndex) {
+		data = new double[row][col];
+		if (initIndex) {
+			rowIndex = new int[row];
+			colIndex = new int[col];
+			for (int i = 0; i < row; i++) {
+				rowIndex[i] = i;
+			}
+			for (int j = 0; j < col; j++) {
+				colIndex[j] = j;
+			}
+		}
+	}
+	
+	public double get(int row, int col) {
+		int r = rowIndex[row];
+		int c = colIndex[col];
+		if (r >= 0 && c >= 0)
+			return data[r][c];
+		else 
+			return 0.0;
+	}
+	
+	public void set(int row, int col, double value) {
+		int r = rowIndex[row];
+		int c = colIndex[col];
+		data[r][c] = value;
+	}
+	
+	public void setRowIndex(int[] rowIndex) {
+		this.rowIndex = rowIndex;
+	}
+	
+	public void setColIndex(int[] colIndex) {
+		this.colIndex = colIndex;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("row, col, rowIdx, colInx: " + data.length + ", " + data[0].length
+					+ ", " + rowIndex.length + ", " + colIndex.length + "\n");
+		for (int i = 0; i < rowIndex.length; i++) {
+			for (int j = 0; j < colIndex.length; j++) {
+				sb.append(String.format("%7.3f", get(i,j)));
+				if (j < colIndex.length - 1)
+					sb.append(", ");
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+}
