25317592026b0a651709ccef4637e5811bc5948b
IpssDev-4 <ipss.cntester@gmail.com>
2025-11-15 10:32:22 +0800

    feat: enhance Basenative simulatorJSonUpdater to accept a consumer for field name definitions

--- ipss.plugin.core/src/main/java/org/interpss/fadapter/export/psse/BasePSSEJSonUpdater.java
+++ ipss.plugin.core/src/main/java/org/interpss/fadapter/export/psse/BasePSSEJSonUpdater.java
@@ -4,6 +4,7 @@ package org.interpss.fadapter.export.psse;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * PSSE JSon format data updater
@@ -23,10 +24,24 @@ public abstract class BasePSSEJSonUpdater {
 	 * @param fieldDef the field definition list
 	 */
 	public BasePSSEJSonUpdater(List<String> fieldDef) {
+		this(fieldDef, null);
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param fieldDef field name definitions
+	 * @param consumer a consumer to add more field names
+	 */
+	public BasePSSEJSonUpdater(List<String> fieldDef, Consumer<List<String>> consumer) {
+		if (consumer != null)
+			consumer.accept(fieldDef);
+		
 		this.positionTable = new LinkedHashMap<>();
 		int cnt =0;
 		for (String s : fieldDef) {
 			this.positionTable.put(s.trim(), cnt++);
 		}
 	}
+	
 }
