03fed4c44e129bb26b2c498a39508f39d001ed94
IpssDev-4 <ipss.cntester@gmail.com>
2025-12-02 11:35:44 +0800

    refactor(exchange): replace BaseExchangeBean with BaseElemExchangeBean for bus and branch exchange info

--- ipss.plugin.core/src/main/java/org/interpss/plugin/exchange/bean/AclfBusExchangeInfo.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/exchange/bean/AclfBusExchangeInfo.java
@@ -6,7 +6,7 @@ package org.interpss.plugin.exchange.bean;
  * @author mzhou
  *
  */
-public class AclfBusExchangeInfo extends BaseExchangeBean {
+public class AclfBusExchangeInfo extends BaseElemExchangeBean {
 	// bus voltage results
 	public double[] volt_mag;   // in p.u.
 	public double[] volt_ang;   // in degree
