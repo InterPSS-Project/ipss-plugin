2d935421721361099222d61271cf6558c1520e06
IpssDev-4 <ipss.cntester@gmail.com>
2025-12-05 13:56:47 +0800

    feat: refactor imports to use InterPSS module and clean up unused code

--- ipss.plugin.py/sample/sample_psse.py
+++ ipss.plugin.py/sample/sample_psse.py
@@ -14,7 +14,7 @@ if str(project_root) not in sys.path:
     sys.path.append(str(project_root))
 
 #  Configure and Start the JVM
-from src.config.config_mgr import ConfigManager, JvmManager
+from src.config import ConfigManager, JvmManager
 
 # Load configuration file
 config_path=str(project_root / "config" / "config.json")
@@ -22,32 +22,25 @@ config = ConfigManager.load_config(config_path)
 # Initialize and start the JVM
 JvmManager.initialize_jvm(config)
 
+# import InterPSS modules
+from src.interpss import ipss
+
 # Load data and create the Network Model
-from src.adapter.input_adapter import PsseRawFileAdapter
 #from org.ieee.odm.adapter.psse.PSSEAdapter import PsseVersion
 
 file_path = str(script_dir.parent / "tests" / "testData" / "psse" / "IEEE9Bus" / "ieee9.raw")
-net = PsseRawFileAdapter.createAclfNet(file_path, PsseRawFileAdapter.version.PSSE_30)
+net = ipss.PsseRawFileAdapter.createAclfNet(file_path, ipss.PsseRawFileAdapter.version.PSSE_30)
 
 # InterPSS core related classes
-from com.interpss.core import LoadflowAlgoObjectFactory
-
-algo = LoadflowAlgoObjectFactory.createLoadflowAlgorithm(net)
+algo = ipss.LoadflowAlgoObjectFactory.createLoadflowAlgorithm(net)
 
 algo.loadflow()
 
-# InterPSS output related classes
-from org.interpss.display import AclfOutFunc
-
 # basic load flow results summary, showing the bus type, voltage magnitude and angle and bus net power  	
-print(AclfOutFunc.loadFlowSummary(net))
-
-# PSS/E output related classes
-from org.interpss.display.impl import AclfOut_PSSE
-from org.interpss.display.impl.AclfOut_PSSE import Format
+print(ipss.AclfOutFunc.loadFlowSummary(net))
 
 # print out more detailed power flow results in PSS/E style
-# print(AclfOut_PSSE.lfResults(net, PSSEOutFormat.GUI))
+print(ipss.AclfOut_PSSE.lfResults(net, ipss.PSSEOutFormat.GUI))
 
 # Shutdown JVM
 jpype.shutdownJVM()
