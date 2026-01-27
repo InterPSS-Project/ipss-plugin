f40d6b4f5b00b9098ed853f36f1a6ff863e79a40
IpssDev-4 <ipss.cntester@gmail.com>
2026-01-27 13:13:38 +0800

    feat: add sorting comparators and result extraction methods in AclfResultAdapter

--- ipss.plugin.core/src/main/java/org/interpss/plugin/result/AclfResultAdapter.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/result/AclfResultAdapter.java
@@ -12,6 +12,7 @@ import com.interpss.core.algo.AclfMethodType;
 public class AclfResultAdapter {	
 	public static final int MaxNumOfResults = 50;
 	
+	// Pre-defined comparators for common sorting needs
 	public static Comparator<AclfBus> busVoltHigherComparator = 
 						(b1, b2) -> b1.getVoltageMag() < b2.getVoltageMag()? 1 : -1; 
 	public static Comparator<AclfBus> busVoltLowerComparator = 
@@ -26,55 +27,109 @@ public class AclfResultAdapter {
 						
 	public static Comparator<AclfBranch> branchFlowLargerComparator = 
 						(b1, b2) -> b1.powerFrom2To().abs() < b2.powerFrom2To().abs()? 1 : -1; 
-								
+	
+	// default comparators for no sorting					
 	private Comparator<AclfBus> busComparator = (b1, b2) -> 0;
 	private Comparator<AclfGen> genComparator = (b1, b2) -> 0;
 	private Comparator<AclfLoad> loadComparator = (b1, b2) -> 0;
 	private Comparator<AclfBranch> branchComparator = (b1, b2) -> 0;
+	
+	// default number of results to be extracted
 	private int numOfBusResults = MaxNumOfResults;
 	private int numOfGenResults = MaxNumOfResults;
 	private int numOfLoadResults = MaxNumOfResults;
 	private int numOfBranchResults = MaxNumOfResults;
 	
+	/**
+	 * Default constructor
+	 */
 	public AclfResultAdapter() {
 		// Constructor
 	}
 	
+	/**
+	 * Set the bus comparator for sorting bus results
+	 * 
+	 * @param comparator
+	 * @return
+	 */
 	public AclfResultAdapter busComparator(Comparator<AclfBus> comparator) {
 		this.busComparator = comparator;
 		return this;
 	}
 	
+	/**
+	 * Set the generator comparator for sorting generator results
+	 * 
+	 * @param comparator
+	 * @return
+	 */
 	public AclfResultAdapter genComparator(Comparator<AclfGen> comparator) {
 		this.genComparator = comparator;
 		return this;
 	}
 	
+	/**
+	 * Set the load comparator for sorting load results
+	 * 
+	 * @param comparator
+	 * @return
+	 */
 	public AclfResultAdapter loadComparator(Comparator<AclfLoad> comparator) {
 		this.loadComparator = comparator;
 		return this;
 	}
 	
+	/**
+	 * Set the branch comparator for sorting branch results
+	 * 
+	 * @param comparator
+	 * @return
+	 */
 	public AclfResultAdapter branchComparator(Comparator<AclfBranch> comparator) {
 		this.branchComparator = comparator;
 		return this;
 	}
 	
+	/**
+	 * Set the number of bus results to be extracted
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public AclfResultAdapter numOfBusResults(int num) {
 		this.numOfBusResults = num;
 		return this;
 	}
 	
+	/**
+	 * Set the number of generator results to be extracted
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public AclfResultAdapter numOfGenResults(int num) {
 		this.numOfGenResults = num;
 		return this;
 	}
 	
+	/**
+	 * Set the number of load results to be extracted
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public AclfResultAdapter numOfLoadResults(int num) {
 		this.numOfLoadResults = num;
 		return this;
 	}
 	
+	/**
+	 * Set the number of branch results to be extracted
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public AclfResultAdapter numOfBranchResults(int num) {
 		this.numOfBranchResults = num;
 		return this;
