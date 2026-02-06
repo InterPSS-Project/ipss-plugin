10691adb8b42a23bc8931bd1fa335988dfa9237a
hao <duoluojind@163.com>
2026-02-06 11:32:32 +0800

    Revert "code refactor"
    This reverts commit 42275cfa1c0e34ac5410132bc6bbd9a05a7ffd7d.

--- ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/optimizer/GenStateOptimizer.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/optimizer/GenStateOptimizer.java
@@ -1,71 +1,32 @@
 
 package org.interpss.plugin.optadj.optimizer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.math3.optim.linear.Relationship;
-import org.interpss.plugin.optadj.optimizer.bean.BaseConstrainData;
-import org.interpss.plugin.optadj.optimizer.bean.GenConstrainData;
+import org.interpss.plugin.optadj.optimizer.bean.DeviceConstrainData;
 import org.interpss.plugin.optadj.optimizer.bean.SectionConstrainData;
 import org.ojalgo.optimisation.Expression;
-import org.ojalgo.optimisation.ExpressionsBasedModel;
-import org.ojalgo.optimisation.Optimisation;
-
 
 /**
  * 
  * @author Donghao.F
  * 
- * @date 2024年5月27日 下午5:19:38
  * 
  * 
  * 
  */
-public class GenStateOptimizer extends BaseStateOptimizer  {
+public class GenStateOptimizer extends BaseStateOptimizer{
 
-	List<GenConstrainData> genConstrainDataList = new ArrayList<GenConstrainData>();
-	
-	List<SectionConstrainData> secConstrainDataList = new ArrayList<SectionConstrainData>();
-	
-	double senLimit = 0.01;
+	private double senLimit = 0.05;
 
-	int genSize;
-	
-	Optimisation.Result result;
-	
-	ExpressionsBasedModel model;
-	
-	private double interfaceFactor = 1e4;
-	private double sectionFactor = 1e2;
-	public GenStateOptimizer() {
-		 model = new ExpressionsBasedModel();
-		
-	}
-
-	public void adConstraint(BaseConstrainData data) {
-		if (data instanceof GenConstrainData) {
-			genConstrainDataList.add((GenConstrainData) data);
-		} else {
-			secConstrainDataList.add((SectionConstrainData) data);
-		}
-	}
-	
 	public void optimize() {
-		//System.out.println("gen size:" + this.genConstrainDataList.size() + "," + "section size:"
-		//		+ this.secConstrainDataList.size());
+		System.out.println("gen constrain size:" + this.deviceConstrainDataList.size() + ", " + 
+							"section constrain size:"	+ this.secConstrainDataList.size());
 		 // Determine genSize
-        
-		Map<Integer, Double> weightMap = new HashMap<Integer, Double>();
-		genConstrainDataList.forEach(data -> {
-			if (data.getIndex() + 1 > genSize) {
-				genSize = data.getIndex() + 1;
-			}
-			weightMap.put(data.getIndex(), data.getWeight());
-		});
+        deviceConstrainDataList.forEach(data -> {
+            if (data.getIndex() + 1 > genSize) {
+                genSize = data.getIndex() + 1;
+            }
+        });
 		
         // Create variables: dGenP (genSize), dSecP (secConstrainDataList.size()), |dGenP| (genSize), |sum(dGenP)| (1)
         int totalVars = genSize + secConstrainDataList.size() + genSize + 1;
@@ -74,13 +35,11 @@ public class GenStateOptimizer extends BaseStateOptimizer  {
 		for (int i = 0; i < totalVars; i++) {
 			model.addVariable("v" + i).weight(0);
 		}
-		// 1. 发电机约束
-		for (GenConstrainData data : genConstrainDataList) {
-			Expression constraint = model.getExpression("gen_constraint_" + data.getIndex());
-			if (constraint == null) {
-				constraint = model.addExpression("gen_constraint_" + data.getIndex());
-				constraint.set(data.getIndex(), 1.0);
-			}
+		
+		for (DeviceConstrainData data : deviceConstrainDataList) {
+			final Expression constraint = model
+					.addExpression("gen_constraint_" + data.getIndex() + "_" + data.getLimit());
+            constraint.set(data.getIndex(), 1.0);
             
 			switch (data.getRelationship()) {
 			case LEQ:
@@ -94,21 +53,15 @@ public class GenStateOptimizer extends BaseStateOptimizer  {
 			}
         }
 		
-		// 2. 断面约束
+		// 2. 
         for (int i = 0; i < secConstrainDataList.size(); i++) {
             final SectionConstrainData data = secConstrainDataList.get(i);
-            final double[] senArray = data.getSenArray();
-			if (Arrays.stream(senArray).allMatch(value -> Math.abs(value) < senLimit)) {
-				continue;
-			}
-            
             final Expression constraint = model.addExpression("section_constraint_" + i);
             
-            
-			for (int j = 0; j < Math.min(senArray.length, genSize); j++) {
-				double sen = Math.abs(senArray[j]) < senLimit ? 0 : senArray[j];
-				constraint.set(j, sen);
-			}
+            final double[] senArray = data.getSenArray();
+            for (int j = 0; j < Math.min(senArray.length, genSize); j++) {
+                constraint.set(j, senArray[j]);
+            }
             
             if (data.getRelationship() == Relationship.LEQ) {
                 constraint.set(genSize + i, -1.0);
@@ -120,16 +73,14 @@ public class GenStateOptimizer extends BaseStateOptimizer  {
         }
 		
         
-		// 确保dSecP >= 0
+		// dSecP >= 0
 		for (int i = 0; i < secConstrainDataList.size(); i++) {
 			final Expression constraint = model.addExpression("dSecP_nonneg_" + i);
 			constraint.set(genSize + i, 1.0);
 			constraint.lower(0.0);
-			final SectionConstrainData data = secConstrainDataList.get(i);
-			constraint.upper(Math.abs(data.getLimit() - data.getValue()));
 		}
 		
-		 // 3. |xi| 约束
+		 // 3. |xi| 
         for (int i = 0; i < genSize; i++) {
             // 3.1 xi + wi >= 0
             final Expression constraint1 = model.addExpression("abs_constraint1_" + i);
@@ -149,7 +100,7 @@ public class GenStateOptimizer extends BaseStateOptimizer  {
             constraint3.lower(0.0);
         }
         
-		// 4. |x1+x2+....| 约束
+		// 4. |x1+x2+....| 
 		// 4.1 x1+x2 ... +w >= 0
 		final Expression sumConstraint1 = model.addExpression("sum_abs_constraint1");
 		for (int i = 0; i < genSize; i++) {
@@ -171,50 +122,19 @@ public class GenStateOptimizer extends BaseStateOptimizer  {
         sumConstraint3.set(totalVars - 1, 1.0);
         sumConstraint3.lower(0.0);
 
-        // 设置目标函数: |x1+x2+..| + (|x1| + |x2| +...) + factor* (|xLn|...) + factor*(s1+s2+...)
+        //  |x1+x2+..| + 0.5*(|x1| + |x2| +...) + 10*(s1+s2+...)
         final Expression objective = model.addExpression("Objective").weight(1.0);
-		for (int i = genSize; i < genSize + secConstrainDataList.size(); i++) {
-			objective.set(i, sectionFactor);
-		}
-		for (int i = genSize + secConstrainDataList.size(); i < totalVars - 1; i++) {
-			int index = i - (genSize + secConstrainDataList.size());
-			objective.set(i, weightMap.get(index));
-		}
-        objective.set(totalVars - 1, interfaceFactor );
-        
-//		System.out.println("totalVars:" + totalVars + " ,  expressions :" + model.getExpressions().size());
-        
-        // 求解问题
+        for (int i = genSize; i < genSize + secConstrainDataList.size(); i++) {
+            objective.set(i, 1.0/senLimit);
+        }
+        for (int i = genSize + secConstrainDataList.size(); i < totalVars - 1; i++) {
+            objective.set(i, 0.5);
+        }
+        objective.set(totalVars - 1, 1.0);
         result = model.minimise();
         
-        // 打印求解状态
 //        System.out.println("Optimisation status: " + result.getState());
 	}
-	
-	public double[] getPoint() {
-        return result.toRawCopy1D();
-    }
 
-    public double getValue() {
-        return result.getValue();
-    }
-
-	public double getSenLimit() {
-		return senLimit;
-	}
-
-	public void setSenLimit(double senLimit) {
-		this.senLimit = senLimit;
-	}
-
-	public void setInterfaceFactor(double interfaceFactor) {
-		this.interfaceFactor = interfaceFactor;
-	}
-
-	public void setSectionFactor(double sectionFactor) {
-		this.sectionFactor = sectionFactor;
-	};
-	
-	
 }
 
