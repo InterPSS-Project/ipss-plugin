61df4470af3718b5ba2ee3c2d73ab64fc6a4d2a7
IpssDev-4 <ipss.cntester@gmail.com>
2026-02-12 16:22:30 +0800

    feat(groovy): refactor Groovy code snippets for clarity and add new branch evaluation

--- ipss.plugin.core/src/sample/java/org/interpss/gvy/GvySample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/gvy/GvySample.java
@@ -16,11 +16,13 @@ import groovy.lang.GroovyShell;
 public class GvySample {
     public static void main(String[] args) throws InterpssException {
     	GroovyShell shell = new GroovyShell();
-    	String groovyCode = "def name = 'Alice'; println \"Hello, ${name}!\"; return name";
+    	String groovyCode = """
+    						def name = 'Alice'; 
+							println "Hello, ${name}!"; 
+							return name;
+						""";
     	Object result = shell.evaluate(groovyCode);
     	System.out.println("Result: " + result);
-    	
-		IpssCorePlugin.init();
 		
 		// load the IEEE-14 Bus system
 		AclfNetwork net = CorePluginFactory
@@ -32,21 +34,34 @@ public class GvySample {
 	  	algo.loadflow();
 	  	
 	  	AclfNetGvyScriptProcessor gvyProcessor = new AclfNetGvyScriptProcessor(net);
-    	groovyCode = "aclfnet.id = 'Modified';" +
-    				 "return 'Net name: ' + aclfnet.id; ";
+    	groovyCode = """
+    			aclfnet.id = 'Modified';
+    			return 'Net name: ' + aclfnet.id; 		 
+    		""";
     	result = gvyProcessor.evaluate(groovyCode);
     	System.out.println("Result: " + result);
     	
-    	groovyCode = "aclfnet.getBus('Bus14').loadP = 0.18;" +
-				 	 "return 'Bus load: ' + aclfnet.getBus('Bus14').loadP; ";
+    	groovyCode = """
+    			aclfnet.getBus('Bus14').loadP = 0.18;
+				return 'Bus load: ' + aclfnet.getBus('Bus14').loadP; 
+    		""";
 		result = gvyProcessor.evaluate(groovyCode);
 		System.out.println("Result: " + result);
 		
-    	groovyCode = //BaseGvyScriptProcessor.GVY_IMPORTS +
-    				 "bus = aclfnet.getBus('Bus14');" +
-    				 "load = bus.getContributeLoad('Bus14-L1');" +
-    				 "load.loadCP = new Complex(0.18, 0.07);" +
-				 	 "return 'Bus contribute load: ' + aclfnet.getBus('Bus14').getContributeLoad('Bus14-L1').loadCP; ";
+    	groovyCode = """
+    			branch = aclfnet.getBranch("Bus1", "Bus2", "1"); 
+    			branch.z = new Complex(0.02, 0.06 );
+				return 'Branch z : ' + aclfnet.getBranch("Bus1", "Bus2", "1").z; 
+    		""";
+		result = gvyProcessor.evaluate(groovyCode);
+		System.out.println("Result: " + result);
+		
+    	groovyCode = """
+    			bus = aclfnet.getBus("Bus14"); 
+    			load = bus.getContributeLoad("Bus14-L1"); 
+    			load.loadCP = new Complex(0.18, 0.07);
+				return 'Bus contribute load: ' + aclfnet.getBus("Bus14").getContributeLoad("Bus14-L1").loadCP; 
+    		""";
 		result = gvyProcessor.evaluate(groovyCode);
 		System.out.println("Result: " + result);
     }
--- ipss.test.plugin.core/src/test/java/org/interpss/core/script/gvy/GvyScriptEval_Test.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/core/script/gvy/GvyScriptEval_Test.java
@@ -1,32 +1,7 @@
- /*
-  * @(#)Test_IEEECommonFormat.java   
-  *
-  * Copyright (C) 2006 www.interpss.org
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-  * as published by the Free Software Foundation; either version 2.1
-  * of the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * @Author Mike Zhou
-  * @Version 1.0
-  * @Date 09/15/2006
-  * 
-  *   Revision History
-  *   ================
-  *
-  */
-
 package org.interpss.core.script.gvy;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import java.io.File;
 
 import org.apache.commons.math3.complex.Complex;
 import org.interpss.CorePluginFactory;
@@ -61,13 +36,24 @@ public class GvyScriptEval_Test extends CorePluginTestSetup {
 		//System.out.println("Result: " + result);
 		assertTrue("Bus load should be 0.18", NumericUtil.equals(net.getBus("Bus14").getLoadP(), 0.18, 1.0E-4));
 		
-    	groovyCode = "bus = aclfnet.getBus('Bus14');" +
-    				 "load = bus.getContributeLoad('Bus14-L1');" +
-    				 "load.loadCP = new Complex(0.18, 0.07);";
+    	groovyCode = """
+    			bus = aclfnet.getBus('Bus14');
+    			load = bus.getContributeLoad('Bus14-L1');
+    			load.loadCP = new Complex(0.18, 0.07); 
+    		""";
 		result = gvyProcessor.evaluate(groovyCode);
 		System.out.println("Result: " + result);
 		assertTrue("Bus contribute load should be 0.18 + j0.07", 
 				NumericUtil.equals(net.getBus("Bus14").getContributeLoad("Bus14-L1").getLoadCP(), new Complex(0.18, 0.07), 1.0E-4));
+
+		groovyCode = """
+    			branch = aclfnet.getBranch("Bus1", "Bus2", "1"); 
+    			branch.z = new Complex(0.02, 0.06 );
+    		""";
+		result = gvyProcessor.evaluate(groovyCode);
+		System.out.println("Result: " + result);
+		assertTrue("Branch z should be 0.02+j0.06", 
+				NumericUtil.equals(net.getBranch("Bus1", "Bus2", "1").getZ(), new Complex(0.02, 0.06), 1.0E-4));
 	}
 	
 	@Test 
@@ -82,7 +68,7 @@ public class GvyScriptEval_Test extends CorePluginTestSetup {
  		
 	  	AclfNetGvyScriptProcessor gvyProcessor = new AclfNetGvyScriptProcessor(net);
 
-	  	String scriptFile = "script/ieee14_adjust.gvy";
+	  	String scriptFile = "script/ieee14_adjBus14.gvy";
 	  	// Load Groovy script from the file as a string
 	  	String groovyCode = FileUtil.readFileAsString(scriptFile);
 	  	//System.out.println("Groovy Code: " + groovyCode);
@@ -92,5 +78,17 @@ public class GvyScriptEval_Test extends CorePluginTestSetup {
 		System.out.println("Result: " + result);
 		assertTrue("Bus contribute load should be 0.18 + j0.07", 
 				NumericUtil.equals(net.getBus("Bus14").getContributeLoad("Bus14-L1").getLoadCP(), new Complex(0.18, 0.07), 1.0E-4));
+
+		scriptFile = "script/ieee14_adjBranch1_2.gvy";
+	  	// Load Groovy script from the file as a string
+	  	groovyCode = FileUtil.readFileAsString(scriptFile);
+	  	//System.out.println("Groovy Code: " + groovyCode);
+		
+		// Evaluate the Groovy code
+		result = gvyProcessor.evaluate(groovyCode);
+		System.out.println("Result: " + result);
+		assertTrue("Branch z should be 0.02+j0.06", 
+				NumericUtil.equals(net.getBranch("Bus1", "Bus2", "1").getZ(), new Complex(0.02, 0.06), 1.0E-4));
+		assertFalse("Branch should be off", net.getBranch("Bus1", "Bus2", "1").isActive());
 	}
 }
