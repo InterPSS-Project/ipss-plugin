6f38e0a3c1ffb69ca0db9cfe6238093806dec0b3
IpssDev-4 <ipss.cntester@gmail.com>
2026-02-24 16:16:10 +0800

    feat(contingency): add native-format contingency parser and related classes for .con file handling

--- ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/con_format/bean/ConEquipMoveEvent.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/con_format/bean/ConEquipMoveEvent.java
@@ -1,4 +1,4 @@
-package org.interpss.plugin.contingency.parser;
+package org.interpss.plugin.contingency.con_format.bean;
 
 /**
  * A bus-to-bus load (or generation / shunt) transfer event in a PSS/E contingency case.
