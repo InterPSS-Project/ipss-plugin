f4382c91dd329227d5ff11fe605c775f15b5aea9
IpssDev-4 <ipss.cntester@gmail.com>
2026-03-04 16:58:03 +0800

    feat(adapters): add AclfNetDFrameSample2 and corresponding test for AclfResultDFrameAdapter

---
+++ ipss.plugin.core/src/sample/java/org/interpss/result/dframe/AclfNetDFrameSample2.java
@@ -0,0 +1,41 @@
+package org.interpss.result.dframe;
+
+import org.dflib.DataFrame;
+import org.dflib.Printers;
+import org.dflib.csv.Csv;
+import org.dflib.json.Json;
+import org.interpss.CorePluginFactory;
+import org.interpss.fadapter.IpssFileAdapter;
+import org.interpss.plugin.result.dframe.AclfNetDFrameAdapter;
+
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.LoadflowAlgoObjectFactory;
+import com.interpss.core.aclf.AclfBranchCode;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.algo.LoadflowAlgorithm;
+
+public class AclfNetDFrameSample2 {
+    
+    public static void main(String[] args) throws InterpssException {
+		// load the IEEE-14 Bus system
+		AclfNetwork net = CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
+				.load("testData/adpter/ieee_format/Ieee14Bus.ieee")
+				.getAclfNet();	
+		
+	  	LoadflowAlgorithm algo = LoadflowAlgoObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.loadflow();
+	  	
+	  	AclfNetDFrameAdapter dfAdapter = new AclfNetDFrameAdapter();
+	  	dfAdapter.adapt(net);
+	  	
+    	DataFrame dfBus = dfAdapter.getDfBus();    	
+        
+		Json.saver().save(dfBus, "output/Ieee14Bus_bus.json");
+		System.out.println("Save to json file: output/Ieee14Bus_bus.json");
+		
+		// write the dfBus to a csv file
+		Csv.saver().save(dfBus, "output/Ieee14Bus_bus.csv");
+		System.out.println("Save to csv file: output/Ieee14Bus_bus.csv");
+    }
+}
