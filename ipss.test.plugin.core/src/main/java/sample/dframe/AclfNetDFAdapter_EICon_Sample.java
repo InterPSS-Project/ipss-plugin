13a3698552a4450cd1a405c8b706ae97b9244e24
IpssDev-4 <ipss.cntester@gmail.com>
2026-03-05 13:57:50 +0800

    feat(samples): add AclfNetDFAdapter_ACTIVSg25kSample and AclfNetDFAdapter_EICon_Sample for PSSE data processing

--- ipss.test.plugin.core/src/main/java/sample/dframe/AclfNetDFAdapter_EICon_Sample.java
+++ ipss.test.plugin.core/src/main/java/sample/dframe/AclfNetDFAdapter_EICon_Sample.java
@@ -1,4 +1,4 @@
-package sample.exchange;
+package sample.dframe;
 
 import static com.interpss.core.DclfAlgoObjectFactory.createContingencyAnalysisAlgorithm;
 import static org.interpss.plugin.pssl.plugin.IpssAdapter.FileFormat.PSSE;
