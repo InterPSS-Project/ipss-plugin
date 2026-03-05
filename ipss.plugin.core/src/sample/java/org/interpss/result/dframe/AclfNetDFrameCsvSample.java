b48ccb849839940bba1e5497f53f0ecda1f0d2c9
IpssDev-4 <ipss.cntester@gmail.com>
2026-03-05 08:16:21 +0800

    feat(adapters): update AclfNetDFrameCsvSample to use TEST_ROOT for file paths and add AclfNetDFrameAdapterTest

--- ipss.plugin.core/src/sample/java/org/interpss/result/dframe/AclfNetDFrameCsvSample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/result/dframe/AclfNetDFrameCsvSample.java
@@ -15,12 +15,13 @@ import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.algo.LoadflowAlgorithm;
 
 public class AclfNetDFrameCsvSample {
+	private static final String TEST_ROOT = "ipss.plugin.core/";
     
     public static void main(String[] args) throws InterpssException {
 		// load the IEEE-14 Bus system
 		AclfNetwork net = CorePluginFactory
 				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
-				.load("testData/adpter/ieee_format/Ieee14Bus.ieee")
+				.load(TEST_ROOT + "testData/adpter/ieee_format/Ieee14Bus.ieee")
 				.getAclfNet();	
 		
 	  	LoadflowAlgorithm algo = LoadflowAlgoObjectFactory.createLoadflowAlgorithm(net);
@@ -30,9 +31,12 @@ public class AclfNetDFrameCsvSample {
 	  	dfAdapter.adapt(net);
 	  	
     	DataFrame dfBus = dfAdapter.getDfBus();    	
+
+		// print # of rows in dfBus
+		System.out.println("Number of rows in dfBus: " + dfBus.height());
 		
 		// write the dfBus to a csv file
-		Csv.saver().save(dfBus, "output/Ieee14Bus_bus.csv");
+		Csv.saver().save(dfBus, TEST_ROOT + "output/Ieee14Bus_bus.csv");
 		System.out.println("Save to csv file: output/Ieee14Bus_bus.csv");
     }
 }
