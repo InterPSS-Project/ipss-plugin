fb415b92c660a2cdc637361b268492adb5deea15
IpssDev-4 <ipss.cntester@gmail.com>
2026-03-09 16:24:05 +0800

    feat(contingency): refactor contingency handling to use DclfContingencyHelper for improved efficiency

--- ipss.test.plugin.core/src/main/java/sample/contingency/dclf/EICon_CASample.java
+++ ipss.test.plugin.core/src/main/java/sample/contingency/dclf/EICon_CASample.java
@@ -16,6 +16,7 @@ import org.interpss.plugin.contingency.definition.BranchContingencyRecord;
 import org.interpss.plugin.contingency.definition.MonitoredBranchRecord;
 import org.interpss.plugin.contingency.result.DclfContingencyResultRec;
 import org.interpss.plugin.contingency.util.ContingencyFileUtil;
+import org.interpss.plugin.contingency.util.DclfContingencyHelper;
 import org.interpss.plugin.pssl.plugin.IpssAdapter;
 import org.interpss.plugin.pssl.plugin.IpssAdapter.PsseVersion;
 
@@ -45,36 +46,9 @@ public class EICon_CASample {
 		//import contingency definitions from CA file
 		File contFile = new File("testData/psse/v33/OpenEI_filtered_contingencies.json");
 		List<BranchContingencyRecord> contingencies = ContingencyFileUtil.importContingenciesFromJson(contFile);
-		List<DclfBranchOutage> contList = new java.util.ArrayList<>();
-
-		for (BranchContingencyRecord record : contingencies) {
-			try {
-				// Find the branch based on from_bus and to_bus
-				String branchId = record.fromBus + "->" + record.toBus+"("+record.ckt+")";
-				if (net.getBranch(branchId) != null) {
-					DclfBranchOutage cont = createContingency(record.name);
-					
-					// Determine outage type based on action type
-					ContingencyBranchOutageType outageType;
-					switch (record.actionType.toLowerCase()) {
-						case "open":
-							outageType = ContingencyBranchOutageType.OPEN;
-							break;
-						case "close":
-							outageType = ContingencyBranchOutageType.CLOSE;
-							break;
-						default:
-							outageType = ContingencyBranchOutageType.OPEN; // Default to open
-					}
-					
-					DclfOutageBranch outage = createCaOutageBranch(algo.getDclfAlgoBranch(branchId), outageType);
-					cont.setOutageEquip(outage);
-					contList.add(cont);
-				}
-			} catch (Exception ex) {
-				throw new Exception("Warning: Could not create contingency for " + record.name + ": " + ex.getMessage() + "\n");
-			}
-		}
+		
+		List<DclfBranchOutage> dclfContList = new DclfContingencyHelper(algo)
+					.createDclfContList(contingencies);
 
 		//import monitored branches from JSON file
 		File monFile = new File("testData/psse/v33/OpenEI_monitored_branches.json");
@@ -90,7 +64,7 @@ public class EICon_CASample {
 
 		ConcurrentLinkedQueue<DclfContingencyResultRec> results = 
 				ParallelDclfContingencyAnalyzer.executeContingencyAnalysis(
-						net, contList, monitoredBranchIds, config, 16);	
+						net, dclfContList, monitoredBranchIds, config, 16);	
 
 		// print the results
 		int cnt = 0;
