b2907f7ae1ea2838a9ace17ce61442aad61e131d
IpssDev-4 <ipss.cntester@gmail.com>
2026-05-15 14:39:19 +0800

    refactor: update BranchDclfResultRec to simplify its documentation and replace its usage with BranchOptAdjustResultRec in IEEE14 optimization sample

--- ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/algo/result/BranchDclfResultRec.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/algo/result/BranchDclfResultRec.java
@@ -1,26 +1,10 @@
 package org.interpss.plugin.optadj.algo.result;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import org.interpss.datatype.base.BaseJSONBean;
-
-import com.interpss.algo.parallel.BranchCAResultRec;
 import com.interpss.core.algo.dclf.adapter.DclfAlgoBranch;
 
 
 /** 
-* A container class for storing the results of the AclfNetwork SSA algorithm.
-
-* @author  Donghao.F 
-
-* @date 2023 Mar 11 09:50:58 
-
-* 
-
+* Branch DCLF result record
 */
 public class BranchDclfResultRec {
 	public DclfAlgoBranch dclfBranch;
