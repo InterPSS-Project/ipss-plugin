afa32870619fdb528ddf402dd49e50defe6f29c5
IpssDev-4 <ipss.cntester@gmail.com>
2026-05-15 11:10:15 +0800

    refactor: simplify result mapping in AclfNetGenLoadOptimizer and update test assertions for generator naming consistency

--- ipss.test.plugin.core/src/test/java/org/interpss/plugin/optadj/IEEE14_OptAdj_SelOutge_Test.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/plugin/optadj/IEEE14_OptAdj_SelOutge_Test.java
@@ -92,8 +92,8 @@ public class IEEE14_OptAdj_SelOutge_Test extends CorePluginTestSetup {
 		Map<String, Double> resultMap = optimizer.getResultMap();
 		System.out.println(resultMap);
 		
-		assertEquals(resultMap.get("Bus3-G1"), 0.99, 0.0001);
-		assertEquals(resultMap.get("Bus1-G1"), -0.99, 0.0001);
+		assertEquals(resultMap.get("Gen:Bus3-G1"), 0.99, 0.0001);
+		assertEquals(resultMap.get("Gen:Bus1-G1"), -0.99, 0.0001);
 		
 		System.out.println("Optimization gen size." + optimizer.getOptimizer().getGenSize());
 		System.out.println("Optimization gen constrain size." + optimizer.getOptimizer().getGenConstrainDataList().size());
