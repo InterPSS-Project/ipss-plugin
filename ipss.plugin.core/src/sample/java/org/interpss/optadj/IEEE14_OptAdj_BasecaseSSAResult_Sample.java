aa478c2bb2d77246c33b5ff23614c083b68b3d5f
IpssDev-4 <ipss.cntester@gmail.com>
2026-05-17 11:41:23 +0800

    refactor: add optional adjustment result maps to AclfNetSsaResultContainer and update sample implementations

--- ipss.plugin.core/src/sample/java/org/interpss/optadj/IEEE14_OptAdj_BasecaseSSAResult_Sample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/optadj/IEEE14_OptAdj_BasecaseSSAResult_Sample.java
@@ -55,6 +55,8 @@ public class IEEE14_OptAdj_BasecaseSSAResult_Sample {
 		
 		Map<String, Double> resultMap = optimizer.getResultMap();
 		System.out.println(resultMap);
+
+		ssaResults.setOptAdjBaseResultMap(resultMap);
 		
 		System.out.println("Optimization gen size." + optimizer.getOptimizer().getGenSize());
 		System.out.println("Optimization gen constrain size." + optimizer.getOptimizer().getGenConstrainDataList().size());
