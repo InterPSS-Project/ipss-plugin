6c9a57c31c1aa92e19e2026fe0bb3d91772b440a
IpssDev-4 <ipss.cntester@gmail.com>
2026-05-18 14:19:11 +0800

    refactor: remove obsolete sample classes IEEE14_OptAdj_BasecaseSSAResult_Sample and IEEE14_OptAdj_N1ScanSSAResult_Sample to streamline codebase

---
+++ ipss.plugin.core/src/sample/java/org/interpss/optadj/ssaResult/OptAdjGenOnly_Texas2K_SsaResultSample.java
@@ -0,0 +1,96 @@
+package org.interpss.optadj.ssaResult;
+
+import static com.interpss.core.DclfAlgoObjectFactory.createContingencyAnalysisAlgorithm;
+import static org.interpss.plugin.pssl.plugin.IpssAdapter.FileFormat.PSSE;
+
+import java.util.Map;
+import org.interpss.numeric.datatype.AtomicCounter;
+import org.interpss.numeric.datatype.Counter;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.interpss.numeric.util.PerformanceTimer;
+import org.interpss.plugin.optadj.algo.AclfNetGenLoadOptimizer;
+import org.interpss.plugin.pssl.plugin.IpssAdapter;
+
+import com.interpss.core.aclf.AclfBranch;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.algo.dclf.ContingencyAnalysisAlgorithm;
+import com.interpss.core.algo.dclf.DclfMethod;
+import com.interpss.core.algo.dclf.solver.IDclfSolver.CacheType;
+
+public class OptAdjGenOnly_Texas2K_SsaResultSample {
+	static class DblBuffer {
+		double val;
+	}
+    public static void main(String args[]) throws Exception {
+		// load the test data V33
+		AclfNetwork aclfNet = IpssAdapter.importAclfNet("ipss.plugin.core/testData/psse/v36/Texas2k_series24_case1_2016summerPeak_v36.RAW")
+				.setFormat(PSSE)
+				.setPsseVersion(IpssAdapter.PsseVersion.PSSE_36) 
+				.load()
+				.getImportedObj();	
+		
+		// define an caAlgo object and perform DCLF 
+		ContingencyAnalysisAlgorithm dclfAlgo = createContingencyAnalysisAlgorithm(aclfNet, CacheType.SenNotCached, true);
+		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);
+		//System.out.println(DclfOutFunc.dclfResults(dclfAlgo, false));
+		
+		double loadingThreshold = 90.0;
+
+		Counter cnt = new Counter(0);
+		DblBuffer maxLoading = new DblBuffer();
+		dclfAlgo.getDclfAlgoBranchList().forEach(braDclf -> {
+			AclfBranch branch = braDclf.getBranch();
+
+            double powerFlowMW = dclfAlgo.getBranchFlow(branch, UnitType.mW);
+            double ratingMVA = branch.getRatingMvaA();
+            double loadingPercent = ratingMVA > 0 ? (Math.abs(powerFlowMW) / ratingMVA) * 100.0 : 0.0;
+            if ( loadingPercent > loadingThreshold) {
+            	System.out.printf("Overloaded Branch: %s, Flow(MW): %.2f, Rating(MVA): %.2f, Loading(%%): %.2f%n",
+            			branch.getId(), powerFlowMW, ratingMVA, loadingPercent);
+            	cnt.increment();
+            }
+            if (loadingPercent > maxLoading.val) {
+				maxLoading.val = loadingPercent;
+			}
+		});		
+		System.out.println("Number of overloaded branches: " + cnt.getCount()); 
+		System.out.println("Max loading percent: " + maxLoading.val);
+		
+		PerformanceTimer timer = new PerformanceTimer();
+		// perform the Optimization adjustment
+		AclfNetGenLoadOptimizer optimizer = new AclfNetGenLoadOptimizer(dclfAlgo);
+		optimizer.optimize(loadingThreshold, true);
+		
+		timer.log("Opt");
+		
+		Map<String, Double> resultMap = optimizer.getResultGenMap();
+		System.out.println("Optimization gen result: " + resultMap);
+		
+		System.out.println("Optimization gen size: " + optimizer.getOptimizer().getGenSize());
+		System.out.println("Optimization gen constrain size: " + optimizer.getOptimizer().getGenConstrainDataList().size());
+		System.out.println("Optimization sec constrain size: " + optimizer.getOptimizer().getSecConstrainDataList().size());
+	
+		// Dclf after the optimization, Dclf gen has been adjusted
+		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);
+		
+		// check the branch loading after the optimization adjustment
+		double baseMVA = aclfNet.getBaseMva();
+		AtomicCounter cnt1 = new AtomicCounter();
+		maxLoading.val = 0.0;
+		dclfAlgo.getDclfAlgoBranchList().stream()
+			.forEach(dclfBranch -> {
+				double flowMw = dclfBranch.getDclfFlow() * baseMVA;
+				double loading = Math.abs(flowMw / dclfBranch.getBranch().getRatingMvaA())*100;
+				if (loading > loadingThreshold) {
+					cnt1.increment();
+					System.out.printf("Branch: %s  %.2f rating: %.2f loading: %.2f%n",
+							dclfBranch.getId(), flowMw, dclfBranch.getBranch().getRatingMvaA(), loading);
+				}
+	            if (loading > maxLoading.val) {
+					maxLoading.val = loading;
+				}
+			});
+		System.out.println("Total number of branches over limit after OptAdj: " + cnt1.getCount());
+		System.out.println("Max loading percent: " + maxLoading.val);
+    }
+}
