e0fb72ad8f7640fae057f3227fbdd321118248e1
IpssDev-4 <ipss.cntester@gmail.com>
2026-05-22 08:53:24 +0800

    refactor: update AclfNetBusOptimizer to expose control bus roles and enhance overload handling in sample classes by utilizing the new role-based logic for improved clarity and functionality

--- ipss.plugin.core/src/sample/java/org/interpss/optadj/ei/OptAdjGenLoad_EInterconnect_Sample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/optadj/ei/OptAdjGenLoad_EInterconnect_Sample.java
@@ -11,7 +11,7 @@ public class OptAdjGenLoad_EInterconnect_Sample {
 		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);
 
 		System.out.println("=== Base case overloads ===");
-		EInterconnect_Info_Sample.printOverloadSummary(dclfAlgo, 100.0);
+		EInterconnect_Info_Sample.printOverloadSummary(dclfAlgo, EInterconnect_Info_Sample.OPT_THRESHOLD);
 		
 		EInterconnect_Info_Sample.runBusOptimization(dclfAlgo, aclfNet, false, "Gen+Load");
 	}
