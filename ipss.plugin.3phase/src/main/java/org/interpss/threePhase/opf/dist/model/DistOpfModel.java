74802b36bbfee995aa544e921ad558af5d18a99b
IPSSDEV3 <NONE>
2026-05-28 16:54:40 -0600

    feat: add DistOPF API skeleton

---
+++ ipss.plugin.3phase/src/main/java/org/interpss/threePhase/opf/dist/model/DistOpfModel.java
@@ -0,0 +1,60 @@
+package org.interpss.threePhase.opf.dist.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.interpss.plugin.opf.constraint.OpfConstraint;
+
+public class DistOpfModel {
+
+	private final DistOpfVariableIndex variableIndex;
+	private final List<OpfConstraint> constraints = new ArrayList<OpfConstraint>();
+	private double[] linearObjective = new double[0];
+	private double[] lowerBounds = new double[0];
+	private double[] upperBounds = new double[0];
+
+	public DistOpfModel(DistOpfVariableIndex variableIndex) {
+		this.variableIndex = variableIndex;
+	}
+
+	public DistOpfVariableIndex getVariableIndex() {
+		return variableIndex;
+	}
+
+	public int getNumberOfVariables() {
+		return variableIndex.size();
+	}
+
+	public List<OpfConstraint> getConstraints() {
+		return Collections.unmodifiableList(constraints);
+	}
+
+	public void addConstraint(OpfConstraint constraint) {
+		this.constraints.add(constraint);
+	}
+
+	public double[] getLinearObjective() {
+		return linearObjective;
+	}
+
+	public void setLinearObjective(double[] linearObjective) {
+		this.linearObjective = linearObjective;
+	}
+
+	public double[] getLowerBounds() {
+		return lowerBounds;
+	}
+
+	public void setLowerBounds(double[] lowerBounds) {
+		this.lowerBounds = lowerBounds;
+	}
+
+	public double[] getUpperBounds() {
+		return upperBounds;
+	}
+
+	public void setUpperBounds(double[] upperBounds) {
+		this.upperBounds = upperBounds;
+	}
+}
