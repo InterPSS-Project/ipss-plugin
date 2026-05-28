52ab1589290cadebd4e251132e8fbc5dd1495860
IPSSDEV3 <NONE>
2026-05-28 17:16:37 -0600

    feat: add DistOPF objective collectors

---
+++ ipss.plugin.3phase/src/main/java/org/interpss/threePhase/opf/dist/objective/GenMaxObjectiveCollector.java
@@ -0,0 +1,24 @@
+package org.interpss.threePhase.opf.dist.objective;
+
+import org.interpss.threePhase.opf.dist.model.DistOpfDerData;
+import org.interpss.threePhase.opf.dist.model.DistOpfModelData;
+import org.interpss.threePhase.opf.dist.model.DistOpfVariableIndex;
+
+import com.interpss.core.acsc.PhaseCode;
+
+public class GenMaxObjectiveCollector extends BaseDistOpfObjectiveCollector {
+
+	public GenMaxObjectiveCollector(DistOpfModelData modelData,
+			DistOpfVariableIndex variableIndex, double[] objective) {
+		super(modelData, variableIndex, objective);
+	}
+
+	@Override
+	public void collectObjective() {
+		for (DistOpfDerData der : modelData.getDers()) {
+			for (PhaseCode phase : der.getPhases()) {
+				objective[variableIndex.derP(der.getId(), phase)] = -1.0;
+			}
+		}
+	}
+}
