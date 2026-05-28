566926d91c60074839297aeb201c19abfa7a5a79
IPSSDEV3 <NONE>
2026-05-28 17:07:19 -0600

    test: validate DistOPF OpenDSS extraction

---
+++ ipss.plugin.3phase/src/test/java/org/interpss/threePhase/opf/dist/DistOpfOpenDssImportTest.java
@@ -0,0 +1,29 @@
+package org.interpss.threePhase.opf.dist;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.interpss.threePhase.dataParser.opendss.OpenDSSDataParser;
+import org.interpss.threePhase.dynamic.DStabNetwork3Phase;
+import org.interpss.threePhase.opf.dist.model.DistOpfModelData;
+import org.interpss.threePhase.opf.dist.model.DistOpfModelDataExtractor;
+import org.junit.jupiter.api.Test;
+
+public class DistOpfOpenDssImportTest {
+
+	@Test
+	public void extractsExistingOpenDssIeee123Feeder() {
+		OpenDSSDataParser parser = new OpenDSSDataParser();
+		parser.parseFeederData("testData/feeder/IEEE123", "IEEE123Master_Modified_v2.dss");
+		parser.calcVoltageBases();
+		parser.convertActualValuesToPU(1.0);
+
+		DStabNetwork3Phase distNet = parser.getDistNetwork();
+		DistOpfModelData data = new DistOpfModelDataExtractor().extract(distNet);
+
+		assertEquals("150", data.getSwingBusId());
+		assertTrue(data.getBuses().size() > 100);
+		assertTrue(data.getBranches().size() > 100);
+		assertEquals(data.getBuses().size() - 1, data.getBranches().size());
+	}
+}
