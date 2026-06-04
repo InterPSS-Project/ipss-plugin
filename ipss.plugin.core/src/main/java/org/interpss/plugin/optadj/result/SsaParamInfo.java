6aa4772c941f36496b6718f6336120ee4bd1f7ac
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-04 11:54:13 +0800

    refactor: update import for SsaResultOneStateInfo in AclfNetLoadFlowOptimizer
    - Changed the import path for SsaResultOneStateInfo to reflect its new location in the result package, improving code organization.

---
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/result/SsaParamInfo.java
@@ -0,0 +1,38 @@
+package org.interpss.plugin.optadj.result;
+
+/** 
+* SSA input parameter info
+*/
+public class SsaParamInfo {
+	// SSA analysis type [ BaseNetwork("��������"), NetOutage("���翪��") ]
+	private MStateSsaType ssaType;
+	
+	// LODF threshold for N-1 scan calculation
+	private double lodfThreshold = 0.2; 
+	
+	// outage branch name, for the ssaType = NetOutage scenario
+	private String outBranchName;
+	
+	public SsaParamInfo(double lodfThreshold) {
+		this.ssaType = MStateSsaType.BaseNetwork;
+		this.lodfThreshold = lodfThreshold;
+	}
+	
+	public SsaParamInfo(MStateSsaType ssaType, String outBranchName, double lodfThreshold) {
+		this(lodfThreshold);
+		this.ssaType = ssaType;
+		this.outBranchName = outBranchName;
+	}
+
+	public MStateSsaType getSsaType() {
+		return ssaType;
+	}
+	
+	public double getLodfThreshold() {
+		return lodfThreshold;
+	}
+	
+	public String getOutBranchName() {
+		return outBranchName;
+	}
+}
