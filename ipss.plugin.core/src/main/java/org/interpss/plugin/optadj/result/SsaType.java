17e35fd49a77a14585bb79f8ba36d6745260001f
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-04 15:38:41 +0800

    refactor: replace SsaResultOneStateInfo with SsaResultContainer and remove obsolete classes
    - Updated AclfNetLoadFlowOptimizer to use SsaResultContainer instead of SsaResultOneStateInfo, enhancing code clarity and consistency.
    - Deleted SsaResultOneStateInfo and MStateSsaType classes as they were no longer needed, streamlining the codebase.
    - Adjusted related methods to ensure compatibility with the new result container structure.

--- ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/result/SsaType.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/result/SsaType.java
@@ -1,13 +1,13 @@
 package org.interpss.plugin.optadj.result;
 
-public enum MStateSsaType {
+public enum SsaType {
 	BaseNetwork("Base Network"),    		// Run SSA on natural power flow over the base network without static adjustment
 	NetOutage("Network Outage"),      		// Run SSA on natural power flow with an additional branch outage over the base network
-	Net3WXfrOutage("Three-Winding Transformer Outage");  	// Run SSA on natural power flow with a three-winding transformer outage over the base network
+	Net3WXfrOutage("3WXfr Outage");  	// Run SSA on natural power flow with a three-winding transformer outage over the base network
 	
 	private String name;
 	
-	private MStateSsaType(String name) {	
+	private SsaType(String name) {	
 		this.name = name;
 	}
 	
