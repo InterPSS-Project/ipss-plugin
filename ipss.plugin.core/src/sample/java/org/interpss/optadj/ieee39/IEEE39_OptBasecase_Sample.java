8295a3bf60d846bd874f8bbfda568c672c4037f1
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-04 15:20:27 +0800

    refactor: enhance AclfNetLoadFlowOptimizer with new sensitivity matrix handling
    - Updated the AclfNetLoadFlowOptimizer to utilize the newly added sparse sensitivity matrix dependency, improving performance and efficiency.
    - Adjusted related methods to accommodate the new matrix handling, ensuring compatibility and enhancing overall functionality.

---
+++ ipss.plugin.core/src/sample/java/org/interpss/optadj/ieee39/IEEE39_OptBasecase_Sample.java
@@ -0,0 +1,59 @@
+package org.interpss.optadj.ieee39;
+
+import org.interpss.IpssCorePlugin;
+
+import com.interpss.core.DclfAlgoObjectFactory;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.algo.dclf.ContingencyAnalysisAlgorithm;
+import com.interpss.core.algo.dclf.DclfMethod;
+import com.interpss.core.algo.dclf.solver.IDclfSolver.CacheType;
+
+import org.interpss.plugin.optadj.algo.lf.AclfNetLoadFlowOptimizer;
+
+public class IEEE39_OptBasecase_Sample {
+
+	public static void main(String args[]) throws Exception {
+	    // Load network
+	    AclfNetwork net = IEEE39_Sample_Data.createTestCaseNetwork();
+
+		// define an caAlgo object and perform DCLF 
+		ContingencyAnalysisAlgorithm dclfAlgo = DclfAlgoObjectFactory.createContingencyAnalysisAlgorithm(net,
+				CacheType.SenNotCached, true);
+		dclfAlgo.calculateDclf();
+				
+		// check the branch loading
+		double baseMVA = net.getBaseMva();
+		dclfAlgo.getDclfAlgoBranchList().stream() 
+			.forEach(dclfBranch -> {
+				double flowMw = dclfBranch.getDclfFlow() * baseMVA;
+				double loading = Math.abs(flowMw / dclfBranch.getBranch().getRatingMva1())*100;
+				if (loading > 100) {
+					System.out.printf("Over Limit Branch: %s  %.2f rating: %.2f loading: %.2f%n",
+							dclfBranch.getId(),
+							flowMw,
+							dclfBranch.getBranch().getRatingMva1(),
+							loading);
+					}
+			});
+
+		// perform basecase loaing limit optimization	
+		new AclfNetLoadFlowOptimizer().optimize(dclfAlgo, null, 100.0);
+		
+		// perform DCLF recalculation after optimization
+		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);	
+
+		// check the branch loading after optimization
+		dclfAlgo.getDclfAlgoBranchList().stream() 
+			.forEach(dclfBranch -> {
+				double flowMw = dclfBranch.getDclfFlow() * baseMVA;
+				double loading = Math.abs(flowMw / dclfBranch.getBranch().getRatingMva1())*100;
+				if (loading > 100) {
+					System.out.printf("Over Limit Branch: %s  %.2f rating: %.2f loading: %.2f%n",
+							dclfBranch.getId(),
+							flowMw,
+							dclfBranch.getBranch().getRatingMva1(),
+							loading);
+					}
+			});
+	}
+}
