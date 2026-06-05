49c5d95fdf036a05b609a2fe7eab957d081d5546
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-05 09:39:11 +0800

    refactor: enhance AclfNetLoadFlowOptimizer and SsaBranchOverLimitInfo for improved optimization results
    - Updated AclfNetLoadFlowOptimizer to return a map of generator adjustment results, enhancing the optimization process and providing clearer output.
    - Introduced a new record, GenAdjustResult, to encapsulate generator adjustment details.
    - Refactored SsaBranchOverLimitInfo to streamline its constructor and remove unused fields, improving code clarity and maintainability.
    - Adjusted SsaResultContainer to remove obsolete fields and methods, ensuring a cleaner and more efficient result handling structure.

--- ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/result/SsaResultContainer.java
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/optadj/result/SsaResultContainer.java
@@ -4,36 +4,36 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.interpss.datatype.base.BaseJSONBean;
-
-
 /** 
 
-* @author  Donghao.F 
-
-* @date 2023 Mar 11 09:50:58 
-
-* 
-
 */
-public class SsaResultContainer extends BaseJSONBean{
-	double lodfThreshold = 0.2;
+public class SsaResultContainer extends BaseJSONBean{	
+	private double baseLoadingThreshold;
+	private List<SsaBranchOverLimitInfo> baseOverLimitInfo;
 	
-	List<SsaBranchOverLimitInfo> baseOverLimitInfo;
+	private double caLoadingThreshold;
+	private List<SsaBranchOverLimitInfo> caOverLimitInfo;
 	
-	List<SsaBranchOverLimitInfo> caOverLimitInfo;
-
-	List<SsaBranchOverLimitInfo> largeLODFInfo;
-	
-	public SsaResultContainer(double lodfThreshold) {
+	public SsaResultContainer() {
 		super();
-		this.lodfThreshold = lodfThreshold;
 		baseOverLimitInfo = new CopyOnWriteArrayList<SsaBranchOverLimitInfo>();
 		caOverLimitInfo = new CopyOnWriteArrayList<SsaBranchOverLimitInfo>();
-		largeLODFInfo = new CopyOnWriteArrayList<SsaBranchOverLimitInfo>();
 	}
-	
-	public double getLodfThreshold() {
-		return lodfThreshold;
+
+	public double getBaseLoadingThreshold() {
+		return baseLoadingThreshold;
+	}
+
+	public void setBaseLoadingThreshold(double baseLoadingThreshold) {
+		this.baseLoadingThreshold = baseLoadingThreshold;
+	}
+
+	public double getCaLoadingThreshold() {
+		return caLoadingThreshold;
+	}
+
+	public void setCaLoadingThreshold(double caLoadingThreshold) {
+		this.caLoadingThreshold = caLoadingThreshold;
 	}
 
 	public List<SsaBranchOverLimitInfo> getBaseOverLimitInfo() {
@@ -50,13 +50,5 @@ public class SsaResultContainer extends BaseJSONBean{
 
 	public void setCaOverLimitInfo(List<SsaBranchOverLimitInfo> caOverLimitInfo) {
 		this.caOverLimitInfo = caOverLimitInfo;
-	}
-
-	public List<SsaBranchOverLimitInfo> getLargeLODFInfo() {
-		return largeLODFInfo;
-	}
-
-	public void setLargeLODFInfo(List<SsaBranchOverLimitInfo> largeLODFInfo) {
-		this.largeLODFInfo = largeLODFInfo;
 	}
 }
