df59f04b7d3d62f58b7565644e774a5cf6de81e4
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-07 18:07:13 +0800

    refactor: remove obsolete optimization test classes for IEEE39 and Texas2K
    - Deleted outdated test classes including IEEE39_OptBasecase_SsaResult_Test, IEEE39_OptBasecase_Test, IEEE39_OptN1Scan_SsaResult_Test, and others to streamline the test suite.
    - This cleanup aims to enhance maintainability and focus on relevant test cases, ensuring the test suite remains efficient and effective.

--- ipss.test.plugin.core/src/test/java/org/interpss/plugin/optadj/texas2K/Texas2K_OptN1Scan_SsaResult_Test.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/plugin/optadj/texas2K/Texas2K_OptN1Scan_SsaResult_Test.java
@@ -1,4 +1,4 @@
-package org.interpss.plugin.optadj;
+package org.interpss.plugin.optadj.texas2K;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
