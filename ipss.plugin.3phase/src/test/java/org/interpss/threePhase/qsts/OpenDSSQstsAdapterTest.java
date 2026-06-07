1e7780c60e0308856055e7db70ccd8d3ace8120d
IPSSDEV3 <NONE>
2026-06-06 20:02:06 -0600

    fix: make OpenDSS QSTS factory static-first

--- ipss.plugin.3phase/src/test/java/org/interpss/threePhase/qsts/OpenDSSQstsAdapterTest.java
+++ ipss.plugin.3phase/src/test/java/org/interpss/threePhase/qsts/OpenDSSQstsAdapterTest.java
@@ -1,6 +1,7 @@
 package org.interpss.threePhase.qsts;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.interpss.numeric.datatype.Complex3x1;
 import org.interpss.numeric.datatype.Unit.UnitType;
@@ -9,11 +10,13 @@ import org.interpss.threePhase.qsts.opendss.OpenDSSQstsStudyFactory;
 import org.junit.jupiter.api.Test;
 
 import com.interpss.core.threephase.IPhaseGen;
+import com.interpss.core.threephase.Static3PBus;
 
 public class OpenDSSQstsAdapterTest {
 	@Test
-	void factoryKeepsOpenDssMetadataAtAdapterBoundary() {
-		OpenDSSDataParser parser = new OpenDSSDataParser();
+	void factoryUsesStaticNetworkAndKeepsOpenDssMetadataAtAdapterBoundary() {
+		OpenDSSDataParser parser = OpenDSSDataParser.forStaticNetwork();
+		parser.getStaticNetwork().setBaseKva(100000.0);
 		parser.getLoadShapeParser().parseLoadShape("New LoadShape.pvday npts=2 mult=(0.25 0.75)",
 				".", "Master.dss", 1);
 		parser.getPVSystemParser().parsePVSystemData(
@@ -29,12 +32,14 @@ public class OpenDSSQstsAdapterTest {
 				1.0, 1.0, QstsControlMode.OFF);
 		study.getStateApplier().apply(context);
 
-		IPhaseGen generator = parser.getDistNetwork().getBus("bus1").getContributeGenList().get(0);
+		assertFalse(parser.hasDistNetwork());
+		Static3PBus bus = parser.getStaticNetwork().getBus("bus1");
+		IPhaseGen generator = bus.getPhaseGenList().get(0);
 		Complex3x1 power = generator.getPower3Phase(UnitType.PU);
-		assertEquals(0.75 * 60.0 / parser.getDistNetwork().getBaseKva() / 3.0,
+		assertEquals(0.75 * 60.0 / parser.getStaticNetwork().getBaseKva() / 3.0,
 				power.a_0.getReal(), 1.0e-12);
 		assertEquals(0.0, power.a_0.getImaginary(), 1.0e-12);
-		assertEquals(0.75 * 60.0 / parser.getDistNetwork().getBaseKva(),
+		assertEquals(0.75 * 60.0 / parser.getStaticNetwork().getBaseKva(),
 				power.a_0.add(power.b_1).add(power.c_2).getReal(), 1.0e-12);
 	}
 }
