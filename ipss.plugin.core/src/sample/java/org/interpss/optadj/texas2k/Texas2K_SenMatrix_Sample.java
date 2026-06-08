cb9605d2cb80afd6aa4f5a0f12359fbecf9f155a
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-09 07:35:29 +0800

    feat: enhance sensitivity analysis in EInterCon_Sample_Info and Texas2K_Sample_Info
    - Updated EInterCon_Sample_Info to include active generator and branch bus counting, improving the sensitivity analysis process.
    - Modified Texas2K_Sample_Info to utilize both dense and sparse matrix calculations for sensitivity analysis, providing more comprehensive insights into non-zero elements.
    - These enhancements aim to improve the usability and effectiveness of the sample information classes for testing and analysis.

--- ipss.plugin.core/src/sample/java/org/interpss/optadj/texas2k/Texas2K_SenMatrix_Sample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/optadj/texas2k/Texas2K_SenMatrix_Sample.java
@@ -1,42 +1,39 @@
 package org.interpss.optadj.texas2k;
 
-import static org.interpss.plugin.pssl.plugin.IpssAdapter.FileFormat.PSSE;
-
 import org.ejml.data.DMatrixSparseCSC;
 import org.interpss.plugin.optadj.algo.util.AclfNetSensHelper;
 import org.interpss.plugin.optadj.algo.util.AclfNetSensSparseHelper;
-import org.interpss.plugin.pssl.plugin.IpssAdapter;
-
 import com.interpss.core.aclf.AclfNetwork;
 
-public class Texas2K_Sample_Info {
-    public static AclfNetwork loadNetwork() throws Exception {
-		// load the test data V33
-		AclfNetwork aclfNet = IpssAdapter.importAclfNet("ipss.plugin.core/testData/psse/v36/Texas2k_series24_case1_2016summerPeak_v36.RAW")
-				.setFormat(PSSE)
-				.setPsseVersion(IpssAdapter.PsseVersion.PSSE_36) 
-				.load()
-				.getImportedObj();	
-
-		aclfNet.getBranchList().stream().forEach(branch -> {
-				branch.setName(branch.getId());
-			});
-
-		return aclfNet;
-	}
-
+public class Texas2K_SenMatrix_Sample {
 	public static void main(String args[]) throws Exception {
 		// load the test data V33
-		AclfNetwork aclfNet = loadNetwork();
+		AclfNetwork aclfNet = Texas2K_Sample_Info.loadNetwork();
 
 		AclfNetSensHelper helper = new AclfNetSensHelper(aclfNet);
 
 		float[][] sen = helper.calSen();
 		System.out.println("Number of non-zero elements in the dense matrix: " + sen.length * sen[0].length);
+		System.out.println(sen[0][0]);
 
 		AclfNetSensSparseHelper helperSparse = new AclfNetSensSparseHelper(aclfNet);
 
 		DMatrixSparseCSC senSparse = helperSparse.calSen();
 		System.out.println("Number of non-zero elements in the sparse matrix: " + senSparse.nz_length);
+		System.out.println(senSparse.get(0, 0, 0.0));
+
+		int count = 0;
+		for (int i = 0; i < sen.length; i++) {
+			for (int j = 0; j < sen[i].length; j++) {
+				double value = senSparse.get(i, j, 0.0);
+				if (value != 0.0 && Math.abs(sen[i][j] - value) > 1e-6) {
+					System.out.println("Error: " + sen[i][j] + " != " + value);
+				}
+				else
+					if (value != 0.0) 
+						count++;
+			}
+		}
+		System.out.println("Number of non-zero elements in the sparse matrix: " + count);
 	}
 }
