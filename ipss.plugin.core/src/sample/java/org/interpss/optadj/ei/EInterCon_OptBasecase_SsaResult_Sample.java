8de353e006461a00a49346969a1913b3d0248c04
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-08 09:34:36 +0800

    refactor: enhance sensitivity analysis handling in Texas2K_Sample_Info
    - Improved the main method in Texas2K_Sample_Info to better facilitate the loading of test data for sensitivity analysis.
    - This update aims to streamline the process and enhance the usability of the sample information class for testing purposes.

---
+++ ipss.plugin.core/src/sample/java/org/interpss/optadj/ei/EInterCon_OptBasecase_SsaResult_Sample.java
@@ -0,0 +1,52 @@
+package org.interpss.optadj.ei;
+
+import static com.interpss.core.DclfAlgoObjectFactory.createContingencyAnalysisAlgorithm;
+import org.interpss.plugin.optadj.algo.lf.AclfNetLoadFlowOptimizer;
+import org.interpss.plugin.optadj.algo.util.AclfNetSsaHelper;
+import org.interpss.plugin.optadj.result.OptAdjResultContainer;
+import org.interpss.plugin.optadj.result.SsaResultContainer;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.algo.dclf.ContingencyAnalysisAlgorithm;
+import com.interpss.core.algo.dclf.DclfMethod;
+import com.interpss.core.algo.dclf.solver.IDclfSolver.CacheType;
+
+public class EInterCon_OptBasecase_SsaResult_Sample {
+    public static void main(String args[]) throws Exception {
+		// load the test data V33
+		AclfNetwork aclfNet = EInterCon_Sample_Info.loadNetwork();
+		
+		// define an caAlgo object and perform DCLF 
+		ContingencyAnalysisAlgorithm dclfAlgo = createContingencyAnalysisAlgorithm(aclfNet, CacheType.SenNotCached, true);
+		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);
+		//System.out.println(DclfOutFunc.dclfResults(dclfAlgo, false));
+		
+		double loadingThreshold = 100.0;
+		SsaResultContainer ssaResult = new AclfNetSsaHelper(dclfAlgo).baseCaseScan(loadingThreshold);   
+		ssaResult.printBaseOverLimitInfo();
+		
+		// perform basecase loaing limit optimization	
+		OptAdjResultContainer optAdjResult = new OptAdjResultContainer(ssaResult);
+		new AclfNetLoadFlowOptimizer().optimize(dclfAlgo, optAdjResult, loadingThreshold);
+		optAdjResult.getOptAdjResults().forEach((genName, result) -> {
+			System.out.println("GenAdjustResult: " + genName + ", " + result.toString());
+		});
+				
+		// perform DCLF recalculation after optimization
+		dclfAlgo.calculateDclf(DclfMethod.INC_LOSS);	
+
+		// check the branch loading after optimization
+		double baseMVA = dclfAlgo.getNetwork().getBaseMva();
+		dclfAlgo.getDclfAlgoBranchList().stream() 
+			.forEach(dclfBranch -> {
+				double flowMw = dclfBranch.getDclfFlow() * baseMVA;
+				double loading = Math.abs(flowMw / dclfBranch.getBranch().getRatingMvaA())*100;
+				if (loading >= optAdjResult.getOptAdjThreshold()) {
+					System.out.printf("Over Limit Branch: %s  %.2f rating: %.2f loading: %.2f%n",
+							dclfBranch.getId(),
+							flowMw,
+							dclfBranch.getBranch().getRatingMvaA(),
+							loading);
+				}
+			});
+	}
+}
