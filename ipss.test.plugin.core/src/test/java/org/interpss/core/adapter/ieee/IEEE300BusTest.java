da224676bd92dfe554e43d11eaee8e19bdc0ab84
IPSSDEV3 <NONE>
2026-06-11 09:24:55 -0600

    test: isolate IEEE300 power adjustment control

--- ipss.test.plugin.core/src/test/java/org/interpss/core/adapter/ieee/IEEE300BusTest.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/core/adapter/ieee/IEEE300BusTest.java
@@ -24,7 +24,10 @@
 
 package org.interpss.core.adapter.ieee;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.function.Consumer;
 
 import org.interpss.CorePluginFactory;
 import org.interpss.CorePluginTestSetup;
@@ -41,29 +44,58 @@ import com.interpss.core.algo.LoadflowAlgorithm;
 public class IEEE300BusTest extends CorePluginTestSetup {
 	@Test
 	public void xtestCase5() throws Exception{
-		AclfNetwork net = CorePluginFactory
-				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
-				.load("testData/ieee_format/ieee300.ieee")
-				.getAclfNet();	
-		
-//		IpssFileAdapter adapter = PluginSpringCtx.getCustomFileAdapter("ieee");
-//		SimuContext simuCtx = adapter.load("testData/ieee_format/ieee300.ieee");
-//
-//		AclfNetwork net = simuCtx.getAclfNet();
+		AclfNetwork net = loadIeee300();
 		
   		assertTrue((net.getBusList().size() == 300 && net.getBranchList().size() == 411));
 
 	  	LoadflowAlgorithm algo = LoadflowAlgoObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.getLfAdjAlgo().setApplyAdjustAlgo(false);
 	  	algo.loadflow();
   		//System.out.println(net.net2String());
 	  	
-  		assertTrue(net.isLfConverged());		
- 		AclfBus swingBus = (AclfBus)net.getBus("Bus69");
+	  	assertTrue(net.isLfConverged());		
+ 		AclfBus swingBus = (AclfBus)net.getBus("Bus7049");
  		AclfSwingBusAdapter swing = swingBus.toSwingBus();
 		//System.out.println(swing.getGenResults(UnitType.PU, net.getBaseKva()).re);
 		//System.out.println(swing.getGenResults(UnitType.PU, net.getBaseKva()).im);
-  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-5.1348)<0.0001);
-  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.8239)<0.0001);
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-4.57025)<0.0001);
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()-0.39049)<0.0001);
+	}
+
+	@Test
+	public void testIEEE300ControlIsolation() throws Exception {
+		assertFalse(runIeee300Loadflow(algo -> {
+		}).isLfConverged());
+
+		assertFalse(runIeee300Loadflow(algo -> {
+			algo.getLfAdjAlgo().getVoltAdjConfig().setAdjust(false);
+		}).isLfConverged());
+
+		assertFalse(runIeee300Loadflow(algo -> {
+			algo.getLfAdjAlgo().getLimitCtrlConfig().setAdjust(false);
+		}).isLfConverged());
+
+		assertTrue(runIeee300Loadflow(algo -> {
+			algo.getLfAdjAlgo().getPowerAdjConfig().setAdjust(false);
+		}).isLfConverged());
+
+		assertTrue(runIeee300Loadflow(algo -> {
+			algo.getLfAdjAlgo().getPowerAdjConfig().setPsXfrPControl(false);
+		}).isLfConverged());
+	}
+
+	private AclfNetwork runIeee300Loadflow(Consumer<LoadflowAlgorithm> configureAlgo) throws Exception {
+		AclfNetwork net = loadIeee300();
+		LoadflowAlgorithm algo = LoadflowAlgoObjectFactory.createLoadflowAlgorithm(net);
+		configureAlgo.accept(algo);
+		algo.loadflow();
+		return net;
+	}
+
+	private AclfNetwork loadIeee300() throws Exception {
+		return CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
+				.load("testData/adpter/ieee_format/ieee300.ieee")
+				.getAclfNet();
 	}
 }
-
