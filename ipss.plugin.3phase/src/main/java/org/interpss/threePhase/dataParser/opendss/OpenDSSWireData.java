c89f975ab4d9a92be9f50dd8c2514e651acb4557
IPSSDEV3 <NONE>
2026-06-11 13:02:11 -0600

    fix: add OpenDSS geometry line charging

--- ipss.plugin.3phase/src/main/java/org/interpss/threePhase/dataParser/opendss/OpenDSSWireData.java
+++ ipss.plugin.3phase/src/main/java/org/interpss/threePhase/dataParser/opendss/OpenDSSWireData.java
@@ -7,14 +7,19 @@ final class OpenDSSWireData {
 	private final String rUnits;
 	private final double gmrAc;
 	private final String gmrUnits;
+	private final double radius;
+	private final String radiusUnits;
 	private final double normAmps;
 
-	OpenDSSWireData(String id, double rac, String rUnits, double gmrAc, String gmrUnits, double normAmps) {
+	OpenDSSWireData(String id, double rac, String rUnits, double gmrAc, String gmrUnits,
+			double radius, String radiusUnits, double normAmps) {
 		this.id = id;
 		this.rac = rac;
 		this.rUnits = rUnits;
 		this.gmrAc = gmrAc;
 		this.gmrUnits = gmrUnits;
+		this.radius = radius;
+		this.radiusUnits = radiusUnits;
 		this.normAmps = normAmps;
 	}
 
@@ -28,6 +33,14 @@ final class OpenDSSWireData {
 
 	double getGmrFeet() {
 		return gmrAc * OpenDSSUnitConverter.lengthFactor(gmrUnits, "ft");
+	}
+
+	double getRadiusFeet() {
+		return radius * OpenDSSUnitConverter.lengthFactor(radiusUnits, "ft");
+	}
+
+	boolean hasRadius() {
+		return radius > 0.0;
 	}
 
 	double getNormAmps() {
