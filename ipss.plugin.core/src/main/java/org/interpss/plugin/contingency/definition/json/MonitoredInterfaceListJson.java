c67bf75b0b99b6a4d424abe02bdf4c80f64d4b09
IPSSDEV3 <NONE>
2026-06-19 15:29:09 -0600

    feat: add DCLF monitoring JSON config

---
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/definition/json/MonitoredInterfaceListJson.java
@@ -0,0 +1,8 @@
+package org.interpss.plugin.contingency.definition.json;
+
+import java.util.List;
+
+public class MonitoredInterfaceListJson {
+    public List<MonitoredInterfaceJson> monitored_interfaces;
+    public MetadataJson metadata;
+}
