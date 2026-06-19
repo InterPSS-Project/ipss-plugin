a108d044f7c34c8ae36f26cc0262c18bf6371a6b
IPSSDEV3 <NONE>
2026-06-19 15:29:09 -0600

    feat: add DCLF monitoring JSON config

---
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/definition/json/FlowgateLimitSetJson.java
@@ -0,0 +1,8 @@
+package org.interpss.plugin.contingency.definition.json;
+
+public class FlowgateLimitSetJson {
+    public Double source_limit_mw;
+    public Double realtime_effective_limit_mw;
+    public Double initial_effective_limit_mw;
+    public String selection_policy;
+}
