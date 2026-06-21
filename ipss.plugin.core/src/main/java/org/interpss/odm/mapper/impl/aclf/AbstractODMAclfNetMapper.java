ec4dd4eb974fc4e56e6e893b17bbd7d58ef4554d
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-21 11:39:12 +0800

    refactor: add logging for bus and branch count during ODM to AclfNetwork mapping in AbstractODMAclfNetMapper

--- ipss.plugin.core/src/main/java/org/interpss/odm/mapper/impl/aclf/AbstractODMAclfNetMapper.java
+++ ipss.plugin.core/src/main/java/org/interpss/odm/mapper/impl/aclf/AbstractODMAclfNetMapper.java
@@ -154,6 +154,7 @@ public abstract class AbstractODMAclfNetMapper<Tfrom> extends AbstractODMSimuCtx
 			int busCnt = xmlNet.getBusList().getBus().size();
 			int branchCnt = xmlNet.getBranchList().getBranch().size();
 			aclfNet.ensureLookupCapacity(busCnt, branchCnt);
+			log.info("ODM to AclfNetwork: busCnt: " + busCnt + ", branchCnt: " + branchCnt);
 
 			//XformerZTableXmlType xfrZTable = xmlNet.getXfrZTable();
 			AclfBusDataHelper busHelper = new AclfBusDataHelper(aclfNet);
