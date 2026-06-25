cd95ba2040aed4cf975b28ea84f68e12b9a8910f
IpssDev-4 <ipss.cntester@gmail.com>
2026-06-25 14:15:17 +0800

    refactor: remove obsolete classes related to load flow optimization and sensitivity analysis to streamline the codebase

--- ipss.test.plugin.core/src/test/java/org/interpss/plugin/optadj/texas2K/sparse/Texas2K_OptN1Scan_SsaResult_Sparse_Test1.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/plugin/optadj/texas2K/sparse/Texas2K_OptN1Scan_SsaResult_Sparse_Test1.java
@@ -8,10 +8,6 @@ import java.util.Map;
 
 import org.interpss.CorePluginTestSetup;
 import org.interpss.numeric.datatype.AtomicCounter;
-import org.interpss.plugin.optadj.algo.lf.AclfNetContigencyOptimizer;
-import org.interpss.plugin.optadj.algo.util.AclfNetSsaHelper;
-import org.interpss.plugin.optadj.result.OptAdjResultContainer;
-import org.interpss.plugin.optadj.result.SsaResultContainer;
 import org.interpss.plugin.optadj.texas2K.Texas2K_TestCaseInfo;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +18,10 @@ import com.interpss.core.algo.dclf.ContingencyAnalysisAlgorithm;
 import com.interpss.core.algo.dclf.DclfMethod;
 import com.interpss.core.algo.dclf.solver.IDclfSolver.CacheType;
 import com.interpss.core.contingency.dclf.DclfBranchOutage;
+import com.interpss.optadj.algo.lf.AclfNetContigencyOptimizer;
+import com.interpss.optadj.algo.util.AclfNetSsaHelper;
+import com.interpss.optadj.result.OptAdjResultContainer;
+import com.interpss.optadj.result.SsaResultContainer;
 
 /**
  * Regression test for {@code Texas2K_OptN1Scan_SsaResult_Sparse_Sample1}: full N-1 branch-outage
