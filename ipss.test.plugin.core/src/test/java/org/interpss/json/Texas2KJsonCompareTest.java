8ea795b2f5dbe76c0c9b70165e876edd4372d0c5
power4all <thuang@users.noreply.github.com>
2026-07-01 11:03:34 -0600

    update version to 1.2.6 and add json round-trip test

--- ipss.test.plugin.core/src/test/java/org/interpss/json/Texas2KJsonCompareTest.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/json/Texas2KJsonCompareTest.java
@@ -3,14 +3,10 @@ package org.interpss.json;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.nio.file.Path;
-
 import org.interpss.CorePluginTestSetup;
 import org.interpss.plugin.optadj.texas2K.Texas2K_TestCaseInfo;
 import org.interpss.util.AclfNetJsonComparator;
-import org.interpss.util.FileUtil;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.state.StateObjectFactory;
@@ -23,13 +19,12 @@ import com.interpss.state.aclf.AclfNetworkState;
 public class Texas2KJsonCompareTest extends CorePluginTestSetup {
 
 	@Test
-	void jsonRoundTripMatchesOriginal(@TempDir Path tempDir) throws Exception {
+	void jsonRoundTripMatchesOriginal() throws Exception {
 		AclfNetwork aclfNet = Texas2K_TestCaseInfo.createTestCaseNetwork();
 
-		Path jsonFile = tempDir.resolve("texas2k.json");
-		FileUtil.writeText2File(jsonFile.toString(), new AclfNetworkState(aclfNet).toString());
-
-		AclfNetwork aclfNetCopy = StateObjectFactory.createAclfNetwork(jsonFile.toString());
+		String json = new AclfNetworkState(aclfNet).toString();
+		AclfNetworkState state = StateObjectFactory.GSON.fromJson(json, AclfNetworkState.class);
+		AclfNetwork aclfNetCopy = AclfNetworkState.create(state);
 
 		assertTrue(new AclfNetJsonComparator("Texas2k JSON round-trip")
 				.compareJson(aclfNet, aclfNetCopy));
