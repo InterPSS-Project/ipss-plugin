4b65c372e7ce410c69f839c1a5177f83a7650bfd
power4all <thuang@users.noreply.github.com>
2026-07-02 23:54:43 -0600

    Add grouped contingency definition schema

---
+++ ipss.plugin.core/src/main/java/org/interpss/plugin/contingency/definition/json/ContingencyActionJson.java
@@ -0,0 +1,16 @@
+package org.interpss.plugin.contingency.definition.json;
+
+import java.util.Map;
+
+public class ContingencyActionJson {
+    @SuppressWarnings("unused")
+    public String object_type;
+    @SuppressWarnings("unused")
+    public String action_type;
+    @SuppressWarnings("unused")
+    public String object_id;
+    @SuppressWarnings("unused")
+    public String extUID;
+    @SuppressWarnings("unused")
+    public Map<String, String> metadata;
+}
