2b042da4062bd85a13bdb541e221db49af28c1fd
IpssDev-4 <ipss.cntester@gmail.com>
2026-07-08 14:34:02 +0800

    Update the json file name

--- ipss.plugin.core/src/sample/java/org/interpss/fstate/FSPluginDclfAlgoRunSample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/fstate/FSPluginDclfAlgoRunSample.java
@@ -22,7 +22,7 @@ public class FSPluginDclfAlgoRunSample {
 	public static void main(String[] args) throws Exception {
 		AclfNetwork aclfNet = IEEE39_RAW_Info_Sample.loadIEEE39Raw();
 
-		String JSON_FILE = "ipss.plugin.core/testData/psse/v30/ieee39_dayahead_plan_maintain_plan.json";
+		String JSON_FILE = "ipss.plugin.core/testData/psse/v30/ieee39Raw_dayahead_plan_maintain_plan.json";
 		Path path = Path.of(JSON_FILE);
 		PlanMaintainModel model = PlanMaintainModelBuilder.fromJson(Files.readString(path, StandardCharsets.UTF_8));
 
@@ -42,7 +42,7 @@ public class FSPluginDclfAlgoRunSample {
 				+ ", clonedNets=" + numNets + ", dclfAlgos=" + numAlgos
 				+ ", T0 refBusP(pu)=" + String.format("%.4f", refBusP));
 
-		System.out.println(fsAlgo.getSimuSummaryInfoMap().toString());
+		//System.out.println(fsAlgo.getSimuSummaryInfoMap().toString());
 		/* 
 		var netSeries = new FStateDclfNetInfoAdapter().adapt(fsAlgo);
 		System.out.println("Net info series:");
@@ -63,72 +63,5 @@ public class FSPluginDclfAlgoRunSample {
 		var subSeries = new FStateDclfSubStationInfoAdapter(sampleSub).adapt(fsAlgo);
 		System.out.println("Sub station info series (" + sampleSub + "):");
 		subSeries.forEach((tpoint, rec) -> System.out.printf("  T%d: %s%n", tpoint, rec.toString()));
-	}
-
-	public static void addInfo2Network(AclfNetwork aclfNet) throws Exception {
-	    // Clear zero-generation contributions
-	    aclfNet.getBusList().forEach(bus -> {
-	        if (bus.getGenP() == 0)
-	            bus.getContributeGenList().clear();
-	    });
-
-	    // PSSE import names devices Gen:1(31) / Load:1(31); plan JSON uses Bus31-G1 / Bus31-L1.
-	    applyInterpssDeviceNames(aclfNet);
-
-	    aclfNet.createAclfGenUIDLookupTable(true);
-	    aclfNet.createAclfLoadUIDLookupTable(true);
-		
-		String namePrefix = "";
-	    aclfNet.getAclfGenUIDLookupTable().values()
-			.forEach(gen -> {
-				if (gen.getName().equals(namePrefix + "Bus39-G1")) {
-					gen.setPGenLimit(new LimitType(10, 0));
-				}
-				else if (gen.getName().equals(namePrefix + "Bus38-G1")) {
-					gen.setPGenLimit(new LimitType(8.3, 0));
-				}
-				else
-					gen.setPGenLimit(new LimitType(7, 0));
-			});
-
-		// set the branch rating.
-		aclfNet.getBranchList().stream() 
-			.forEach(branch -> {
-				AclfBranch aclfBranch = (AclfBranch) branch;
-				//System.out.println("Branch: " + aclfBranch.getName() + " " + aclfBranch.getBranchCode());
-				//aclfBranch.setName(aclfBranch.getId());
-				// Mva1 is used for basecase loading limit
-				aclfBranch.setRatingMva1(600.0);
-			});
-	}
-
-	private static void applyInterpssDeviceNames(AclfNetwork aclfNet) {
-		aclfNet.getBusList().forEach(bus -> {
-			bus.getContributeGenList().forEach(gen -> {
-				if (gen.getName() != null && gen.getName().startsWith("Gen:")) {
-					gen.setName(interpssGenName(bus.getId(), gen.getId()));
-				}
-			});
-			bus.getContributeLoadList().forEach(load -> {
-				if (load.getName() != null && load.getName().startsWith("Load:")) {
-					load.setName(interpssLoadName(bus.getId(), load.getId()));
-				}
-			});
-		});
-	}
-
-	private static String interpssGenName(String busId, String machineId) {
-		return busId + "-G" + trimMachineId(machineId);
-	}
-
-	private static String interpssLoadName(String busId, String machineId) {
-		return busId + "-L" + trimMachineId(machineId);
-	}
-
-	private static String trimMachineId(String machineId) {
-		if (machineId == null || machineId.isBlank()) {
-			return "1";
-		}
-		return machineId.replace("'", "").trim();
 	}
 }
