ad9f88784f54d016fc1918b5f295552b018d392b
IpssDev-4 <ipss.cntester@gmail.com>
2026-07-27 13:51:01 +0800

    Refactor PSSE_IEEE14_NB_BusSplit_Sample and SubstationNBreakerHelper
    - Updated the PSSE_IEEE14_NB_BusSplit_Sample class to streamline the bus splitting process and improve output clarity by renaming variables and methods.
    - Removed redundant methods from SubstationNBreakerHelper, enhancing code maintainability and focusing on the essential functionality for equipment grouping and bus management.
    - Introduced a new helper method for printing equipment by group, improving the usability of the class for network analysis.

--- ipss.plugin.core/src/sample/java/org/interpss/nbreaker/PSSE_IEEE14_NB_BusSplit_Sample.java
+++ ipss.plugin.core/src/sample/java/org/interpss/nbreaker/PSSE_IEEE14_NB_BusSplit_Sample.java
@@ -1,15 +1,10 @@
 package org.interpss.nbreaker;
 
-import java.util.List;
-
 import org.interpss.fadapter.psse.PSSEDirectParser;
 
 import com.interpss.common.exp.InterpssException;
-import com.interpss.core.CoreObjectFactory;
-import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.net.Substation;
-import com.interpss.core.net.nb.NBEquipConnection;
 import com.interpss.core.net.nb.NBSwitch;
 
 /**
@@ -32,32 +27,14 @@ public class PSSE_IEEE14_NB_BusSplit_Sample {
 		// 0 open, 1 closed, 2 stuck closed (default 1)
 		busBar.setCurrentStatus(0);
 
-		int groupNo = subHelper.topoAnalysis();
-		System.out.println("Total number of groups: " + groupNo);
+		int totalGroupNo = subHelper.topoAnalysis();
+		System.out.println("Total number of groups: " + totalGroupNo);
 
 		subHelper.printTopoFlags();
+	
+		SubstationBusSplitMergeHelper busSplitMergeHelper = new SubstationBusSplitMergeHelper(sub2);
+		busSplitMergeHelper.printEquipByGroup(totalGroupNo);
 
-		subHelper.printEquipByGroup(groupNo);
-
-		List<NBEquipConnection> group1 = subHelper.getEquipByGroup(1);
-		System.out.println("Group 1 equip count: " + group1.size());
-
-		List<NBEquipConnection> group2 = subHelper.getEquipByGroup(2);
-		System.out.println("Group 2 equip count: " + group2.size());
-
-		AclfBus bus1 = subHelper.getBusByGroup(1);
-		AclfBus bus1Split = CoreObjectFactory.createAclfBus(bus1.getId() + "_split", net).get();
-		bus1Split.setName(bus1.getName() + " Split");
-		bus1Split.setBaseVoltage(bus1.getBaseVoltage());
-		bus1Split.setVoltage(bus1.getVoltage());
-		System.out.println("Split bus: " + bus1Split.getId()
-				+ " name=" + bus1Split.getName()
-				+ " Vbase=" + bus1Split.getBaseVoltage()
-				+ " V=" + bus1Split.getVoltage()
-				+ " (from " + bus1.getId() + ")");
-
-		// TODO:
-		// 1) remove the equip connection objects of group 1 from bus1
-		// 2) add removed equip connection objects to bus1Split		
+		busSplitMergeHelper.splitBus(1, 2);
 	}
 }
