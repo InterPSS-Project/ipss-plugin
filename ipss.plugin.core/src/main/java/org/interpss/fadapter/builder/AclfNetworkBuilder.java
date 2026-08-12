96f7eeb0049a3eb9f29eed380246946c02ea3d88
IPSSDEV3 <NONE>
2026-08-12 09:17:29 -0600

    fix: align MTDC converter with Core 1.3.10

--- ipss.plugin.core/src/main/java/org/interpss/fadapter/builder/AclfNetworkBuilder.java
+++ ipss.plugin.core/src/main/java/org/interpss/fadapter/builder/AclfNetworkBuilder.java
@@ -1287,8 +1287,10 @@ public class AclfNetworkBuilder {
             conv.setBus(bus);
         }
         conv.setNBridges(nBridges);
-        conv.setFiringAngLimit(new LimitType(angMaxDeg, angMinDeg));
-        conv.setCommutingZ(new Complex(rcOhm, xcOhm));
+        conv.setAngMax(angMaxDeg);
+        conv.setAngMin(angMinDeg);
+        conv.setRc(rcOhm);
+        conv.setXc(xcOhm);
         //conv.setEbas(ebasKv);
         conv.setXformerRatio(xfrRatio);
         conv.setXformerTapSetting(tap);
