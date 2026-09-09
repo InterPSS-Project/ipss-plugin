8851a4cbaa246f754d2f28003042081db02e86b7
power4all <thuang@users.noreply.github.com>
2026-09-09 11:55:35 -0600

    Verify AC11C against repeatable independent benchmark

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -133,6 +133,7 @@ import org.interpss.core.dstab.Ac6cPowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Ac7cPowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Ac8cPowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Ac9cPowerWorldSmibConformanceTest;
+import org.interpss.core.dstab.Ac11cPowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aAndesSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aPowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Esst1aAndesSmibConformanceTest;
@@ -414,6 +415,7 @@ import org.junit.platform.suite.api.Suite;
 	Ac7cPowerWorldSmibConformanceTest.class,
 	Ac8cPowerWorldSmibConformanceTest.class,
 	Ac9cPowerWorldSmibConformanceTest.class,
+	Ac11cPowerWorldSmibConformanceTest.class,
 	Esac1aAndesSmibConformanceTest.class,
 	Esac1aPowerWorldSmibConformanceTest.class,
 	Esst1aAndesSmibConformanceTest.class,
