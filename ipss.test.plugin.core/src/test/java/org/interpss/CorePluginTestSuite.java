bd239a5ad779ccd722cc09f6ab6806a6c2439c8f
power4all <thuang@users.noreply.github.com>
2026-09-08 22:36:10 -0600

    Add IEEEG1 independent trajectory contract

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -124,6 +124,7 @@ import org.interpss.core.dstab.Esst1aPowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Exst1AndesSmibConformanceTest;
 import org.interpss.core.dstab.HygovAndesSmibConformanceTest;
 import org.interpss.core.dstab.Ieeeg1AndesSmibConformanceTest;
+import org.interpss.core.dstab.Ieeeg1PowerWorldSmibConformanceTest;
 import org.interpss.core.dstab.Ieeet1AndesSmibConformanceTest;
 import org.interpss.core.dstab.Ieeet1PowerWorldSmibConformanceTest;
 import org.interpss.core.adapter.cim.CIMDirectParserTest;
@@ -387,6 +388,7 @@ import org.junit.platform.suite.api.Suite;
 	Ieeet1AndesSmibConformanceTest.class,
 	Ieeet1PowerWorldSmibConformanceTest.class,
 	Ieeeg1AndesSmibConformanceTest.class,
+	Ieeeg1PowerWorldSmibConformanceTest.class,
 	HygovAndesSmibConformanceTest.class,
 	Exdc2AndesSmibConformanceTest.class,
 	Ac8bAndesSmibConformanceTest.class,
