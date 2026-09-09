6c583f7f7a892ed2286ae332a28a2741737a2bb0
power4all <thuang@users.noreply.github.com>
2026-09-09 11:33:56 -0600

    Verify AC9C against repeatable independent benchmark

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -132,6 +132,7 @@ import org.interpss.core.dstab.Ac5cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Ac6cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Ac7cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Ac8cIndependentSmibConformanceTest;
+import org.interpss.core.dstab.Ac9cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aAndesSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esst1aAndesSmibConformanceTest;
@@ -412,6 +413,7 @@ import org.junit.platform.suite.api.Suite;
 	Ac6cIndependentSmibConformanceTest.class,
 	Ac7cIndependentSmibConformanceTest.class,
 	Ac8cIndependentSmibConformanceTest.class,
+	Ac9cIndependentSmibConformanceTest.class,
 	Esac1aAndesSmibConformanceTest.class,
 	Esac1aIndependentSmibConformanceTest.class,
 	Esst1aAndesSmibConformanceTest.class,
