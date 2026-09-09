527244d04061df1cccb57fe80a11947b683e564f
power4all <thuang@users.noreply.github.com>
2026-09-08 23:59:51 -0600

    Add PSS2A independent trajectory contract

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -130,6 +130,7 @@ import org.interpss.core.dstab.Ieeeg1AndesSmibConformanceTest;
 import org.interpss.core.dstab.Ieeeg1IndependentSmibConformanceTest;
 import org.interpss.core.dstab.Ieeet1AndesSmibConformanceTest;
 import org.interpss.core.dstab.Ieeet1IndependentSmibConformanceTest;
+import org.interpss.core.dstab.Pss2aIndependentSmibConformanceTest;
 import org.interpss.core.adapter.cim.CIMDirectParserTest;
 import org.interpss.core.adapter.cim.IEEE118CimVsMatpowerJsonCompareTest;
 import org.interpss.core.adapter.ge.EpcDirectParser_SectionGate_Test;
@@ -395,6 +396,7 @@ import org.junit.platform.suite.api.Suite;
 	HygovAndesSmibConformanceTest.class,
 	HygovIndependentSmibConformanceTest.class,
 	Ggov1IndependentSmibConformanceTest.class,
+	Pss2aIndependentSmibConformanceTest.class,
 	Exdc2AndesSmibConformanceTest.class,
 	Ac8bAndesSmibConformanceTest.class,
 	Exdc2aOpenSourceEquationConformanceTest.class,
