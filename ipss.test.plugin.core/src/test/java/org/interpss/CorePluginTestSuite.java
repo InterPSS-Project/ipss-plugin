a7a145d9d9f0fe6cc9d136997408b1d0576d149d
power4all <thuang@users.noreply.github.com>
2026-09-09 09:39:25 -0600

    Verify ESAC2A against repeatable independent benchmark

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -124,6 +124,7 @@ import org.interpss.core.dstab.Exac1aAndesSmibConformanceTest;
 import org.interpss.core.dstab.Exac2AndesSmibConformanceTest;
 import org.interpss.core.dstab.Exac2IndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esac2aAndesExac2SmibConformanceTest;
+import org.interpss.core.dstab.Esac2aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aAndesSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esst1aAndesSmibConformanceTest;
@@ -396,6 +397,7 @@ import org.junit.platform.suite.api.Suite;
 	Exac2AndesSmibConformanceTest.class,
 	Exac2IndependentSmibConformanceTest.class,
 	Esac2aAndesExac2SmibConformanceTest.class,
+	Esac2aIndependentSmibConformanceTest.class,
 	Esac1aAndesSmibConformanceTest.class,
 	Esac1aIndependentSmibConformanceTest.class,
 	Esst1aAndesSmibConformanceTest.class,
