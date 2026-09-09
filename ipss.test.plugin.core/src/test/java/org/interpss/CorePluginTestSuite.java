5fb86ecf161beb53f7d3ecb1688a86f2bd8e889f
power4all <thuang@users.noreply.github.com>
2026-09-09 13:09:59 -0600

    Verify AC2C against repeatable independent benchmark

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -54,6 +54,7 @@ import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderEsurryTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderAc1cTest;
 import org.interpss.core.dstab.Ac1cPowerWorldSmibConformanceTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderAc2cTest;
+import org.interpss.core.dstab.Ac2cPowerWorldSmibConformanceTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderAc3cTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderAc4cTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderAc5cTest;
@@ -546,6 +547,7 @@ import org.junit.platform.suite.api.Suite;
 	DStabNetworkBuilderAc1cTest.class,
 	Ac1cPowerWorldSmibConformanceTest.class,
 	DStabNetworkBuilderAc2cTest.class,
+	Ac2cPowerWorldSmibConformanceTest.class,
 	DStabNetworkBuilderAc3cTest.class,
 	DStabNetworkBuilderAc4cTest.class,
 	DStabNetworkBuilderAc5cTest.class,
