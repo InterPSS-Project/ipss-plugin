dcabbb3063d26967a5276999f52ff88573e93da7
power4all <thuang@users.noreply.github.com>
2026-09-10 12:22:21 -0600

    Verify DC4B against independent

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -181,6 +181,7 @@ import org.interpss.core.dstab.Ieeet1IndependentSmibConformanceTest;
 import org.interpss.core.dstab.Pss2aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Tgov1IndependentSmibConformanceTest;
 import org.interpss.core.dstab.Lcfb1IndependentSmibConformanceTest;
+import org.interpss.core.dstab.Dc4bIndependentSmibConformanceTest;
 import org.interpss.core.adapter.cim.CIMDirectParserTest;
 import org.interpss.core.adapter.cim.IEEE118CimVsMatpowerJsonCompareTest;
 import org.interpss.core.adapter.ge.EpcDirectParser_SectionGate_Test;
@@ -496,6 +497,7 @@ import org.junit.platform.suite.api.Suite;
 	Pss2aIndependentSmibConformanceTest.class,
 	Tgov1IndependentSmibConformanceTest.class,
 	Lcfb1IndependentSmibConformanceTest.class,
+	Dc4bIndependentSmibConformanceTest.class,
 	Exdc2AndesSmibConformanceTest.class,
 	Ac8bAndesSmibConformanceTest.class,
 	Ac8bIndependentSmibConformanceTest.class,
