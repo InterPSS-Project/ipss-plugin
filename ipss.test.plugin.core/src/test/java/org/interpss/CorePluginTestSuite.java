84b2fb6f3f5dc9c7cca9f761ddfbe3b82ae6b22c
power4all <thuang@users.noreply.github.com>
2026-09-09 21:54:09 -0600

    Add ESDC1A independent trajectory contract

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -148,6 +148,7 @@ import org.interpss.core.dstab.Ac11cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Dc1cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Dc2cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Dc4cIndependentSmibConformanceTest;
+import org.interpss.core.dstab.Esdc1aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aAndesSmibConformanceTest;
 import org.interpss.core.dstab.Esac1aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esst1aAndesSmibConformanceTest;
@@ -449,6 +450,7 @@ import org.junit.platform.suite.api.Suite;
 	Dc1cIndependentSmibConformanceTest.class,
 	Dc2cIndependentSmibConformanceTest.class,
 	Dc4cIndependentSmibConformanceTest.class,
+	Esdc1aIndependentSmibConformanceTest.class,
 	Esac1aAndesSmibConformanceTest.class,
 	Esac1aIndependentSmibConformanceTest.class,
 	Esst1aAndesSmibConformanceTest.class,
