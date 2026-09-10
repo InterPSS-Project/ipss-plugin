41c9939f8bbf5ae472879468dc9a791893dc87a3
power4all <thuang@users.noreply.github.com>
2026-09-10 13:47:56 -0600

    Verify ST4C and make independent AUX portable

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -186,6 +186,7 @@ import org.interpss.core.dstab.St1cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esst2aIndependentSmibConformanceTest;
 import org.interpss.core.dstab.St2cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.St3cIndependentSmibConformanceTest;
+import org.interpss.core.dstab.St4cIndependentSmibConformanceTest;
 import org.interpss.core.adapter.cim.CIMDirectParserTest;
 import org.interpss.core.adapter.cim.IEEE118CimVsMatpowerJsonCompareTest;
 import org.interpss.core.adapter.ge.EpcDirectParser_SectionGate_Test;
@@ -506,6 +507,7 @@ import org.junit.platform.suite.api.Suite;
 	Esst2aIndependentSmibConformanceTest.class,
 	St2cIndependentSmibConformanceTest.class,
 	St3cIndependentSmibConformanceTest.class,
+	St4cIndependentSmibConformanceTest.class,
 	Exdc2AndesSmibConformanceTest.class,
 	Ac8bAndesSmibConformanceTest.class,
 	Ac8bIndependentSmibConformanceTest.class,
