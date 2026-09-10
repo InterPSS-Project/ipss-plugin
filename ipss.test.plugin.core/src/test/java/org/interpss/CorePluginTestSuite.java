5b3693bad6a9221ff5e3fa3b5988fb86fec72edc
power4all <thuang@users.noreply.github.com>
2026-09-10 12:58:35 -0600

    Verify ST2C against independent

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -184,6 +184,7 @@ import org.interpss.core.dstab.Lcfb1IndependentSmibConformanceTest;
 import org.interpss.core.dstab.Dc4bIndependentSmibConformanceTest;
 import org.interpss.core.dstab.St1cIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Esst2aIndependentSmibConformanceTest;
+import org.interpss.core.dstab.St2cIndependentSmibConformanceTest;
 import org.interpss.core.adapter.cim.CIMDirectParserTest;
 import org.interpss.core.adapter.cim.IEEE118CimVsMatpowerJsonCompareTest;
 import org.interpss.core.adapter.ge.EpcDirectParser_SectionGate_Test;
@@ -502,6 +503,7 @@ import org.junit.platform.suite.api.Suite;
 	Dc4bIndependentSmibConformanceTest.class,
 	St1cIndependentSmibConformanceTest.class,
 	Esst2aIndependentSmibConformanceTest.class,
+	St2cIndependentSmibConformanceTest.class,
 	Exdc2AndesSmibConformanceTest.class,
 	Ac8bAndesSmibConformanceTest.class,
 	Ac8bIndependentSmibConformanceTest.class,
