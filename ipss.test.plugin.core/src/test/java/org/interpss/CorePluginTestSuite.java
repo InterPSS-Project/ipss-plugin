38f2bb4b974b196584d01b05e29680cee4f5edc0
power4all <thuang@users.noreply.github.com>
2026-09-10 18:37:56 -0600

    Verify ST6C against native simulator

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -55,6 +55,7 @@ import org.interpss.core.dstab.ScrxPsseSmibConformanceTest;
 import org.interpss.core.dstab.Exst2PsseSmibConformanceTest;
 import org.interpss.core.dstab.St7bPsseSmibConformanceTest;
 import org.interpss.core.dstab.St7cPsseSmibConformanceTest;
+import org.interpss.core.dstab.St6cPsseSmibConformanceTest;
 import org.interpss.core.adapter.builder.dstab.Esst3aExciterTest;
 import org.interpss.core.dstab.Esst3aIndependentSmibConformanceTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderEsac5aTest;
@@ -459,6 +460,7 @@ import org.junit.platform.suite.api.Suite;
 	Exst2PsseSmibConformanceTest.class,
 	St7bPsseSmibConformanceTest.class,
 	St7cPsseSmibConformanceTest.class,
+	St6cPsseSmibConformanceTest.class,
 	EmbeddedTrajectoryReferenceTest.class,
 	Regfma1IndependentTrajectoryTest.class,
 	GenrouConformanceTest.class,
