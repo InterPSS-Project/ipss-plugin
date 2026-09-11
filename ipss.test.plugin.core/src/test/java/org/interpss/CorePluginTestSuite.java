a303e3cbe072f3167efb2246e578040da0d896a6
power4all <thuang@users.noreply.github.com>
2026-09-10 18:21:43 -0600

    Verify ST7B against native simulator

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -53,6 +53,7 @@ import org.interpss.core.dstab.Ieeet4PsseSmibConformanceTest;
 import org.interpss.core.dstab.ExeliPsseSmibConformanceTest;
 import org.interpss.core.dstab.ScrxPsseSmibConformanceTest;
 import org.interpss.core.dstab.Exst2PsseSmibConformanceTest;
+import org.interpss.core.dstab.St7bPsseSmibConformanceTest;
 import org.interpss.core.adapter.builder.dstab.Esst3aExciterTest;
 import org.interpss.core.dstab.Esst3aPowerWorldSmibConformanceTest;
 import org.interpss.core.adapter.builder.dstab.DStabNetworkBuilderEsac5aTest;
@@ -455,6 +456,7 @@ import org.junit.platform.suite.api.Suite;
 	ExeliPsseSmibConformanceTest.class,
 	ScrxPsseSmibConformanceTest.class,
 	Exst2PsseSmibConformanceTest.class,
+	St7bPsseSmibConformanceTest.class,
 	PowerWorldCsvReferenceTest.class,
 	Regfma1PowerWorldTrajectoryTest.class,
 	GenrouConformanceTest.class,
