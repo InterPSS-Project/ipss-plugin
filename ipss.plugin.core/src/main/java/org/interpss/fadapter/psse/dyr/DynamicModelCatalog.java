bb4415f2b39a46a52059d11b9c726871755bbf21
power4all <thuang@users.noreply.github.com>
2026-09-12 14:04:52 -0600

    Implement GEWTECU1 electrical controller

--- ipss.plugin.core/src/main/java/org/interpss/fadapter/psse/dyr/DynamicModelCatalog.java
+++ ipss.plugin.core/src/main/java/org/interpss/fadapter/psse/dyr/DynamicModelCatalog.java
@@ -199,6 +199,8 @@ public final class DynamicModelCatalog {
                     "org.interpss.dstab.mach.Wt3g1Model", "Generator%20WT3G1.htm"),
             descriptor("GEWTGCU1", Set.of(), CONVERTER_MACHINE, 26, LOADABLE,
                     "org.interpss.dstab.mach.Gewtgcu1Model", "Vendor_Specific_Models.pdf"),
+            descriptor("GEWTECU1", Set.of(), ELECTRICAL_CONTROLLER, 82, LOADABLE,
+                    "org.interpss.dstab.mach.Gewtecu1Model", "Vendor_Specific_Models.pdf"),
             descriptor("WT3G2", Set.of(), CONVERTER_MACHINE, 14, LOADABLE,
                     "org.interpss.dstab.mach.Wt3g2Model", "Generator%20WT3G2.htm"),
             descriptor("WT4G1", Set.of(), CONVERTER_MACHINE, 9, LOADABLE,
