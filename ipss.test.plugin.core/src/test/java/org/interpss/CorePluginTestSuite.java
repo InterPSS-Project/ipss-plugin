c36853a36b9f467b1e049f70f1bc0669f74ec084
power4all <thuang@users.noreply.github.com>
2026-09-12 21:26:16 -0600

    Register PSS4B verification

--- ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/test/java/org/interpss/CorePluginTestSuite.java
@@ -59,6 +59,7 @@ import org.interpss.core.dstab.Exst2PsseSmibConformanceTest;
 import org.interpss.core.dstab.IeeestPsseSmibConformanceTest;
 import org.interpss.core.dstab.Pss3bPsseSmibConformanceTest;
 import org.interpss.core.dstab.Pss2cPsseSmibConformanceTest;
+import org.interpss.core.dstab.Pss4bPsseSmibConformanceTest;
 import org.interpss.core.dstab.Pss2bIndependentSmibConformanceTest;
 import org.interpss.core.dstab.Pss3cPsseSmibConformanceTest;
 import org.interpss.core.dstab.Pss5cIndependentSmibConformanceTest;
@@ -547,6 +548,7 @@ import org.junit.platform.suite.api.Suite;
 	IeeestPsseSmibConformanceTest.class,
 	Pss3bPsseSmibConformanceTest.class,
 	Pss2cPsseSmibConformanceTest.class,
+	Pss4bPsseSmibConformanceTest.class,
 	Pss2bIndependentSmibConformanceTest.class,
 	Pss3cPsseSmibConformanceTest.class,
     Pss4cIndependentSmibConformanceTest.class,
